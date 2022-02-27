70d1df2ce9156784fa2afcef650b94a13ee8000a
duongpd <duongpd@vinivia.dev>
2022-02-27 22:20:28 +0700

    update icon

--- src/app/pages/nhap/nhap.constant.ts
+++ src/app/pages/nhap/nhap.constant.ts
@@ -1,17 +1,17 @@
 export const NHAP_MAIN_ROUTE = 'nhap';
 export const NHAP_ROUTE_LIST = [
   {
-    icon: 'htvbdh_office',
+    icon: 'htvbdh_tcdt_icon-common',
     title: 'Đấu thầu',
     url: `/${NHAP_MAIN_ROUTE}/dau-thau`,
   },
   {
-    icon: 'htvbdh_office',
+    icon: 'htvbdh_tcdt_icon-common',
     title: 'Mua trực tiếp',
     url: `/${NHAP_MAIN_ROUTE}/mua-truc-tiep`,
   },
   {
-    icon: 'htvbdh_office',
+    icon: 'htvbdh_tcdt_icon-common',
     title: 'Khác',
     url: `/${NHAP_MAIN_ROUTE}/khac`,
   },
--- src/app/pages/xuat/xuat.constant.ts
+++ src/app/pages/xuat/xuat.constant.ts
@@ -1,22 +1,22 @@
 export const NHAP_MAIN_ROUTE = 'xuat';
 export const NHAP_ROUTE_LIST = [
   {
-    icon: 'htvbdh_office',
+    icon: 'htvbdh_tcdt_icon-common',
     title: 'Đấu giá',
     url: `/${NHAP_MAIN_ROUTE}/dau-gia`,
   },
   {
-    icon: 'htvbdh_office',
+    icon: 'htvbdh_tcdt_icon-common',
     title: 'Cứu trợ, hỗ trợ',
     url: `/${NHAP_MAIN_ROUTE}/cuu-tro-ho-tro`,
   },
   {
-    icon: 'htvbdh_office',
+    icon: 'htvbdh_tcdt_icon-common',
     title: 'Viện trợ',
     url: `/${NHAP_MAIN_ROUTE}/vien-tro`,
   },
   {
-    icon: 'htvbdh_office',
+    icon: 'htvbdh_tcdt_icon-common',
     title: 'Xuất khác',
     url: `/${NHAP_MAIN_ROUTE}/xuat-khac`,
   },
