efb414c36f42ec82f36636da67993322c10c44c6
cong <cunbinnak>
2022-04-07 10:32:32 +0700

    fix tong hop

--- src/app/constants/messageValidate.ts
+++ src/app/constants/messageValidate.ts
@@ -1,6 +1,6 @@
 export const MESSAGEVALIDATE = {
-    NOTBLANK: 'Trường này không được để trống',
-    KHONGDUNGDINHDANG: 'Không đúng định dạng',
+    NOTBLANK: 'Trường này không được để trống!',
+    KHONGDUNGDINHDANG: 'Trường này không được để trống và giá trị từ 1000 đến 2999',
     YEAR: 'Vui lòng nhập từ 1000 đến 2999'
   };
   
