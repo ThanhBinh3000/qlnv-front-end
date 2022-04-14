858461787171f6421f95070584eb0a4109bf6a5e
duongpd <phanduyduong97@gmail.com>
2022-04-14 21:46:24 +0700

    quyet dinh phe duyet ket qua lua chon nha thau

--- src/app/pages/nhap/dau-thau/luong-thuc/thong-tin-quyet-dinh-phe-duyet-ket-qua-lcnt/thong-tin-quyet-dinh-phe-duyet-ket-qua-lcnt.component.ts
+++ src/app/pages/nhap/dau-thau/luong-thuc/thong-tin-quyet-dinh-phe-duyet-ket-qua-lcnt/thong-tin-quyet-dinh-phe-duyet-ket-qua-lcnt.component.ts
@@ -1,29 +1,30 @@
+import { ChiTietDeXuatCuaCuc } from './../../../../../models/ChiTietDeXuatCuaCuc';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { NzModalService } from 'ng-zorro-antd/modal';
-import { PAGE_SIZE_DEFAULT } from 'src/app/constants/config';
 import { ActivatedRoute, Router } from '@angular/router';
-import { DialogThongTinPhuLucKHLCNTComponent } from 'src/app/components/dialog/dialog-thong-tin-phu-luc-khlcnt/dialog-thong-tin-phu-luc-khlcnt.component';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { DialogThongTinPhuLucKHLCNTChoCacCucDTNNKVComponent } from 'src/app/components/dialog/dialog-thong-tin-phu-luc-khlcnt-cho-cac-cuc-dtnn-kv/dialog-thong-tin-phu-luc-khlcnt-cho-cac-cuc-dtnn-kv.component';
-import { Subject } from 'rxjs';
-import { MESSAGE } from 'src/app/constants/message';
-import { NzNotificationService } from 'ng-zorro-antd/notification';
 import dayjs from 'dayjs';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Subject } from 'rxjs';
 import { DialogCanCuQDPheDuyetKHLCNTComponent } from 'src/app/components/dialog/dialog-can-cu-qd-phe-duyet-khlcnt/dialog-can-cu-qd-phe-duyet-khlcnt.component';
+import { DialogThongTinPhuLucKHLCNTChoCacCucDTNNKVComponent } from 'src/app/components/dialog/dialog-thong-tin-phu-luc-khlcnt-cho-cac-cuc-dtnn-kv/dialog-thong-tin-phu-luc-khlcnt-cho-cac-cuc-dtnn-kv.component';
+import { PAGE_SIZE_DEFAULT } from 'src/app/constants/config';
+import { MESSAGE } from 'src/app/constants/message';
+import { ThongTinQuyetDinhPheDuyetKHLCNT } from './../../../../../models/ThongTinQuyetDinhPheDuyetKHLCNT';
 
-interface ItemData {
-  id: string;
-  stt: string;
-  tenGoiThau: string;
-  soHieuGoiThau: string;
-  diaDiemNhapKho: string;
-  soLuong: string;
-  trungHuy: string;
-  donViTrungThau: string;
-  giaTrungThau: string;
-  lyDoHuyThau: string;
-}
+// interface ItemData {
+//   id: string;
+//   stt: string;
+//   tenGoiThau: string;
+//   soHieuGoiThau: string;
+//   diaDiemNhapKho: string;
+//   soLuong: string;
+//   trungHuy: string;
+//   donViTrungThau: string;
+//   giaTrungThau: string;
+//   lyDoHuyThau: string;
+// }
 @Component({
   selector: 'thong-tin-quyet-dinh-phe-duyet-ket-qua-lcnt',
   templateUrl: './thong-tin-quyet-dinh-phe-duyet-ket-qua-lcnt.component.html',
@@ -41,12 +42,13 @@ export class ThongTinQuyetDinhPheDuyetKetQuaLCNTComponent implements OnInit {
   totalRecord: number = 10;
   i = 0;
   editId: string | null = null;
-  listOfData: ItemData[] = [];
-  isVisibleChangeTab$ = new Subject();
-  visibleTab: boolean = false;
+  // listOfData: ItemData[] = [];
+  // isVisibleChangeTab$ = new Subject();
+  // visibleTab: boolean = false;
   listNam: any[] = [];
   yearNow: number = 0;
-  selectedCanCu: any = {};
+  chiTietThongTinQDPheDuyetKQLCNT: ThongTinQuyetDinhPheDuyetKHLCNT =
+    new ThongTinQuyetDinhPheDuyetKHLCNT();
   constructor(
     private modal: NzModalService,
     private router: Router,
@@ -63,24 +65,24 @@ export class ThongTinQuyetDinhPheDuyetKetQuaLCNTComponent implements OnInit {
         text: this.yearNow - i,
       });
     }
-    this.isVisibleChangeTab$.subscribe((value: boolean) => {
-      this.visibleTab = value;
-    });
-    this.listOfData = [
-      ...this.listOfData,
-      {
-        id: `${this.i}`,
-        stt: `${this.i}`,
-        tenGoiThau: 'Mua gạo dự trữ A',
-        soHieuGoiThau: `Gói 1`,
-        diaDiemNhapKho: 'Cục DTNN KV Vĩnh phú',
-        soLuong: `120`,
-        trungHuy: `Trúng`,
-        donViTrungThau: `Công ty A`,
-        giaTrungThau: `10.000.000`,
-        lyDoHuyThau: `Mua gạo dự trữ A`,
-      },
-    ];
+    // this.isVisibleChangeTab$.subscribe((value: boolean) => {
+    //   this.visibleTab = value;
+    // });
+    // this.listOfData = [
+    //   ...this.listOfData,
+    //   {
+    //     id: `${this.i}`,
+    //     stt: `${this.i}`,
+    //     tenGoiThau: 'Mua gạo dự trữ A',
+    //     soHieuGoiThau: `Gói 1`,
+    //     diaDiemNhapKho: 'Cục DTNN KV Vĩnh phú',
+    //     soLuong: `120`,
+    //     trungHuy: `Trúng`,
+    //     donViTrungThau: `Công ty A`,
+    //     giaTrungThau: `10.000.000`,
+    //     lyDoHuyThau: `Mua gạo dự trữ A`,
+    //   },
+    // ];
     this.id = +this.routerActive.snapshot.paramMap.get('id');
   }
 
@@ -88,7 +90,7 @@ export class ThongTinQuyetDinhPheDuyetKetQuaLCNTComponent implements OnInit {
     this.router.navigate([`/nhap/dau-thau/quyet-dinh-phe-duyet-ket-qua-lcnt`]);
   }
 
-  openDialogThongTinPhuLucKLCNT() {
+  openDialogThongTinPhuLucKLCNT(data: ChiTietDeXuatCuaCuc) {
     this.modal.create({
       nzTitle: 'Thông tin phụ lục KH LNCT cho các Cục DTNN KV',
       nzContent: DialogThongTinPhuLucKHLCNTChoCacCucDTNNKVComponent,
@@ -97,13 +99,10 @@ export class ThongTinQuyetDinhPheDuyetKetQuaLCNTComponent implements OnInit {
       nzWidth: '900px',
       nzFooter: null,
       nzComponentParams: {
-        // totalRecord: this.totalRecord,
-        // date: event,
+        data: data,
       },
     });
   }
-
-  themMoi() {}
 
   async changePageIndex(event) {
     this.spinner.show();
@@ -140,8 +139,8 @@ export class ThongTinQuyetDinhPheDuyetKetQuaLCNTComponent implements OnInit {
     });
     modalQD.afterClose.subscribe((data) => {
       if (data) {
-        console.log(data);
-        this.selectedCanCu = data;
+        this.chiTietThongTinQDPheDuyetKQLCNT = data;
+        console.log(this.chiTietThongTinQDPheDuyetKQLCNT);
       }
     });
   }
