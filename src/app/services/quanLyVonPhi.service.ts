4467f4a3975adcb42165983b3c81f6a1e3165fa0
duydat26 <duydat26@gmail.com>
2022-04-15 16:10:12 +0700

    update service

--- src/app/services/quanLyVonPhi.service.ts
+++ src/app/services/quanLyVonPhi.service.ts
@@ -146,8 +146,8 @@ export class QuanLyVonPhiService extends BaseService {
   // upload list
   updatelist(request: any): Observable<any> {
     return this.httpClient.put(
-      // this.urlDefault + '/qlnv-khoachphi/lap-tham-dinh-du-toan/cap-nhat',
-      'http://192.168.1.111:8094/lap-tham-dinh-du-toan/cap-nhat',
+      this.urlDefault + '/qlnv-khoachphi/lap-tham-dinh-du-toan/cap-nhat',
+      // 'http://192.168.1.111:8094/lap-tham-dinh-du-toan/cap-nhat',
       request,
     );
   }
