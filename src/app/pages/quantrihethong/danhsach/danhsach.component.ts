d33ad2cb6f4040abb201d42bcc390114fb80ab44
Danhlocnd <danhloc.nd@gmail.com>
2022-04-16 17:38:24 +0700

    add giao dien danh sach quan trị

--- src/app/pages/quantrihethong/danhsach/danhsach.component.ts
+++ src/app/pages/quantrihethong/danhsach/danhsach.component.ts
@@ -1,80 +1,231 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild
-} from '@angular/core';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-// import { NHAP_ROUTE_LIST } from './danhsach.constant';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { DonviService } from 'src/app/services/donvi.service';
+import { MESSAGE } from 'src/app/constants/message';
+import * as dayjs from 'dayjs';
+import { NzDatePickerComponent } from 'ng-zorro-antd/date-picker';
+import { PAGE_SIZE_DEFAULT } from 'src/app/constants/config';
+import { DanhSachDauThauService } from 'src/app/services/danhSachDauThau.service';
+import { Subject } from 'rxjs';
+import { convertTrangThai } from 'src/app/shared/commonFunction';
+
+
 @Component({
   selector: 'app-danhsach',
   templateUrl: './danhsach.component.html',
   styleUrls: ['./danhsach.component.scss'],
 })
-export class DanhSachComponent implements OnInit, AfterViewInit {
-  @ViewChild('myTab') myTab: ElementRef;
-  // routes = NHAP_ROUTE_LIST;
-  routerUrl: string = "";
-
+export class DanhSachComponent implements OnInit {
+  @ViewChild('endDatePicker') endDatePicker!: NzDatePickerComponent;
+  searchValue = '';
+  searchFilter = {
+    soDxuat: '',
+    trichYeu: '',
+  };
+  inputDonVi: string = '';
+  options: any[] = [];
+  optionsDonVi: any[] = [];
+  errorMessage: string = '';
+  startValue: Date | null = null;
+  endValue: Date | null = null;
+  page: number = 1;
+  pageSize: number = PAGE_SIZE_DEFAULT;
+  totalRecord: number = 0;
+  dataTable: any[] = [];
+  isVisibleChangeTab$ = new Subject();
   constructor(
+    private spinner: NgxSpinnerService,
+    private donViService: DonviService,
+    private danhSachDauThauService: DanhSachDauThauService,
+    private notification: NzNotificationService,
     private router: Router,
-  ) { }
+    private modal: NzModalService,
+  ) {}
+  // ngAfterViewInit(): void {
+  //   throw new Error('Method not implemented.');
+  // }
 
-  ngOnInit(): void {
-    if (this.router.url) {
-      this.routerUrl = this.router.url;
-    }
-  }
-
-  ngAfterViewInit() {
-    // if (
-    //   this.myTab.nativeElement.scrollWidth >
-    //   this.myTab.nativeElement.clientWidth
-    // ) {
-    //   this.myTab.nativeElement.className =
-    //     'nav nav-tabs expand-sidebar next-an';
-    // } else {
-    //   this.myTab.nativeElement.className = 'nav nav-tabs';
-    // }
-  }
-
-  updateCssOverlay() {
-    setTimeout(() => {
-      let child = document.getElementsByClassName('dau-thau-tab');
-      if (child && child.length > 0) {
-        child[0].parentElement.classList.add('left-0');
+  async ngOnInit() {
+    this.spinner.show();
+    try {
+      let res = await this.donViService.layTatCaDonVi();
+      this.optionsDonVi = [];
+      if (res.msg == MESSAGE.SUCCESS) {
+        for (let i = 0; i < res.data.length; i++) {
+          var item = {
+            ...res.data[i],
+            labelDonVi: res.data[i].maDvi + ' - ' + res.data[i].tenDvi,
+          };
+          this.optionsDonVi.push(item);
+        }
+      } else {
+        this.notification.error(MESSAGE.ERROR, res.msg);
       }
-    }, 200);
-  }
-
-  endSlide() {
-    if (
-      this.myTab.nativeElement.scrollWidth >
-      this.myTab.nativeElement.clientWidth
-    ) {
-      this.myTab.nativeElement.scrollTo({
-        left: this.myTab.nativeElement.scrollWidth,
-        top: 0,
-        behavior: 'smooth',
-      });
-      this.myTab.nativeElement.className =
-        'nav nav-tabs expand-sidebar prev-an';
+      await this.search();
+      this.spinner.hide();
+    } catch (e) {
+      console.log('error: ', e);
+      this.spinner.hide();
+      this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
     }
   }
 
-  startSlide() {
-    if (
-      this.myTab.nativeElement.scrollWidth >
-      this.myTab.nativeElement.clientWidth
-    ) {
-      this.myTab.nativeElement.scrollTo({
-        left: 0,
-        top: 0,
-        behavior: 'smooth',
-      });
-      this.myTab.nativeElement.className =
-        'nav nav-tabs expand-sidebar next-an';
+  onInput(e: Event): void {
+    const value = (e.target as HTMLInputElement).value;
+    if (!value || value.indexOf('@') >= 0) {
+      this.options = [];
+    } else {
+      this.options = this.optionsDonVi.filter(
+        (x) => x.labelDonVi.toLowerCase().indexOf(value.toLowerCase()) != -1,
+      );
     }
+  }
+
+  disabledStartDate = (startValue: Date): boolean => {
+    if (!startValue || !this.endValue) {
+      return false;
+    }
+    return startValue.getTime() > this.endValue.getTime();
+  };
+
+  disabledEndDate = (endValue: Date): boolean => {
+    if (!endValue || !this.startValue) {
+      return false;
+    }
+    return endValue.getTime() <= this.startValue.getTime();
+  };
+
+  handleStartOpenChange(open: boolean): void {
+    if (!open) {
+      this.endDatePicker.open();
+    }
+  }
+
+  handleEndOpenChange(open: boolean): void {
+    console.log('handleEndOpenChange', open);
+  }
+
+  redirectToChiTiet(id: number) {
+    this.router.navigate([
+      '/nhap/dau-thau/danh-sach-dau-thau/them-moi-de-xuat-ke-hoach-lua-chon-nha-thau',
+      id,
+    ]);
+  }
+
+  clearFilter() {
+    this.searchFilter = {
+      soDxuat: '',
+      trichYeu: '',
+    };
+    this.startValue = null;
+    this.endValue = null;
+    this.inputDonVi = '';
+    this.search();
+  }
+
+  async search() {
+    let maDonVi = null;
+    if (this.inputDonVi && this.inputDonVi.length > 0) {
+      let getDonVi = this.optionsDonVi.filter(
+        (x) => x.labelDonVi == this.inputDonVi,
+      );
+      if (getDonVi && getDonVi.length > 0) {
+        maDonVi = getDonVi[0].maDvi;
+      }
+    }
+    let body = {
+      denNgayKy: this.endValue
+        ? dayjs(this.endValue).format('YYYY-MM-DD')
+        : null,
+      id: 0,
+      // loaiVthh: '00',
+      maDvi: maDonVi,
+      paggingReq: {
+        limit: this.pageSize,
+        page: this.page,
+      },
+      soDxuat: this.searchFilter.soDxuat,
+      str: null,
+      trichYeu: this.searchFilter.trichYeu,
+      tuNgayKy: this.startValue
+        ? dayjs(this.startValue).format('YYYY-MM-DD')
+        : null,
+    };
+    let res = await this.danhSachDauThauService.timKiem(body);
+    if (res.msg == MESSAGE.SUCCESS) {
+      let data = res.data;
+      this.dataTable = data.content;
+      this.totalRecord = data.totalElements;
+    } else {
+      this.notification.error(MESSAGE.ERROR, res.msg);
+    }
+  }
+
+  async changePageIndex(event) {
+    this.spinner.show();
+    try {
+      this.page = event;
+      await this.search();
+      this.spinner.hide();
+    } catch (e) {
+      console.log('error: ', e);
+      this.spinner.hide();
+      this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+    }
+  }
+
+  async changePageSize(event) {
+    this.spinner.show();
+    try {
+      this.pageSize = event;
+      await this.search();
+      this.spinner.hide();
+    } catch (e) {
+      console.log('error: ', e);
+      this.spinner.hide();
+      this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+    }
+  }
+
+  convertTrangThai(status: string) {
+    return convertTrangThai(status);
+  }
+
+  xoaItem(item: any) {
+    this.modal.confirm({
+      nzClosable: false,
+      nzTitle: 'Xác nhận',
+      nzContent: 'Bạn có chắc chắn muốn xóa?',
+      nzOkText: 'Đồng ý',
+      nzCancelText: 'Không',
+      nzOkDanger: true,
+      nzWidth: 310,
+      nzOnOk: () => {
+        this.spinner.show();
+        try {
+          this.danhSachDauThauService
+            .deleteKeHoachLCNT({ id: item.id })
+            .then((res) => {
+              if (res.msg == MESSAGE.SUCCESS) {
+                this.notification.success(
+                  MESSAGE.SUCCESS,
+                  MESSAGE.DELETE_SUCCESS,
+                );
+                this.search();
+              } else {
+                this.notification.error(MESSAGE.ERROR, res.msg);
+              }
+              this.spinner.hide();
+            });
+        } catch (e) {
+          console.log('error: ', e);
+          this.spinner.hide();
+          this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+        }
+      },
+    });
   }
 }
--- src/app/pages/quantrihethong/quantrihethong.module.ts
+++ src/app/pages/quantrihethong/quantrihethong.module.ts
@@ -6,12 +6,14 @@ import { ComponentsModule } from 'src/app/components/components.module';
 import { MainModule } from 'src/app/layout/main/main.module';
 import { QuanTriHeThongRoutingModule } from './quantrihethong-routing.module';
 import { QuanTriHeThongComponent } from './quantrihethong.component';
+import { DanhSachComponent } from './danhsach/danhsach.component';
 
 @NgModule({
   declarations: [
     QuanTriHeThongComponent,
-    
+    DanhSachComponent
   ],
   imports: [CommonModule, QuanTriHeThongRoutingModule, ComponentsModule, MainModule],
+  
 })
 export class QuanTriHeThongModule { }
