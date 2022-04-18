a1ae43f5eae5ef1a26264aeb795502f9b5929384
hoangth <hoangth@hoangth.me>
2022-04-18 19:46:48 +0700

    feat: commit

--- src/app/pages/quan-ly-ke-hoach-von-phi/quan-ly-lap-tham-dinh-du-toan-nsnn/chuc-nang-chi-cuc/ke-hoach-xay-dung-van-ban-qppl-dtqg-3-nam/ke-hoach-xay-dung-van-ban-qppl-dtqg-3-nam.component.ts
+++ src/app/pages/quan-ly-ke-hoach-von-phi/quan-ly-lap-tham-dinh-du-toan-nsnn/chuc-nang-chi-cuc/ke-hoach-xay-dung-van-ban-qppl-dtqg-3-nam/ke-hoach-xay-dung-van-ban-qppl-dtqg-3-nam.component.ts
@@ -179,6 +179,7 @@ export class KeHoachXayDungVanBanQpplDtqg3NamComponent implements OnInit {
       this.maBaoCao = '';
       this.namBaoCaoHienHanh = new Date().getFullYear();
     }
+    
 
     this.getStatusButton();
 
