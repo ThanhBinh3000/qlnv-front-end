d5023779b0251364d2a5963a3577bcfdc7d53339
honghv <a366ngay@gmail.com>
2022-05-23 23:51:17 +0700

    update phieu kiem tra

--- src/app/services/quanLyPhieuKiemTraChatLuongHang.service.ts
+++ src/app/services/quanLyPhieuKiemTraChatLuongHang.service.ts
@@ -17,47 +17,8 @@ export class QuanLyPhieuKiemTraChatLuongHangService extends BaseService {
   }
 
   timKiem(body: any): Promise<any> {
-    let url_ = `${environment.SERVICE_API}${this.GATEWAY}/ql-phieu-kiem-tra-chat-luong-hang-lt?`;
-    if (body.maDonVi)
-      url_ += 'maDonVi=' + encodeURIComponent('' + body.maDonVi) + '&';
-    if (body.maHangHoa)
-      url_ += 'maHangHoa=' + encodeURIComponent('' + body.maHangHoa) + '&';
-    if (body.maNganKho)
-      url_ += 'maNganKho=' + encodeURIComponent('' + body.maNganKho) + '&';
-    if (body.ngayKiemTraDenNgay)
-      url_ +=
-        'ngayKiemTraDenNgay=' +
-        encodeURIComponent('' + body.ngayKiemTraDenNgay) +
-        '&';
-    if (body.ngayKiemTraTuNgay)
-      url_ +=
-        'ngayKiemTraTuNgay=' +
-        encodeURIComponent('' + body.ngayKiemTraTuNgay) +
-        '&';
-    if (body.pageNumber)
-      url_ +=
-        'pageable.pageNumber=' + encodeURIComponent('' + body.pageNumber) + '&';
-    if (body.offset)
-      url_ += 'pageable.offset=' + encodeURIComponent('' + body.offset) + '&';
-    if (body.pageSize)
-      url_ +=
-        'pageable.pageSize=' + encodeURIComponent('' + body.pageSize) + '&';
-    if (body.paged)
-      url_ += 'pageable.paged=' + encodeURIComponent('' + body.paged) + '&';
-    if (body.sorted)
-      url_ +=
-        'pageable.sort.sorted=' + encodeURIComponent('' + body.sorted) + '&';
-    if (body.unsorted)
-      url_ +=
-        'pageable.sort.unsorted=' +
-        encodeURIComponent('' + body.unsorted) +
-        '&';
-    if (body.unpaged)
-      url_ += 'pageable.unpaged=' + encodeURIComponent('' + body.unpaged) + '&';
-    if (body.soPhieu)
-      url_ += 'soPhieu=' + encodeURIComponent('' + body.soPhieu) + '&';
-    url_ = url_.replace(/[?&]$/, '');
-    return this.httpClient.get<any>(url_).toPromise();
+    let url = `${environment.SERVICE_API}${this.GATEWAY}/ql-phieu-kiem-tra-chat-luong-hang-lt/tra-cuu`
+    return this.httpClient.post<any>(url, body).toPromise();
   }
 
   them(body: any): Promise<any> {
@@ -67,6 +28,11 @@ export class QuanLyPhieuKiemTraChatLuongHangService extends BaseService {
 
   sua(body: any): Promise<any> {
     const url = `${environment.SERVICE_API}${this.GATEWAY}/ql-phieu-kiem-tra-chat-luong-hang-lt`;
+    return this.httpClient.put(url, body).toPromise();
+  }
+
+  updateStatus(body: any): Promise<any> {
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/ql-phieu-kiem-tra-chat-luong-hang-lt/phe-duyet`;
     return this.httpClient.put(url, body).toPromise();
   }
 }
