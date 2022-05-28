e70a5f1ea8f2947275e1d39dbb3c4d6af3b86ead
duongpd <phanduyduong97@gmail.com>
2022-05-28 22:43:01 +0700

    ky hop dong- thong tin phu luc

--- src/app/pages/mua-hang/hop-dong/danh-sach-hop-dong/phu-luc/phu-luc.component.ts
+++ src/app/pages/mua-hang/hop-dong/danh-sach-hop-dong/phu-luc/phu-luc.component.ts
@@ -1,4 +1,6 @@
+import { DialogThongTinPhuLucHopDongMuaComponent } from './../../../../../components/dialog/dialog-thong-tin-phu-luc-hop-dong-mua/dialog-thong-tin-phu-luc-hop-dong-mua.component';
 import { Component, OnInit } from '@angular/core';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-phu-luc',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhuLucComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modal: NzModalService) { }
 
   ngOnInit() {
   }
-
+  thongTinPhuLuc() {
+    const modal = this.modal.create({
+      nzTitle: 'Thông tin phụ lục KH LNCT cho các Cục DTNN KV',
+      nzContent: DialogThongTinPhuLucHopDongMuaComponent,
+      nzMaskClosable: false,
+      nzClosable: false,
+      nzWidth: '1200px',
+      nzFooter: null,
+      nzComponentParams: {},
+    });
+    modal.afterClose.subscribe((res) => { });
+  }
 }
