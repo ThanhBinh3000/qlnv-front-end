de9b1aca4fc1fe038fdd19d70401d0086102346f
duydat26 <duydat26@gmail.com>
2022-05-30 16:46:20 +0700

    fix: sua lai giao du toan chi NSNN

---
+++ src/app/pages/quan-ly-ke-hoach-von-phi/quan-ly-giao-du-toan-chi-nsnn/chuc-nang-TCDT/tim-kiem-quyet-dinh-nhap-du-toan-chi-NSNN/tim-kiem-quyet-dinh-nhap-du-toan-chi-NSNN.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TimKiemQuyetDinhNhapDuToanChiNSNNComponent } from './tim-kiem-quyet-dinh-nhap-du-toan-chi-NSNN.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TimKiemQuyetDinhNhapDuToanChiNSNNComponent]
+})
+export class TimKiemQuyetDinhNhapDuToanChiNSNNModule { }
