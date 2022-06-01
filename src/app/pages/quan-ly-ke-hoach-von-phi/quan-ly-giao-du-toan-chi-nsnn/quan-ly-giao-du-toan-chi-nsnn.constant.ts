1c4bb00039032277d2077ece627a3d0241e8e881
duydat26 <duydat26@gmail.com>
2022-06-01 14:43:37 +0700

    feat: them man tin kiem giao va man giao  cho cac don vi

--- src/app/pages/quan-ly-ke-hoach-von-phi/quan-ly-giao-du-toan-chi-nsnn/quan-ly-giao-du-toan-chi-nsnn.constant.ts
+++ src/app/pages/quan-ly-ke-hoach-von-phi/quan-ly-giao-du-toan-chi-nsnn/quan-ly-giao-du-toan-chi-nsnn.constant.ts
@@ -13,4 +13,10 @@ export const QUAN_LY_GIAO_DU_TOAN_CHI_NSNN_LIST: QuanLyGiaoDuToanChiNSNN[] = [
     description: 'Tìm kiếm Phương án phân bổ giao dự toán chi NSNN cho các đơn vị',
     url: `/${MAIN_ROUTE_QUAN_LY_KE_HOACH_VON_PHI}/${MAIN_ROUTE_QUAN_LY_GIAO_DU_TOAN_CHI_NSNN}/tim-kiem-phan-bo-giao-du-toan-chi-NSNN-cho-cac-don-vi`,
   },
+  {
+    icon: 'htvbdh_tcdt_icon-common',
+    title: 'Tìm kiếm giao dự toán chi NSNN của các đơn vị',
+    description: 'Tìm kiếm giao dự toán chi NSNN của các đơn vị',
+    url: `/${MAIN_ROUTE_QUAN_LY_KE_HOACH_VON_PHI}/${MAIN_ROUTE_QUAN_LY_GIAO_DU_TOAN_CHI_NSNN}/tim-kiem-giao-du-toan-chi-NSNN-cua-cac-don-vi`,
+  },
 ];
