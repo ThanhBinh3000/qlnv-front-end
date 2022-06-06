7fa124aea5cbe5c1bf6475c7a24ae40ed43ce537
duongpd <phanduyduong97@gmail.com>
2022-06-06 21:57:11 +0700

    phieu nhap day kho

--- src/app/pages/nhap/dau-thau/nhap-kho/nhap-kho.module.ts
+++ src/app/pages/nhap/dau-thau/nhap-kho/nhap-kho.module.ts
@@ -1,14 +1,18 @@
+import { ThemMoiPhieuNhapDayKhoComponent } from './quan-ly-phieu-nhap-day-kho/them-moi-phieu-nhap-day-kho/them-moi-phieu-nhap-day-kho.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NhapKhoComponent } from './nhap-kho.component';
 import { ChucNangNhapKhoComponent } from './chuc-nang-nhap-kho/chuc-nang-nhap-kho.component';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { DirectivesModule } from 'src/app/directives/directives.module';
+import { QuanLyPhieuNhapDayKhoComponent } from './quan-ly-phieu-nhap-day-kho/quan-ly-phieu-nhap-day-kho.component';
 
 @NgModule({
   declarations: [
     NhapKhoComponent,
     ChucNangNhapKhoComponent,
+    QuanLyPhieuNhapDayKhoComponent,
+    ThemMoiPhieuNhapDayKhoComponent
   ],
   imports: [
     CommonModule,
@@ -18,6 +22,7 @@ import { DirectivesModule } from 'src/app/directives/directives.module';
   exports: [
     NhapKhoComponent,
     ChucNangNhapKhoComponent,
+    QuanLyPhieuNhapDayKhoComponent
   ]
 })
 export class NhapKhoModule { }
