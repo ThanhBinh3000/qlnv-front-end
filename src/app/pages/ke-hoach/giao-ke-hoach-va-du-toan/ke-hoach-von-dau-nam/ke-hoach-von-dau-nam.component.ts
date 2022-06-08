b14712c2f8a0a48d4f65e8dbdffcf080aac31f05
honghv <a366ngay@gmail.com>
2022-06-08 10:48:09 +0700

    update giao chi tieu dau nam count

--- src/app/pages/ke-hoach/giao-ke-hoach-va-du-toan/ke-hoach-von-dau-nam/ke-hoach-von-dau-nam.component.ts
+++ src/app/pages/ke-hoach/giao-ke-hoach-va-du-toan/ke-hoach-von-dau-nam/ke-hoach-von-dau-nam.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { MESSAGE } from 'src/app/constants/message';
+import { GiaoKeHoachVonDauNamService } from 'src/app/services/giaoKeHoachVonDauNam.service';
 
 @Component({
   selector: 'app-ke-hoach-von-dau-nam',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ke-hoach-von-dau-nam.component.scss']
 })
 export class KeHoachVonDauNamComponent implements OnInit {
+  countQuyetDinh: number = 0;
+  countChiTieu: number = 0;
+  countDieuChinh: number = 0;
+  countDuToan: number = 0;
 
-  constructor() { }
+  constructor(
+    private spinner: NgxSpinnerService,
+    private giaoKeHoachVonDauNamService: GiaoKeHoachVonDauNamService,
+    private notification: NzNotificationService,
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.spinner.show();
+    try {
+      let res = await this.giaoKeHoachVonDauNamService.countSoLuong();
+      if (res.msg == MESSAGE.SUCCESS) {
+        if (res.data) {
+          if (res.data.chiTieuKeHoach) {
+            this.countChiTieu = res.data.chiTieuKeHoach;
+          }
+          if (res.data.dieuChinhChiTieu) {
+            this.countDieuChinh = res.data.dieuChinhChiTieu;
+          }
+        }
+      } else {
+        this.notification.error(MESSAGE.ERROR, res.msg);
+      }
+      this.spinner.hide();
+    } catch (e) {
+      console.log('error: ', e);
+      this.spinner.hide();
+      this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+    }
   }
-
 }
