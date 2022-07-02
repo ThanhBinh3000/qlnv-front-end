b639281f178e0f5c41de4c814220dcef187f0dfb
VuNT <vunt@ttkd6.com>
2022-07-02 09:43:01 +0700

    fix mua hang

--- src/app/components/dialog/dialog-them-moi-goi-thau/dialog-them-moi-goi-thau.component.ts
+++ src/app/components/dialog/dialog-them-moi-goi-thau/dialog-them-moi-goi-thau.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NzModalRef } from 'ng-zorro-antd/modal';
 import { uniqBy } from 'lodash';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { PAGE_SIZE_DEFAULT } from 'src/app/constants/config';
+import { DanhSachGoiThau } from 'src/app/models/DeXuatKeHoachuaChonNhaThau';
 
 @Component({
   selector: 'dialog-them-moi-goi-thau',
@@ -15,28 +17,41 @@ export class DialogThemMoiGoiThauComponent implements OnInit {
   vatTuChaId?: any;
   listChungLoai = [];
   listDonVi = [];
+  page: number = 1;
+  pageSize: number = PAGE_SIZE_DEFAULT;
+  totalRecord: number = 0;
+  dataTable: any[] = [];
+  thongtinDauThau: DanhSachGoiThau = new DanhSachGoiThau();
+
   constructor(
     private _modalRef: NzModalRef,
     private fb: FormBuilder
   ) {
     this.formGoiThau = this.fb.group({
       tenGThau: [null],
-      loaiHh: [null],
-      cLoaiVThh: [null],
+      tenVthh: [null],
+      loaiVthh: [null],
+      cloaiVthh: [null],
+      tenCloaiVthh: [null],
       dvTinh: [null],
-      sLuong: [null],
+      soLuong: [null],
       donGia: [null],
       thanhTien: [null],
       hThuc: [null],
       pThucLCNT: [null],
       tgianBatDau: [null],
       loaiHdong: [null],
-      tgianThucHien: [null]
+      tgianThienHd: [null]
     });
   }
 
   ngOnInit(): void {
-    this.listChungLoai = uniqBy(this.listVatTu.filter(item => item.maVatTuCha == this.vatTuChaId), 'maVatTu');
+    this.listChungLoai = uniqBy(this.listVatTu.filter(item => item.maVatTuCha == this.vatTuChaId), 'maVatTu', 'tenVatTu');
+    console.log(this.listChungLoai);
+    this.formGoiThau.patchValue({
+      loaiVthh: this.listChungLoai[0].maVatTuCha,
+      tenVthh: this.listChungLoai[0].tenVatTuCha
+    })
   }
 
   handleOk() {
@@ -50,6 +65,20 @@ export class DialogThemMoiGoiThauComponent implements OnInit {
   }
 
   changeChungLoai(event) {
-    this.listDonVi = uniqBy(this.listChungLoai.filter(item => item.maVatTu == event), 'maDvi');
+    this.listDonVi = uniqBy(this.listChungLoai.filter(item => item.maVatTu == event), 'maDvi', 'tenDonVi');
   }
+
+  addDiemKho() {
+    if (this.thongtinDauThau.maDvi && this.thongtinDauThau.soLuong) {
+      let dataDvi = this.listDonVi.filter(d => d.maDvi == this.thongtinDauThau.maDvi)
+      this.thongtinDauThau.tenDvi = dataDvi[0].tenDonVi;
+      this.dataTable = [...this.dataTable, this.thongtinDauThau];
+      this.thongtinDauThau = new DanhSachGoiThau();
+    }
+  }
+
+  clearDiemKho() {
+
+  }
+
 }
