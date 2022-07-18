8ba683e67c8e86e1031d9a6f9beb08b19017c17d
Binh <binhk4a@gmail.com>
2022-07-18 08:35:00 +0700

    tim kiem xuat file

--- src/app/pages/ke-hoach/giao-ke-hoach-va-du-toan/ke-hoach-von-dau-nam/quyet-dinh/btc-giao-cac-bo-nganh/btc-giao-cac-bo-nganh.component.ts
+++ src/app/pages/ke-hoach/giao-ke-hoach-va-du-toan/ke-hoach-von-dau-nam/quyet-dinh/btc-giao-cac-bo-nganh/btc-giao-cac-bo-nganh.component.ts
@@ -2,7 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { PAGE_SIZE_DEFAULT } from 'src/app/constants/config';
 import * as dayjs from 'dayjs';
-
+import { QuyetDinhBtcNganhService } from 'src/app/services/quyetDinhBtcNganh.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { UserService } from 'src/app/services/user.service';
+import { MESSAGE } from 'src/app/constants/message';
+import { cloneDeep } from 'lodash';
+import { saveAs } from 'file-saver';
 @Component({
   selector: 'app-btc-giao-cac-bo-nganh',
   templateUrl: './btc-giao-cac-bo-nganh.component.html',
@@ -25,105 +31,46 @@ export class BtcGiaoCacBoNganhComponent implements OnInit {
     ngayQd: new Date(),
     trichYeu: null,
   };
+  filterTable: any = {
+    soQd: '',
+    namKhoach: '',
+    ngayQd: '',
+    trichYeu: '',
+    taiLieuDinhKem: '',
+    trangThai: '',
+  };
   page: number = 1;
   pageSize: number = PAGE_SIZE_DEFAULT;
   totalRecord: number = 10;
   setOfCheckedId = new Set<number>();
   dataTable: any[] = [];
-  dataExample: IBTCGiaoCacBoNganh[] = [
-    {
-      id: 1,
-      soQd: '1',
-      namQd: '2011',
-      ngayQd: new Date(),
-      trichYeu: 'Đây là trích yếu',
-      taiLieuDinhKem: 'Không có',
-      trangThai: 'Dự thảo',
-    },
-    {
-      id: 2,
-      soQd: '2',
-      namQd: '2012',
-      ngayQd: new Date(),
-      trichYeu: 'Đây là trích yếu',
-      taiLieuDinhKem: 'Không có',
-      trangThai: 'Dự thảo',
-    },
-    {
-      id: 3,
-      soQd: '4',
-      namQd: '2011',
-      ngayQd: new Date(),
-      trichYeu: 'Đây là trích yếu',
-      taiLieuDinhKem: 'Không có',
-      trangThai: 'Dự thảo',
-    },
-    {
-      id: 1,
-      soQd: '1',
-      namQd: '2011',
-      ngayQd: new Date(),
-      trichYeu: 'Đây là trích yếu',
-      taiLieuDinhKem: 'Không có',
-      trangThai: 'Dự thảo',
-    },
-    {
-      id: 1,
-      soQd: '1',
-      namQd: '2011',
-      ngayQd: new Date(),
-      trichYeu: 'Đây là trích yếu',
-      taiLieuDinhKem: 'Không có',
-      trangThai: 'Dự thảo',
-    },
-    {
-      id: 1,
-      soQd: '1',
-      namQd: '2011',
-      ngayQd: new Date(),
-      trichYeu: 'Đây là trích yếu',
-      taiLieuDinhKem: 'Không có',
-      trangThai: 'Dự thảo',
-    },
-    {
-      id: 1,
-      soQd: '1',
-      namQd: '2011',
-      ngayQd: new Date(),
-      trichYeu: 'Đây là trích yếu',
-      taiLieuDinhKem: 'Không có',
-      trangThai: 'Dự thảo',
-    },
-    {
-      id: 1,
-      soQd: '1',
-      namQd: '2011',
-      ngayQd: new Date(),
-      trichYeu: 'Đây là trích yếu',
-      taiLieuDinhKem: 'Không có',
-      trangThai: 'Dự thảo',
-    },
-  ];
+  dataTableAll: any[] = [];
 
-  constructor(private readonly fb: FormBuilder) {}
-
-  ngOnInit(): void {
-    this.initForm();
-    this.initData();
-    this.dataTable = [...this.dataExample];
-  }
-
-  initForm(): void {
+  constructor(private readonly fb: FormBuilder,
+    private quyetDinhBtcNganhService: QuyetDinhBtcNganhService,
+    private spinner: NgxSpinnerService,
+    private notification: NzNotificationService,
+    public userService: UserService,
+  ) {
     this.formData = this.fb.group({
       namQd: [null],
       soQd: [null],
-      ngayQd: [this.last30Day, this.toDay],
+      ngayQd: [[]],
       trichYeu: [null],
     });
   }
 
-  initData() {
+  async ngOnInit() {
     this.loadDsNam();
+    this.search();
+  }
+
+  initForm(): void {
+
+  }
+
+  initData() {
+
   }
 
   loadDsNam() {
@@ -136,10 +83,61 @@ export class BtcGiaoCacBoNganhComponent implements OnInit {
   clearFilter() {
     this.formData.reset();
   }
+  async search() {
+    this.spinner.show();
+    let body = this.formData.value;
+    body.tuNgay = body.ngayQd[0];
+    body.denNgay = body.ngayQd[1];
+    body.paggingReq = {
+      limit: this.pageSize,
+      page: this.page - 1,
 
-  xoa() {}
+    }
+    let res = await this.quyetDinhBtcNganhService.search(body);
+    if (res.msg == MESSAGE.SUCCESS) {
+      let data = res.data;
+      this.dataTable = data.content;
+      this.totalRecord = data.totalElements;
+      if (this.dataTable && this.dataTable.length > 0) {
+        this.dataTable.forEach((item) => {
+          item.checked = false;
+          // item.statusConvert = this.convertTrangThai(item.trangThai);
+        });
+      }
+      this.dataTableAll = cloneDeep(this.dataTable);
 
-  exportData() {}
+    } else {
+      this.dataTable = [];
+      this.totalRecord = 0;
+      this.notification.error(MESSAGE.ERROR, res.msg);
+    }
+    this.spinner.hide();
+  }
+
+  xoa() { }
+
+  exportData() {
+    if (this.totalRecord > 0) {
+      this.spinner.show();
+      try {
+        let body = this.formData.value;
+        body.tuNgay = body.ngayQd[0];
+        body.denNgay = body.ngayQd[1];
+        this.quyetDinhBtcNganhService
+          .export(body)
+          .subscribe((blob) =>
+            saveAs(blob, 'quyet-dinh-bo-tai-chinh-giao-bo-nganh.xlsx'),
+          );
+        this.spinner.hide();
+      } catch (e) {
+        console.log('error: ', e);
+        this.spinner.hide();
+        this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+      }
+    } else {
+      this.notification.error(MESSAGE.ERROR, MESSAGE.DATA_EMPTY);
+    }
+  }
 
   themMoi() {
     this.isAddNew = true;
@@ -176,21 +174,51 @@ export class BtcGiaoCacBoNganhComponent implements OnInit {
     this.isAddNew = false;
   }
 
-  changePageIndex(event) {}
+  changePageIndex(event) { }
 
-  changePageSize(event) {}
+  changePageSize(event) { }
 
-  viewDetail(id: number, isViewDetail: boolean) {}
+  viewDetail(id: number, isViewDetail: boolean) { }
 
-  xoaItem(id: number) {}
+  xoaItem(id: number) { }
+
+  filterInTable(key: string, value: string) {
+    if (value && value != '') {
+      this.dataTable = [];
+      let temp = [];
+      if (this.dataTableAll && this.dataTableAll.length > 0) {
+        this.dataTableAll.forEach((item) => {
+          if (item[key] && item[key].toString().toLowerCase().indexOf(value.toString().toLowerCase()) != -1) {
+            temp.push(item)
+          }
+        });
+      }
+      this.dataTable = [...this.dataTable, ...temp];
+    }
+    else {
+      this.dataTable = cloneDeep(this.dataTableAll);
+    }
+  }
+
+  clearFilterTable() {
+    this.filterTable = {
+      soQd: '',
+      namKhoach: '',
+      ngayQd: '',
+      trichYeu: '',
+      taiLieuDinhKem: '',
+      trangThai: '',
+    }
+  }
 }
 
-interface IBTCGiaoCacBoNganh {
-  id: number;
-  soQd: string;
-  namQd: string;
-  ngayQd: Date;
-  trichYeu: string;
-  taiLieuDinhKem: any;
-  trangThai: string;
-}
+
+// interface IBTCGiaoCacBoNganh {
+//   id: number;
+//   soQd: string;
+//   namQd: string;
+//   ngayQd: Date;
+//   trichYeu: string;
+//   taiLieuDinhKem: any;
+//   trangThai: string;
+// }
