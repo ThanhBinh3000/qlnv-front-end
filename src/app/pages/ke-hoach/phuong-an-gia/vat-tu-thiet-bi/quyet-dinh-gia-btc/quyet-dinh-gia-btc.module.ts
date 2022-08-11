941d797a116f3efcca27db846501a5c3b48e9ece
othoa <othoa>
2022-08-11 17:38:32 +0700

    add ui vat tu thiet bi ,phuong an gia

---
+++ src/app/pages/ke-hoach/phuong-an-gia/vat-tu-thiet-bi/quyet-dinh-gia-btc/quyet-dinh-gia-btc.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ComponentsModule } from 'src/app/components/components.module';
+import { QuyetDinhGiaBtcComponent } from './quyet-dinh-gia-btc.component';
+
+
+@NgModule({
+  declarations: [QuyetDinhGiaBtcComponent],
+  imports: [CommonModule,
+    ComponentsModule
+  ],
+  exports: [QuyetDinhGiaBtcComponent],
+})
+export class QuyetDinhGiaBtcModule { }
