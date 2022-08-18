beec83a3013ae4cd8e517cf5e162dd93d8eb4eed
nguyenminhduc2c19 <nguyenminhduc2c19@gmail.com>
2022-08-18 17:50:19 +0700

    update_UI_kiem_tra_chat_luong&&nhap_kho

--- src/app/pages/nhap/dau-thau/kehoach-luachon-nhathau/cuc/main-cuc/main-cuc.component.ts
+++ src/app/pages/nhap/dau-thau/kehoach-luachon-nhathau/cuc/main-cuc/main-cuc.component.ts
@@ -1,22 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { Globals } from 'src/app/shared/globals';
 @Component({
   selector: 'app-main-cuc',
   templateUrl: './main-cuc.component.html',
-  styleUrls: ['./main-cuc.component.scss']
+  styleUrls: ['./main-cuc.component.scss'],
 })
 export class MainCucComponent implements OnInit {
-
   @Input() loaiVthh: string;
-  constructor() { }
+  constructor(public globals: Globals) {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   tabSelected: number = 0;
   selectTab(tab: number) {
     this.tabSelected = tab;
   }
-
 }
