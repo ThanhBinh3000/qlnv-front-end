cab78e9f430d96f926c51a7ff0c66ae29906f7cd
Nguyen Trung Hieu <hieunt@techvify.com.vn>
2022-08-21 20:44:33 +0700

    Update Icon and required lokho

--- src/app/pages/luu-kho/quan-ly-so-the-kho/hang-theo-doi-dac-thu/hang-thuoc-dien-tieu-huy/them-hang-thuoc-dien-tieu-huy/them-hang-thuoc-dien-tieu-huy.component.ts
+++ src/app/pages/luu-kho/quan-ly-so-the-kho/hang-theo-doi-dac-thu/hang-thuoc-dien-tieu-huy/them-hang-thuoc-dien-tieu-huy/them-hang-thuoc-dien-tieu-huy.component.ts
@@ -273,6 +273,10 @@ export class ThemHangThuocDienTieuHuyComponent implements OnInit {
     }
 
     themMoiItem() {
+        if (!this.rowItem.idLoKho) {
+            this.notification.error(MESSAGE.ERROR_NOT_EMPTY, 'Vui lòng chọn lô kho.');
+            return;
+        }
         const newItem = {
             lyDo: this.rowItem.lyDo,
             chungLoaiHang: this.dsChungLoaiHangHoa.find((item) => item.ma == this.rowItem.chungLoaiHangHoa).ten,
