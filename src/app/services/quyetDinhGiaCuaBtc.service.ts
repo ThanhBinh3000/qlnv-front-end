5b89d0c4744cc9689bee6e498f16fcf5fcb743ab
VuNT <vunt@ttkd6.com>
2022-08-22 16:06:22 +0700

    fix import

--- src/app/services/quyetDinhGiaCuaBtc.service.ts
+++ src/app/services/quyetDinhGiaCuaBtc.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BaseService } from './base.service.local';
+import { BaseService } from './base.service';
+
 
 @Injectable({
   providedIn: 'root'
