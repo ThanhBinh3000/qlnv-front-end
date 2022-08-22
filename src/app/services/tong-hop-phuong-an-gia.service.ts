58f849e189191bec1c82f6f778208019b31f7935
ta quang chien <tqchien1990@gmail.com>
2022-08-22 10:59:56 +0700

    màn hình quyết định giá của bộ tài chính upadte thêm api lấy thông tin tờ trình đề xuất

--- src/app/services/tong-hop-phuong-an-gia.service.ts
+++ src/app/services/tong-hop-phuong-an-gia.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BaseService } from './base.service';
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,14 @@ export class TongHopPhuongAnGiaService extends BaseService {
     super(httpClient, 'kh-lt-pag/luong-thuc/gia-lh/tong-hop', '/qlnv-khoach')
 
   }
+  loadToTrinhDeXuat(body: any) {
+    const url = `${environment.SERVICE_API}${this.gateway}/kh-lt-pag/luong-thuc/gia-lh/tong-hop/danh-sach`;
+    return this.httpClient.post<any>(url, body).toPromise();
 
+  }
+  loadToTrinhDeXuatThongTinGia(id: number) {
+    const url = `${environment.SERVICE_API}${this.gateway}/kh-lt-pag/luong-thuc/gia-lh/tong-hop/danh-sach/chi-tiet/${id}`;
+    return this.httpClient.get<any>(url).toPromise()
+
+  }
 }
