192e866f32218bc5fc9e923c45d5887743f8bd5f
BinhTA <binhta@ttkd6.com>
2022-08-23 13:47:07 +0700

    tong hop phuong an gia

--- src/app/services/tong-hop-phuong-an-gia.service.ts
+++ src/app/services/tong-hop-phuong-an-gia.service.ts
@@ -10,23 +10,23 @@ export class TongHopPhuongAnGiaService extends BaseService {
   gateway: string = "/qlnv-khoach";
 
   constructor(public httpClient: HttpClient) {
-    super(httpClient, "kh-lt-pag/luong-thuc/gia-lh/tong-hop", "/qlnv-khoach");
+    super(httpClient, "phuong-an-gia/tong-hop", "/qlnv-khoach");
 
   }
 
   loadToTrinhDeXuat(body: any) {
-    const url = `${environment.SERVICE_API}${this.gateway}/kh-lt-pag/luong-thuc/gia-lh/tong-hop/danh-sach`;
+    const url = `${environment.SERVICE_API}${this.gateway}/phuong-an-gia/tong-hop/danh-sach`;
     return this.httpClient.post<any>(url, body).toPromise();
 
   }
 
   loadToTrinhDeXuatThongTinGia(id: number) {
-    const url = `${environment.SERVICE_API}${this.GATEWAY}/kh-lt-pag/luong-thuc/gia-lh/tong-hop/danh-sach/chi-tiet/${id}`;
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/phuong-an-gia/tong-hop/danh-sach/chi-tiet/${id}`;
     return this.httpClient.get<any>(url).toPromise();
   }
 
   tongHop(body: any): Promise<any> {
-    const url = `${environment.SERVICE_API}${this.GATEWAY}/kh-lt-pag/luong-thuc/gia-lh/tong-hop`;
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/phuong-an-gia/tong-hop`;
     return this.httpClient.post(url, body).toPromise();
   }
 }
