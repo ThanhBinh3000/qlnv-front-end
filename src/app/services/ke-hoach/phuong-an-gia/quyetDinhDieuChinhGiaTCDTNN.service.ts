d5392039bc11002aeee8082368565aa3befdc6f5
ta quang chien <tqchien1990@gmail.com>
2022-08-30 16:00:30 +0700

    update quyet dinh dieu chinh

--- src/app/services/ke-hoach/phuong-an-gia/quyetDinhDieuChinhGiaTCDTNN.service.ts
+++ src/app/services/ke-hoach/phuong-an-gia/quyetDinhDieuChinhGiaTCDTNN.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import {BaseService} from "./base.service";
+import {BaseService} from "../../base.service";
 import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
-export class QuyetDinhDcPagTcdtnnService extends BaseService{
+export class QuyetDinhDieuChinhGiaTCDTNNService extends BaseService{
 
   gateway: string = '/qlnv-category'
   constructor(public httpClient: HttpClient) {
