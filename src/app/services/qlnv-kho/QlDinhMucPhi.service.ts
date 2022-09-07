4438da50f7b419888af1c0aac802d7cf349aba36
othoa <othoa>
2022-09-07 16:45:11 +0700

    fix Bug #2610

--- src/app/services/qlnv-kho/QlDinhMucPhi.service.ts
+++ src/app/services/qlnv-kho/QlDinhMucPhi.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
 import { BaseService } from '../base.service';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
