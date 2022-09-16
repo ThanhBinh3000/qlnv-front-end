6e0214609414ca322dcceb2354a66ee721729078
honghv <a366ngay@gmail.com>
2022-09-17 00:08:01 +0700

    update module cuu tro

--- src/app/pages/xuat/cuu-tro-vien-tro/tong-hop-phuong-an/tong-hop-phuong-an.module.ts
+++ src/app/pages/xuat/cuu-tro-vien-tro/tong-hop-phuong-an/tong-hop-phuong-an.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { DirectivesModule } from 'src/app/directives/directives.module';
 import { TongHopPhuongAnComponent } from './tong-hop-phuong-an.component';
 import { ThongTinTongHopPhuongAnComponent } from './thong-tin-tong-hop-phuong-an/thong-tin-tong-hop-phuong-an.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule,
+    DirectivesModule,
   ],
   declarations: [
     TongHopPhuongAnComponent,
