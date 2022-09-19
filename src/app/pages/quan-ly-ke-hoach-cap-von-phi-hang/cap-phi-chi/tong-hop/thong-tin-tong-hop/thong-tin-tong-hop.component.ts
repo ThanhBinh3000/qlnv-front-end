fa51801541be01e2b8de49d90c60559d5fbdf7c2
thanhnm <vuibinhyen2003@gmail.com>
2022-09-19 11:56:12 +0700

    update man-hinh-cap-phi-chi

---
+++ src/app/pages/quan-ly-ke-hoach-cap-von-phi-hang/cap-phi-chi/tong-hop/thong-tin-tong-hop/thong-tin-tong-hop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thong-tin-tong-hop',
+  templateUrl: './thong-tin-tong-hop.component.html',
+  styleUrls: ['./thong-tin-tong-hop.component.scss']
+})
+export class ThongTinTongHopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
