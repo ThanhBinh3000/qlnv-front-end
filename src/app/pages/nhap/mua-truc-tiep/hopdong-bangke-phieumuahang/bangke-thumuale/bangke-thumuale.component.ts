0a5b2ca74ca308bed3d441f9a30c109012d2aa30
BinhTA <binhta@ttkd6.com>
2022-09-28 11:54:01 +0700

    hop dong bang ke, dieu chinh

---
+++ src/app/pages/nhap/mua-truc-tiep/hopdong-bangke-phieumuahang/bangke-thumuale/bangke-thumuale.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bangke-thumuale',
+  templateUrl: './bangke-thumuale.component.html',
+  styleUrls: ['./bangke-thumuale.component.scss']
+})
+export class BangkeThumualeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
