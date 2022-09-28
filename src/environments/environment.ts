f43eb519fdbfef83300582b7b8c3afacebde159d
hoangth <hoangth@hoangth.me>
2022-09-28 18:24:06 +0700

    merge code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  SERVICE_API: 'http://192.168.1.118:8888',
-  AUTHEN_API: 'http://192.168.1.118:8888',
+  AUTHEN_API: 'http://192.168.1.80:8888',
+  SERVICE_API: 'http://192.168.1.80:8888',
+  LOCAL: 'http://192.168.5.197:189'
 };
 
 /*
