46495abbfe7e1133d9d21e98c93e51f4afec6044
BinhTA <binhta@ttkd6.com>
2022-10-19 15:12:02 +0700

    hợp đồng/bảng kê, phiếu mua hàng

---
+++ src/app/services/hopdong-phuluc-hopdong.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BaseService } from './base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HopdongPhulucHopdongService extends BaseService {
+  GATEWAY = '/qlnv-hang';
+
+  constructor(public httpClient: HttpClient) {
+    super(httpClient, 'mua-truc-tiep/hd-bk-pmh', '/qlnv-hang');
+  }
+
+
+}
