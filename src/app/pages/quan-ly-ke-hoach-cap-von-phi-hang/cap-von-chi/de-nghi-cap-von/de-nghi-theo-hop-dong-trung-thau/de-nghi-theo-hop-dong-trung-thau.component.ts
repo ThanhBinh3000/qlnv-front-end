1edc7acee531f6d6406edc69dd5cffc4f107d9cb
hoangth <hoangth@hoangth.me>
2022-11-19 18:44:11 +0700

    fix trang thai 2.1

--- src/app/pages/quan-ly-ke-hoach-cap-von-phi-hang/cap-von-chi/de-nghi-cap-von/de-nghi-theo-hop-dong-trung-thau/de-nghi-theo-hop-dong-trung-thau.component.ts
+++ src/app/pages/quan-ly-ke-hoach-cap-von-phi-hang/cap-von-chi/de-nghi-cap-von/de-nghi-theo-hop-dong-trung-thau/de-nghi-theo-hop-dong-trung-thau.component.ts
@@ -176,6 +176,10 @@ export class DeNghiTheoHopDongTrungThauComponent implements OnInit {
         this.spinner.hide();
     }
 
+    getStatusName() {
+        return this.trangThais.find(e => e.id == this.trangThai)?.tenDm;
+    }
+
     async initialization() {
         //lay id cua de nghi
         this.id = this.data?.id;
