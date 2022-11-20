fed1d1efe2df51567425763f3fbb9885e8d11205
datpd <duydat246@gmail.com>
2022-11-20 09:15:01 +0700

    fix: dieu chinh + menu 1.6

--- src/app/pages/ke-hoach/du-toan-nsnn/giao-du-toan/giao-du-toan.module.ts
+++ src/app/pages/ke-hoach/du-toan-nsnn/giao-du-toan/giao-du-toan.module.ts
@@ -2,15 +2,36 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { DirectivesModule } from 'src/app/directives/directives.module';
+import { BaoCaoTuDonViCapDuoiModule } from './bao-cao-tu-don-vi-cap-duoi/bao-cao-tu-don-vi-cap-duoi.module';
+import { DialogTaoMoiComponent } from './dialog-tao-moi/dialog-tao-moi.component';
+import { DuToanGiaoTuCapTrenComponent } from './du-toan-giao-tu-cap-tren/du-toan-giao-tu-cap-tren.component';
 import { GiaoDuToanComponent } from './giao-du-toan.component';
+import { PhanBoDuToanComponent } from './phan-bo-du-toan/phan-bo-du-toan.component';
+import { QuyetDinhBoTaiChinhComponent } from './quyet-dinh-bo-tai-chinh/quyet-dinh-bo-tai-chinh.component';
+import { TaoMoiQuyetDinhBtcComponent } from './tao-moi-quyet-dinh-btc/tao-moi-quyet-dinh-btc.component';
 
 @NgModule({
-  declarations: [GiaoDuToanComponent],
+  declarations: [
+    GiaoDuToanComponent,
+    DuToanGiaoTuCapTrenComponent,
+    PhanBoDuToanComponent,
+    QuyetDinhBoTaiChinhComponent,
+    DialogTaoMoiComponent,
+    TaoMoiQuyetDinhBtcComponent,
+  ],
   imports: [
     CommonModule,
     ComponentsModule,
     DirectivesModule,
+    BaoCaoTuDonViCapDuoiModule,
   ],
-  exports: [GiaoDuToanComponent],
+  exports: [
+    GiaoDuToanComponent,
+    DuToanGiaoTuCapTrenComponent,
+    PhanBoDuToanComponent,
+    QuyetDinhBoTaiChinhComponent,
+    DialogTaoMoiComponent,
+    TaoMoiQuyetDinhBtcComponent,
+  ],
 })
 export class GiaoDuToanModule { }
--- src/app/pages/ke-hoach/du-toan-nsnn/giao-du-toan/quyet-dinh-bo-tai-chinh/quyet-dinh-bo-tai-chinh.component.ts
+++ src/app/pages/ke-hoach/du-toan-nsnn/giao-du-toan/quyet-dinh-bo-tai-chinh/quyet-dinh-bo-tai-chinh.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { GDT, Utils } from 'src/app/Utility/utils';
+import { cloneDeep } from 'lodash';
+import { UserService } from 'src/app/services/user.service';
+import { GiaoDuToanChiService } from 'src/app/services/quan-ly-von-phi/giaoDuToanChi.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { MESSAGE } from 'src/app/constants/message';
+import { DialogTaoMoiComponent } from '../dialog-tao-moi/dialog-tao-moi.component';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { MESSAGEVALIDATE } from 'src/app/constants/messageValidate';
+
 
 @Component({
   selector: 'app-quyet-dinh-bo-tai-chinh',
@@ -6,10 +18,271 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quyet-dinh-bo-tai-chinh.component.scss']
 })
 export class QuyetDinhBoTaiChinhComponent implements OnInit {
+  @Output() dataChange = new EventEmitter();
 
-  constructor() { }
+  searchFilter = {
+    dotBcao: null,
+    namPa: null,
+    ngayTaoTu: null,
+    ngayTaoDen: null,
+    maPa: "",
+    trangThai: Utils.TT_BC_1,
+  };
+
+  filterTable: any = {
+    maPa: '',
+    ngayTaoTu: '',
+    namPa: '',
+  };
+
+  pages = {
+    size: 10,
+    page: 1,
+  };
+
+
+  statusBtnValidateNam = true;
+  statusDelete = true;
+  statusNewReport = true;
+  allChecked = false;
+
+  userInfo: any;
+  totalPages = 0;
+  totalElements = 0;
+  dataTable: any[] = [];
+  dataTableAll: any[] = [];
+  listIdDelete: string[] = [];
+
+
+
+  constructor(
+    private datePipe: DatePipe,
+    private userService: UserService,
+    private giaoDuToanChiService: GiaoDuToanChiService,
+    private spinner: NgxSpinnerService,
+    private notification: NzNotificationService,
+    private modal: NzModalService,
+  ) { }
 
   ngOnInit() {
+    this.searchFilter.ngayTaoTu = new Date();
+    const newDate = new Date();
+    newDate.setMonth(newDate.getMonth() - 1);
+    this.searchFilter.ngayTaoDen = newDate;
+
+    this.userInfo = this.userService.getUserLogin();
+    this.search();
+    // this.searchFilter.donViTao = this.userInfo?.MA_DVI;
+  };
+
+  clearFilter() {
+    this.searchFilter.namPa = null;
+    this.searchFilter.ngayTaoTu = null;
+    this.searchFilter.ngayTaoTu = null;
+    this.searchFilter.maPa = null;
+    this.search();
+  };
+
+  async search() {
+
+    if (this.searchFilter.namPa || this.searchFilter.namPa === 0) {
+      if (this.searchFilter.namPa >= 3000 || this.searchFilter.namPa < 1000) {
+        this.notification.warning(MESSAGE.WARNING, MESSAGEVALIDATE.WRONG_FORMAT);
+        return;
+      }
+    }
+    let trangThais = ['1', '2', '3', '4', '5', '7', '8', '9'];
+    if (this.searchFilter.trangThai) {
+      trangThais = [this.searchFilter.trangThai];
+    }
+    const requestReport = {
+      maPhanGiao: '1',
+      maLoai: '2',
+      maPa: this.searchFilter.maPa,
+      namPa: this.searchFilter.namPa,
+      ngayTaoDen: this.datePipe.transform(this.searchFilter.ngayTaoDen, Utils.FORMAT_DATE_STR),
+      ngayTaoTu: this.datePipe.transform(this.searchFilter.ngayTaoTu, Utils.FORMAT_DATE_STR),
+      paggingReq: {
+        limit: this.pages.size,
+        page: this.pages.page,
+      },
+      trangThais: trangThais,
+      trangThaiGiaos: [],
+    };
+    this.spinner.show();
+    //let latest_date =this.datepipe.transform(this.tuNgay, 'yyyy-MM-dd');
+    await this.giaoDuToanChiService.timBaoCaoGiao(requestReport).toPromise().then(
+      (data) => {
+        if (data.statusCode == 0) {
+          this.dataTable = [];
+          data.data.content.forEach(item => {
+            if (this.listIdDelete.findIndex(e => e == item.id) == -1) {
+              this.dataTable.push({
+                ...item,
+                checked: false,
+                isEdit: this.checkEditStatus(item.trangThai),
+                isDelete: this.checkDeleteStatus(item.trangThai),
+              })
+            } else {
+              this.dataTable.push({
+                ...item,
+                checked: true,
+              })
+            }
+          })
+          this.dataTable.forEach(e => {
+            e.ngayTao = this.datePipe.transform(e.ngayTao, Utils.FORMAT_DATE_STR);
+            e.ngayTrinh = this.datePipe.transform(e.ngayTrinh, Utils.FORMAT_DATE_STR);
+            e.ngayDuyet = this.datePipe.transform(e.ngayDuyet, Utils.FORMAT_DATE_STR);
+            e.ngayPheDuyet = this.datePipe.transform(e.ngayPheDuyet, Utils.FORMAT_DATE_STR);
+            e.ngayTraKq = this.datePipe.transform(e.ngayTraKq, Utils.FORMAT_DATE_STR);
+          })
+          this.dataTableAll = cloneDeep(this.dataTable);
+          this.totalElements = data.data.totalElements;
+          this.totalPages = data.data.totalPages;
+        } else {
+          this.notification.error(MESSAGE.ERROR, MESSAGE.ERROR_CALL_SERVICE);
+        }
+      },
+      (err) => {
+        this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+      }
+    );
+    this.spinner.hide();
+  };
+
+  deleteReport(id: string) {
+    let request = [];
+    if (!id) {
+      request = this.listIdDelete;
+    } else {
+      request = [id];
+    }
+    this.spinner.show();
+    this.giaoDuToanChiService.xoaBanGhiGiaoBTC(request).toPromise().then(
+      data => {
+        if (data.statusCode == 0) {
+          this.listIdDelete = [];
+          this.notification.success(MESSAGE.SUCCESS, MESSAGE.DELETE_SUCCESS);
+          this.search();
+        } else {
+          this.notification.error(MESSAGE.ERROR, data?.msg);
+        }
+      },
+      err => {
+        this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+      }
+    )
+    this.spinner.hide();
+  };
+
+  addNewReport() {
+    const modalTuChoi = this.modal.create({
+      nzTitle: 'Thông tin tạo mới báo cáo điều chỉnh dự toán chi ngân sách nhà nước',
+      nzContent: DialogTaoMoiComponent,
+      nzMaskClosable: false,
+      nzClosable: false,
+      nzWidth: '900px',
+      nzFooter: null,
+      nzComponentParams: {
+      },
+    });
+    modalTuChoi.afterClose.toPromise().then(async (res) => {
+      if (res) {
+        const obj = {
+          ...res,
+          id: null,
+          tabSelected: 'quyetDinhBTC',
+          isSynthetic: false,
+        }
+        this.dataChange.emit(obj);
+      }
+    });
+  };
+
+  //xem chi tiet bao cao
+  viewDetail(data: any) {
+    const obj = {
+      id: data.id,
+      tabSelected: 'quyetDinhBTC',
+    }
+    this.dataChange.emit(obj);
+  };
+
+  // Tìm kiếm trong bảng
+  filterInTable(key: string, value: string, isDate: boolean) {
+    if (value && value != '') {
+      this.dataTable = [];
+      let temp = [];
+      if (this.dataTableAll && this.dataTableAll.length > 0) {
+        if (isDate) {
+          value = this.datePipe.transform(value, Utils.FORMAT_DATE_STR);
+        }
+        this.dataTableAll.forEach((item) => {
+          if (item[key] && item[key].toString().toLowerCase().indexOf(value.toString().toLowerCase()) != -1) {
+            temp.push(item)
+          }
+        });
+      }
+      this.dataTable = [...this.dataTable, ...temp];
+    } else {
+      this.dataTable = cloneDeep(this.dataTableAll);
+    }
+  };
+
+
+  updateSingleChecked(): void {
+    if (this.dataTable.every((item) => !item.checked && item.isDelete)) {
+      this.allChecked = false;
+    } else if (this.dataTable.every((item) => item.checked && item.isDelete)) {
+      this.allChecked = true;
+    }
+  };
+
+  updateAllChecked(): void {
+    if (this.dataTable && this.dataTable.length > 0) {
+      if (this.allChecked) {
+        this.dataTable.forEach(item => {
+          if (item.isDelete) {
+            item.checked = true;
+          }
+        })
+      } else {
+        this.dataTable.forEach(item => {
+          if (item.isDelete) {
+            item.checked = false;
+          }
+        })
+      }
+    }
+  };
+
+  changeListIdDelete(id: any) {
+    if (this.listIdDelete.findIndex(e => e == id) == -1) {
+      this.listIdDelete.push(id);
+    } else {
+      this.listIdDelete = this.listIdDelete.filter(e => e != id);
+    }
   }
 
+  onPageIndexChange(page) {
+    this.pages.page = page;
+    this.search();
+  };
+
+  //doi so luong phan tu tren 1 trang
+  onPageSizeChange(size) {
+    this.pages.size = size;
+    this.search();
+  };
+
+  checkEditStatus(trangThai: string) {
+    return Utils.statusSave.includes(trangThai) &&
+      (this.userInfo.CAP_DVI == '1' && this.userService.isAccessPermisson(GDT.EDIT_REPORT_BTC));
+  };
+
+  checkDeleteStatus(trangThai: string) {
+    return Utils.statusDelete.includes(trangThai) &&
+      (this.userInfo.CAP_DVI == '1' && this.userService.isAccessPermisson(GDT.DELETE_REPORT_BTC));
+  };
 }
