7de601000a8b523338e123e97bbbb8a43bfe2f67
VuNT <vunt@ttkd6.com>
2022-11-24 09:01:07 +0700

    fix dx vat tu

--- src/app/services/qlnv-hang/nhap-hang/dau-thau/tochuc-trienkhai/thongTinDauThau.service.ts
+++ src/app/services/qlnv-hang/nhap-hang/dau-thau/tochuc-trienkhai/thongTinDauThau.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BaseService } from '../../../../base.service';
+import { environment } from 'src/environments/environment';
+import { OldResponseData } from 'src/app/interfaces/response';
 
 @Injectable({
   providedIn: 'root',
@@ -12,4 +14,8 @@ export class ThongTinDauThauService extends BaseService {
     super(httpClient, 'dx-kh/ttin-dthau', '/qlnv-hang');
   }
 
+  getDetailThongTin(id, loaiVthh): Promise<OldResponseData> {
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/chi-tiet/${id}/${loaiVthh}`;
+    return this._httpClient.get<OldResponseData>(url).toPromise();
+  }
 }
