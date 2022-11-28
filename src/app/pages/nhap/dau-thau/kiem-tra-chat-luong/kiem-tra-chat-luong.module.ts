3c845eb524bf461b54d8577be709c325d354682e
thongtv <thongtv@tecapro.com>
2022-11-28 11:17:09 +0700

    hồ sơ kỹ thuật

--- src/app/pages/nhap/dau-thau/kiem-tra-chat-luong/kiem-tra-chat-luong.module.ts
+++ src/app/pages/nhap/dau-thau/kiem-tra-chat-luong/kiem-tra-chat-luong.module.ts
@@ -20,7 +20,8 @@ import { QuanLyPhieuKiemNghiemChatLuongComponent } from './quan-ly-phieu-kiem-ng
 import { ThemMoiPhieuKiemNghiemChatLuongComponent } from './quan-ly-phieu-kiem-nghiem-chat-luong/them-moi-phieu-kiem-nghiem-chat-luong/them-moi-phieu-kiem-nghiem-chat-luong.component';
 import { ThanhphanLaymauComponent } from './quan-ly-bien-ban-lay-mau/them-moi-bien-ban-lay-mau/thanhphan-laymau/thanhphan-laymau.component';
 import { ThongtinThuchienComponent } from './bien-ban-chuan-bi-kho/thong-tin-bien-ban-chuan-bi-kho/thongtin-thuchien/thongtin-thuchien.component';
-import { ThemMoHoSoKyThuatComponent } from './ho-so-ky-thuat/them-mo-ho-so-ky-thuat/them-mo-ho-so-ky-thuat.component';
+import { ThemMoiHoSoKyThuatComponent } from './ho-so-ky-thuat/them-moi-ho-so-ky-thuat/them-moi-ho-so-ky-thuat.component';
+
 
 @NgModule({
   declarations: [
@@ -42,7 +43,7 @@ import { ThemMoHoSoKyThuatComponent } from './ho-so-ky-thuat/them-mo-ho-so-ky-th
     ThemMoiPhieuKiemNghiemChatLuongComponent,
     ThanhphanLaymauComponent,
     ThongtinThuchienComponent,
-    ThemMoHoSoKyThuatComponent,
+    ThemMoiHoSoKyThuatComponent,
   ],
   imports: [
     CommonModule,
