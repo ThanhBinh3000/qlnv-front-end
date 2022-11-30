1afd59e6095b793b7d661ebfa09f23086b9c430e
othoa <othoa>
2022-11-30 14:08:50 +0700

    add vaitro and phongban to user

--- src/app/models/userlogin.ts
+++ src/app/models/userlogin.ts
@@ -10,7 +10,9 @@ export class UserLogin {
   MA_TCKT: string;
   MA_TR: string;
   DON_VI: any;
-  TEN_DAY_DU: any
+  TEN_DAY_DU: any;
+  VAI_TRO: string;
+  PHONG_BAN: string
   constructor(initObj: any, dvql: any) {
     if (initObj) {
       for (var key in initObj) {
