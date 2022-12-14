be318440f2015f7100a89388ac2b48b877ad2f1b
othoa <othoa>
2022-12-14 18:20:55 +0700

    add menu tổng hợp, phê duyệt - Quyet toan-von phi

---
+++ src/app/pages/quan-ly-thong-tin-quyet-toan-von-phi-hang-dtqg/tonghop-pheduyet/qd-pd-quyet-toan-btc/qd-pd-quyet-toan-btc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qd-pd-quyet-toan-btc',
+  templateUrl: './qd-pd-quyet-toan-btc.component.html',
+  styleUrls: ['./qd-pd-quyet-toan-btc.component.scss']
+})
+export class QdPdQuyetToanBtcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/quan-ly-thong-tin-quyet-toan-von-phi-hang-dtqg/tonghop-pheduyet/tonghop-pheduyet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tonghop-pheduyet',
+  templateUrl: './tonghop-pheduyet.component.html',
+  styleUrls: ['./tonghop-pheduyet.component.scss']
+})
+export class TonghopPheduyetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
