4c6fdd218e7af0f05c9994d2d0bb874f32d7fd21
othoa <othoa>
2022-12-14 18:38:47 +0700

    add menu tổng hợp, phê duyệt - Quyet toan-von phi

--- src/app/pages/quan-ly-thong-tin-quyet-toan-von-phi-hang-dtqg/tonghop-pheduyet/tonghop-pheduyet.component.ts
+++ src/app/pages/quan-ly-thong-tin-quyet-toan-von-phi-hang-dtqg/tonghop-pheduyet/tonghop-pheduyet.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Subject} from "rxjs";
+import {UserService} from "../../../services/user.service";
+import {Globals} from "../../../shared/globals";
 
 @Component({
   selector: 'app-tonghop-pheduyet',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tonghop-pheduyet.component.scss']
 })
 export class TonghopPheduyetComponent implements OnInit {
+  isVisibleChangeTab$ = new Subject();
+  visibleTab: boolean = true;
 
-  constructor() { }
+  constructor(public userService: UserService,
+              public globals: Globals) {
+  }
 
   ngOnInit(): void {
+    this.isVisibleChangeTab$.subscribe((value: boolean) => {
+      this.visibleTab = value;
+    });
+  }
+
+  tabSelected: number = 0;
+
+  selectTab(tab: number) {
+    this.tabSelected = tab;
   }
 
 }
