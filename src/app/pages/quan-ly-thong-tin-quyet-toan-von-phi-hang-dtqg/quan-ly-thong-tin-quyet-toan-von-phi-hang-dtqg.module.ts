9f7087b071f9fff3ee2c267116175c624b197340
othoa <othoa>
2022-12-14 18:53:12 +0700

    add menu tổng hợp, phê duyệt - Quyet toan-von phi

--- src/app/pages/quan-ly-thong-tin-quyet-toan-von-phi-hang-dtqg/quan-ly-thong-tin-quyet-toan-von-phi-hang-dtqg.module.ts
+++ src/app/pages/quan-ly-thong-tin-quyet-toan-von-phi-hang-dtqg/quan-ly-thong-tin-quyet-toan-von-phi-hang-dtqg.module.ts
@@ -7,10 +7,9 @@ import { NzAffixModule } from 'ng-zorro-antd/affix';
 import { QuanLyThongTinQuyetToanVonPhiHangDtqgComponent } from './quan-ly-thong-tin-quyet-toan-von-phi-hang-dtqg.component';
 import { QuanLyThongTinQuyetToanVonPhiHangDtqgRoutingModule } from './quan-ly-thong-tin-quyet-toan-von-phi-hang-dtqg-routing.module';
 import { TonghopPheduyetComponent } from './tonghop-pheduyet/tonghop-pheduyet.component';
-import { DuLieuTongHopTcdtComponent } from './tonghop-pheduyet/du-lieu-tong-hop-tcdt/du-lieu-tong-hop-tcdt.component';
 import {TonghopPheduyetModule} from "./tonghop-pheduyet/tonghop-pheduyet.module";
 @NgModule({
-  declarations: [QuanLyThongTinQuyetToanVonPhiHangDtqgComponent, TonghopPheduyetComponent, DuLieuTongHopTcdtComponent],
+  declarations: [QuanLyThongTinQuyetToanVonPhiHangDtqgComponent,TonghopPheduyetComponent],
   imports: [
     CommonModule,
     QuanLyThongTinQuyetToanVonPhiHangDtqgRoutingModule,
