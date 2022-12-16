32041fa2a1ce1c20961563fa256854e8b691e2f5
VuNT <vunt@ttkd6.com>
2022-12-16 23:44:25 +0700

    fix base

--- src/app/pages/nhap/dau-thau/dieu-chinh/dieuchinh-luachon-nhathau/themmoi-dieuchinh/themmoi-dieuchinh.component.ts
+++ src/app/pages/nhap/dau-thau/dieu-chinh/dieuchinh-luachon-nhathau/themmoi-dieuchinh/themmoi-dieuchinh.component.ts
@@ -1,26 +1,17 @@
 import { Component, Input, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Validators } from '@angular/forms';
 import dayjs from 'dayjs';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { DialogCanCuQDPheDuyetKHLCNTComponent } from 'src/app/components/dialog/dialog-can-cu-qd-phe-duyet-khlcnt/dialog-can-cu-qd-phe-duyet-khlcnt.component';
-import { DialogDanhSachHangHoaComponent } from 'src/app/components/dialog/dialog-danh-sach-hang-hoa/dialog-danh-sach-hang-hoa.component';
 import { DialogTableSelectionComponent } from 'src/app/components/dialog/dialog-table-selection/dialog-table-selection.component';
 import { DialogThemMoiGoiThauComponent } from 'src/app/components/dialog/dialog-them-moi-goi-thau/dialog-them-moi-goi-thau.component';
 import { DialogThongTinPhuLucQuyetDinhPheDuyetComponent } from 'src/app/components/dialog/dialog-thong-tin-phu-luc-quyet-dinh-phe-duyet/dialog-thong-tin-phu-luc-quyet-dinh-phe-duyet.component';
-import { DialogTuChoiComponent } from 'src/app/components/dialog/dialog-tu-choi/dialog-tu-choi.component';
-import { DATEPICKER_CONFIG } from 'src/app/constants/config';
 import { MESSAGE } from 'src/app/constants/message';
-import { UserLogin } from 'src/app/models/userlogin';
 import { DanhMucService } from 'src/app/services/danhmuc.service';
 import { dauThauGoiThauService } from 'src/app/services/qlnv-hang/nhap-hang/dau-thau/tochuc-trienkhai/dauThauGoiThau.service';
-import { HelperService } from 'src/app/services/helper.service';
 import { DieuChinhQuyetDinhPdKhlcntService } from 'src/app/services/qlnv-hang/nhap-hang/dau-thau/dieuchinh-khlcnt/dieuChinhQuyetDinhPdKhlcnt.service';
 import { QuyetDinhPheDuyetKeHoachLCNTService } from 'src/app/services/qlnv-hang/nhap-hang/dau-thau/kehoach-lcnt/quyetDinhPheDuyetKeHoachLCNT.service';
-import { UserService } from 'src/app/services/user.service';
-import { Globals } from 'src/app/shared/globals';
 import { STATUS } from "../../../../../../constants/status";
 import { cloneDeep } from "lodash";
 import { ThongtinDieuchinhComponent } from './thongtin-dieuchinh/thongtin-dieuchinh.component';
@@ -88,24 +79,15 @@ export class ThemMoiDieuChinhComponent extends Base2Component implements OnInit 
     });
   }
   maQd: string = '';
-  isVatTu: boolean = false;
   listQdGoc: any[] = [];
-  listNthauNopHs: any[] = [];
-  listDiaDiemNhapHang: any[] = [];
-  editCache: { [key: string]: { edit: boolean; data: any } } = {};
-  editDiaDiemCache: { [key: string]: { edit: boolean; data: any } } = {};
-  i = 0;
   listNguonVon: any[] = []
   listPhuongThucDauThau: any[] = []
   listHinhThucDauThau: any[] = []
   listLoaiHopDong: any[] = []
-  listVthh: any[] = [];
-
 
   isDetail = false;
   dataInput: any;
   dataInputCache: any;
-
 
   async ngOnInit() {
     this.spinner.show();
@@ -131,6 +113,7 @@ export class ThemMoiDieuChinhComponent extends Base2Component implements OnInit 
   }
 
   async loadDataComboBox() {
+    this.spinner.show();
     // List nguồn vốn
     this.listNguonVon = [];
     let resNv = await this.danhMucService.danhMucChungGetAll('NGUON_VON');
@@ -155,6 +138,7 @@ export class ThemMoiDieuChinhComponent extends Base2Component implements OnInit 
     if (resHd.msg == MESSAGE.SUCCESS) {
       this.listLoaiHopDong = resHd.data;
     }
+    this.spinner.hide();
   }
 
   initForm() {
@@ -166,7 +150,6 @@ export class ThemMoiDieuChinhComponent extends Base2Component implements OnInit 
   async getDetail() {
     if (this.id > 0) {
       let data = await this.detail(this.id);
-      console.log(data);
       if (data) {
         this.formData.patchValue({
           soQdDc: data.soQdDc?.split("/")[0],
@@ -219,7 +202,7 @@ export class ThemMoiDieuChinhComponent extends Base2Component implements OnInit 
     this.spinner.show();
     if ($event) {
       let res = await this.quyetDinhPheDuyetKeHoachLCNTService.getDetail($event);
-      let qdGoc = this.listQdGoc.filter(item => item.id == $event)
+      // let qdGoc = this.listQdGoc.filter(item => item.id == $event)
       if (res.msg == MESSAGE.SUCCESS) {
         const data = res.data;
         this.dataInput = data;
@@ -230,7 +213,7 @@ export class ThemMoiDieuChinhComponent extends Base2Component implements OnInit 
           ghiChu: data.ghiChu,
           trichYeu: data.trichYeu,
           idQdGoc: $event,
-          soQdGoc: qdGoc.length > 0 ? qdGoc[0].soQd : null,
+          soQdGoc: data.soQd,
           ngayQdGoc: data.ngayQd
         })
         if (data.loaiVthh.startsWith('02')) {
@@ -260,127 +243,45 @@ export class ThemMoiDieuChinhComponent extends Base2Component implements OnInit 
   async pheDuyet() {
     let trangThai = ''
     let mesg = ''
-    if (this.formData.get('loaiVthh').value.startsWith("02")) {
-      switch (this.formData.get('trangThai').value) {
-        case STATUS.DU_THAO: {
-          trangThai = STATUS.CHO_DUYET_LDV;
-          mesg = 'Bạn có muốn gửi duyệt ?'
-          break;
-        }
-        case STATUS.TU_CHOI_LDV: {
-          trangThai = STATUS.CHO_DUYET_LDV;
-          mesg = 'Bạn có muốn gửi duyệt ?'
-          break;
-        }
-        case STATUS.CHO_DUYET_LDV: {
-          trangThai = STATUS.DA_DUYET_LDV;
-          mesg = 'Bạn có muốn gửi duyệt ?'
-          break;
-        }
-        case STATUS.DA_DUYET_LDV: {
-          trangThai = STATUS.BAN_HANH;
-          mesg = 'Văn bản sẵn sàng ban hành ?'
-          break;
-        }
-      }
-    } else {
-      trangThai = STATUS.BAN_HANH;
-      mesg = 'Văn bản sẵn sàng ban hành ?'
-    }
-    await this.approve(this.formData.value.id, trangThai, mesg)
+    // if (this.formData.get('loaiVthh').value.startsWith("02")) {
+    //   switch (this.formData.get('trangThai').value) {
+    //     case STATUS.DU_THAO: {
+    //       trangThai = STATUS.CHO_DUYET_LDV;
+    //       mesg = 'Bạn có muốn gửi duyệt ?'
+    //       break;
+    //     }
+    //     case STATUS.TU_CHOI_LDV: {
+    //       trangThai = STATUS.CHO_DUYET_LDV;
+    //       mesg = 'Bạn có muốn gửi duyệt ?'
+    //       break;
+    //     }
+    //     case STATUS.CHO_DUYET_LDV: {
+    //       trangThai = STATUS.DA_DUYET_LDV;
+    //       mesg = 'Bạn có muốn gửi duyệt ?'
+    //       break;
+    //     }
+    //     case STATUS.DA_DUYET_LDV: {
+    //       trangThai = STATUS.BAN_HANH;
+    //       mesg = 'Văn bản sẵn sàng ban hành ?'
+    //       break;
+    //     }
+    //   }
+    // } else {
+
+    // }
+    trangThai = STATUS.BAN_HANH;
+    mesg = 'Văn bản sẵn sàng ban hành ?'
+    let result = await this.approve(this.formData.value.id, trangThai, mesg);
+    console.log(result);
   }
 
   async tuChoi() {
-    await this.reject(this.formData.value.id, STATUS.TU_CHOI_LDC);
+    let res = await this.reject(this.formData.value.id, STATUS.TU_CHOI_LDC);
+    console.log(res);
   }
 
   quayLai() {
     this.showListEvent.emit();
-  }
-
-  themMoiGoiThau() {
-    this.listNthauNopHs.forEach((value, index) => {
-      this.editCache[index] = {
-        edit: false,
-        data: { ...value }
-      };
-    })
-  }
-
-  addRow(): void {
-    this.listNthauNopHs = [
-      ...this.listNthauNopHs,
-      {}
-    ];
-    this.i++;
-    this.listNthauNopHs.forEach((value, index) => {
-      this.editCache[index] = {
-        edit: false,
-        data: { ...value }
-      };
-    })
-    this.editCache[this.i].edit = true;
-  }
-
-  updateEditCache(): void {
-    this.listNthauNopHs.forEach((item, index) => {
-      this.editCache[index] = {
-        edit: true,
-        data: { ...item }
-      };
-    });
-    this.listDiaDiemNhapHang.forEach((item, index) => {
-      this.editDiaDiemCache[index] = {
-        edit: true,
-        data: { ...item }
-      };
-    });
-  }
-
-  startEdit(index: number): void {
-    console.log(index);
-    this.editCache[index].edit = true;
-  }
-
-  deleteRow(index: number) {
-    this.listNthauNopHs = this.listNthauNopHs.filter((d, index) => index !== index);
-  }
-
-  cancelEdit(id: any): void {
-    const index = this.listNthauNopHs.findIndex(item => item.id === id);
-    this.editCache[id] = {
-      data: { ...this.listNthauNopHs[index] },
-      edit: false
-    };
-  }
-
-  saveEdit(index: any): void {
-    Object.assign(
-      this.listNthauNopHs[index],
-      this.editCache[index].data,
-    );
-    this.editCache[index].edit = false;
-  }
-
-  async saveDthauGthau() {
-    let body = this.formData.value;
-    console.log(body)
-    body.children = this.listNthauNopHs;
-    let res = await this.dauThauGoiThauService.create(body);
-    console.log(res);
-  }
-
-  calendarGia() {
-    let donGia = this.formData.get('donGia').value;
-    let VAT = this.formData.get('VAT').value;
-    let soLuong = this.formData.get('soLuong').value;
-    if (donGia >= 0 && VAT >= 0) {
-      this.formData.patchValue({
-        dgianHdongSauThue: (donGia + (donGia * VAT / 100)),
-        giaHdongTruocThue: donGia * soLuong,
-        giaHdongSauThue: (donGia + (donGia * VAT / 100)) * soLuong,
-      })
-    }
   }
 
   openDialogGoiThau(data?: any, index?: number, isReadOnly?: boolean) {
@@ -413,14 +314,6 @@ export class ThemMoiDieuChinhComponent extends Base2Component implements OnInit 
     });
   }
 
-  deleteSelect() {
-
-  }
-
-  deleteItem(index) {
-    this.danhsachDx = this.danhsachDx.filter((item, i) => i !== index)
-  }
-
   async openDialogDeXuat(index) {
     let data = this.danhsachDx[index]
     this.modal.create({
@@ -439,13 +332,20 @@ export class ThemMoiDieuChinhComponent extends Base2Component implements OnInit 
 
   async save(isGuiDuyet?) {
     this.setValidator();
-    let body = { ...this.formData.value, ... this.thongtinDieuchinhComponent.formData.value };
+    // let body = { ...this.formData.value, ... this.thongtinDieuchinhComponent.formData.value };
+    let body = { ...this.formData.value };
     body.soQdDc = body.soQdDc + this.maQd;
     body.children = this.danhsachDx;
     body.id = this.id;
-    await this.createUpdate(body);
+    let data = await this.createUpdate(body);
+    if (data) {
+      if (isGuiDuyet) {
+        this.pheDuyet();
+      } else {
+        this.quayLai();
+      }
+    }
   }
-
 
 
   setValidator() {
