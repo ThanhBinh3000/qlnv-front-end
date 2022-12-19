a5788b58ad6406a12eb352c62f67cfd349542041
hoangth <hoangth@hoangth.me>
2022-12-19 18:15:01 +0700

    sua phu luc 3

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -161,6 +161,7 @@ import { NzStatisticModule } from "ng-zorro-antd/statistic";
 import { NgxCurrencyModule } from 'ngx-currency';
 import { DialogThemDiaDiemPhanLoComponent } from './dialog/dialog-them-dia-diem-phan-lo/dialog-them-dia-diem-phan-lo.component';
 import { DialogThemMoiSoDuDauKyComponent } from './dialog/dialog-them-moi-so-du-dau-ky/dialog-them-moi-so-du-dau-ky.component';
+import { DialogDanhSachVatTuHangHoaComponent } from './dialog/dialog-danh-sach-vat-tu-hang-hoa/dialog-danh-sach-vat-tu-hang-hoa.component';
 @NgModule({
   declarations: [
     //components
@@ -271,7 +272,8 @@ import { DialogThemMoiSoDuDauKyComponent } from './dialog/dialog-them-moi-so-du-
     DialogCanCuThongTinChaoGiaComponent,
     DialogDieuChinhQuyetDinhPheDuyetComponent,
     DialogThemDiaDiemPhanLoComponent,
-    DialogThemMoiSoDuDauKyComponent
+    DialogThemMoiSoDuDauKyComponent,
+    DialogDanhSachVatTuHangHoaComponent,
   ],
   imports: [
     CommonModule,
@@ -401,7 +403,8 @@ import { DialogThemMoiSoDuDauKyComponent } from './dialog/dialog-them-moi-so-du-
     NgxCurrencyModule,
     DialogDieuChinhQuyetDinhPheDuyetComponent,
     DialogThemDiaDiemPhanLoComponent,
-    DialogThemMoiSoDuDauKyComponent
+    DialogThemMoiSoDuDauKyComponent,
+    DialogDanhSachVatTuHangHoaComponent,
   ],
   schemas: [NO_ERRORS_SCHEMA],
 })
