e0e91652ecdc79be539ce11ea0965fb53d784dc8
ta quang chien <tqchien1990@gmail.com>
2022-12-24 15:35:31 +0700

    nhap kho mua tt update

--- src/app/pages/nhap/mua-truc-tiep/nhap-kho-mtt/bang-ke-can-hang/bang-ke-can-hang.component.ts
+++ src/app/pages/nhap/mua-truc-tiep/nhap-kho-mtt/bang-ke-can-hang/bang-ke-can-hang.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./bang-ke-can-hang.component.scss']
 })
 export class BangKeCanHangComponent implements OnInit {
-  @Input() typeVthh: string;
+  @Input() loaiVthh: string;
   constructor() { }
 
   ngOnInit(): void {
