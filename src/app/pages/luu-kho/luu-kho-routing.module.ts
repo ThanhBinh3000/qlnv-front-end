855c6476272983703437261f84c7cac0ad1ab27d
othoa <othoa>
2022-12-26 15:25:43 +0700

    add screen theo dõi bảo quản

--- src/app/pages/luu-kho/luu-kho-routing.module.ts
+++ src/app/pages/luu-kho/luu-kho-routing.module.ts
@@ -1,30 +1,31 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LuuKhoComponent } from './luu-kho.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LuuKhoComponent} from './luu-kho.component';
 
 const routes: Routes = [
-	{
-		path: '',
-		component: LuuKhoComponent,
-		children: [
-			{
-				path: '',
-				redirectTo: 'quan-ly-so-kho-the-kho',
-				pathMatch: 'full'
-			},
-			{
-				path: 'quan-ly-so-kho-the-kho',
-				loadChildren: () =>
-					import(
-						'../luu-kho/quan-ly-so-the-kho/quan-ly-so-the-kho.module'
-					).then((m) => m.QuanLySoTheKhoModule),
-			},
-		],
-	},
+  {
+    path: '',
+    component: LuuKhoComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'quan-ly-so-kho-the-kho',
+        pathMatch: 'full'
+      },
+      {
+        path: 'quan-ly-so-kho-the-kho',
+        loadChildren: () =>
+          import(
+            '../luu-kho/quan-ly-so-the-kho/quan-ly-so-the-kho.module'
+            ).then((m) => m.QuanLySoTheKhoModule),
+      }
+    ],
+  },
 ];
 
 @NgModule({
-	imports: [RouterModule.forChild(routes)],
-	exports: [RouterModule],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class LuuKhoRoutingModule { }
+export class LuuKhoRoutingModule {
+}
--- src/app/services/hangthuocdientheodoidacthu/hang-sap-het-han-bao-hanh.service.ts
+++ src/app/services/hangthuocdientheodoidacthu/hang-sap-het-han-bao-hanh.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { BaseService } from '../base.service';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from 'src/environments/environment';
+import {BaseService} from '../base.service';
+import {Observable} from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,16 +11,19 @@ export class HangSapHetHanBaoHanhService extends BaseService {
 
 
   GATEWAY = '/qlnv-luukho';
+
   constructor(public httpClient: HttpClient) {
     super(httpClient, 'QuanLySoKhoTheKho', '');
   }
+
   search(body: any): Promise<any> {
     let url = `${environment.SERVICE_API}${this.GATEWAY}/hang-het-han-bao-hanh/tra-cuu`
+    // let url = `http://localhost:3333/hang-het-han-bao-hanh/tra-cuu`
     return this.httpClient.post<any>(url, body).toPromise();
   }
 
   exportList(body: any): Observable<Blob> {
     const url = `${environment.SERVICE_API}${this.GATEWAY}/hang-het-han-bao-hanh/export/list`;
-    return this.httpClient.post(url, body, { responseType: 'blob' });
+    return this.httpClient.post(url, body, {responseType: 'blob'});
   }
 }
