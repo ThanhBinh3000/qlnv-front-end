1346357d738a1975e610f3269addcde9e293b389
cuongvotinh <theendvg2003@gmail.com>
2022-12-27 13:57:27 +0700

    add

---
+++ src/app/pages/luu-kho/theo-doi-bao-quan/them-moi-so-theo-doi-bq/them-moi-so-theo-doi-bq.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-them-moi-so-theo-doi-bq',
+  templateUrl: './them-moi-so-theo-doi-bq.component.html',
+  styleUrls: ['./them-moi-so-theo-doi-bq.component.scss']
+})
+export class ThemMoiSoTheoDoiBqComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
