601cd268d1c575af7dc48531a356526bdd1bf99b
ta quang chien <tqchien1990@gmail.com>
2023-02-06 10:05:36 +0700

    rollback

--- src/app/shared/globals.ts
+++ src/app/shared/globals.ts
@@ -1,11 +1,11 @@
-import { formatNumber } from '@angular/common';
-import { Injectable } from '@angular/core';
+import {formatNumber} from '@angular/common';
+import {Injectable} from '@angular/core';
 import jsonGlobals from './globals.json';
 
 @Injectable()
 export class Globals {
   prop: any = jsonGlobals;
-  formatter = (value: number) => value.toLocaleString('vi-VN');
-  parser = (value: string) => value.replace(/\,/g, '');
+  formatter = (value) => value ? formatNumber(value, 'vi_VN', '1.0-1') : 0;
+  parser = value => value.replaceAll('.', '');
   parserInput = value => value.replaceAll(',', '.');
 }
