7dde677092198487d2d1c62f7550922d1c8f18af
datpd <datpd@enao.local>
2023-02-13 18:37:16 +0700

    them: luong giao du toan

---
+++ src/app/pages/ke-hoach/du-toan-nsnn/giao-du-toan-thuc-te/giao-du-toan-thuc-te.constant.ts
@@ -0,0 +1,60 @@
+import { GDT } from "src/app/Utility/utils";
+
+
+export const TAB_LIST = [
+    {
+        name: 'Quyết định từ Bộ Tài Chính',
+        code: 'dsquyetDinh',
+        status: true,
+        role: [GDT.EDIT_REPORT_BTC],
+        isSelected: false,
+    },
+    {
+        name: 'Phân bổ dự toán',
+        code: 'dsphanBo',
+        status: true,
+        role: [
+            GDT.VIEW_REPORT_PA_PBDT,
+            GDT.DELETE_REPORT_PA_PBDT,
+            GDT.EDIT_REPORT_PA_PBDT,
+            GDT.ADD_REPORT_CV_QD_GIAO_PA_PBDT,
+        ],
+        isSelected: false,
+    },
+    {
+        name: 'Dự toán giao từ đơn vị cấp trên',
+        code: 'dsGiaoTuCapTren',
+        status: true,
+        role: [GDT.NHAN_PA_PBDT],
+        isSelected: false,
+    },
+    {
+        name: 'Báo cáo từ đơn vị cấp dưới',
+        code: 'baoCaoCapDuoi',
+        status: true,
+        role: [
+            GDT.XEM_PA_TONGHOP_PBDT,
+        ],
+        isSelected: false,
+    },
+    {
+        name: 'Tổng hợp báo cáo từ đơn vị cấp dưới',
+        code: 'tongHopBaoCaoCapDuoi',
+        status: true,
+        role: [
+            GDT.XEM_PA_TONGHOP_PBDT,
+        ],
+        isSelected: false,
+    },
+    {
+        name: 'Danh sách báo cáo',
+        code: 'danhSachBaoCao',
+        status: true,
+        role: [
+            GDT.EDIT_REPORT_TH,
+            GDT.XOA_REPORT_TH,
+            GDT.XEM_PA_TONGHOP_PBDT,
+        ],
+        isSelected: false,
+    },
+]
