7ff36ebbd7d86a7cb1689ce271dbc60af2b699ee
thongtv <thongtv@tecapro.com>
2023-02-23 15:03:27 +0700

    xuất CtVt: phiếu xuất kho

--- src/app/pages/xuat/cuu-tro-vien-tro/xuat-kho/xuat-kho.module.ts
+++ src/app/pages/xuat/cuu-tro-vien-tro/xuat-kho/xuat-kho.module.ts
@@ -4,13 +4,15 @@ import { PhieuXuatKhoComponent } from './phieu-xuat-kho/phieu-xuat-kho.component
 import { ComponentsModule } from 'src/app/components/components.module';
 import { DirectivesModule } from 'src/app/directives/directives.module';
 import { MainXuatKhoComponent } from './main-xuat-kho/main-xuat-kho.component';
+import { ThemMoiPhieuXuatKhoComponent } from './phieu-xuat-kho/them-moi-phieu-xuat-kho/them-moi-phieu-xuat-kho.component';
 
 
 
 @NgModule({
   declarations: [
     MainXuatKhoComponent,
-    PhieuXuatKhoComponent
+    PhieuXuatKhoComponent,
+    ThemMoiPhieuXuatKhoComponent
   ],
   imports: [
     CommonModule,
