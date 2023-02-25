b366ad1d5b479f4bae07013211c27da4ee16f9ed
hoangth <hoangth@enao.local>
2023-02-25 13:18:11 +0700

    fix bug 1.7

--- src/app/pages/quan-ly-ke-hoach-cap-von-phi-hang/cap-von-mua-ban-va-thanh-toan-tien-hang/von-mua-von-ung/danh-sach-von-mua-von-ung/danh-sach-von-mua-von-ung.component.ts
+++ src/app/pages/quan-ly-ke-hoach-cap-von-phi-hang/cap-von-mua-ban-va-thanh-toan-tien-hang/von-mua-von-ung/danh-sach-von-mua-von-ung/danh-sach-von-mua-von-ung.component.ts
@@ -10,7 +10,7 @@ import { CapVonMuaBanTtthService } from 'src/app/services/quan-ly-von-phi/capVon
 import { QuanLyVonPhiService } from 'src/app/services/quanLyVonPhi.service';
 import { UserService } from 'src/app/services/user.service';
 import { CAN_CU_GIA, CVMB, LOAI_DE_NGHI, Utils } from 'src/app/Utility/utils';
-import { TRANG_THAI } from '../../cap-von-mua-ban-va-thanh-toan-tien-hang.constant';
+import { receivedInfo, Report, sendInfo, TienThua, TRANG_THAI } from '../../cap-von-mua-ban-va-thanh-toan-tien-hang.constant';
 import { DialogTaoMoiCapVonComponent } from '../dialog-tao-moi-cap-von/dialog-tao-moi-cap-von.component';
 import { DialogTaoMoiThanhToanComponent } from '../dialog-tao-moi-thanh-toan/dialog-tao-moi-thanh-toan.component';
 import { DialogTaoMoiTienThuaComponent } from '../dialog-tao-moi-tien-thua/dialog-tao-moi-tien-thua.component';
@@ -301,13 +301,16 @@ export class DanhSachVonMuaVonUngComponent implements OnInit {
                     default:
                         break;
                 }
+                if (obj.baoCao.maLoai != 6) {
+                    await this.addVonBanGuiDvct(obj.baoCao.namDnghi);
+                }
                 this.dataChange.emit(obj);
             }
         });
     }
 
     //xem chi tiet bao cao
-    viewDetail(data: any) {
+    async viewDetail(data: any) {
         const obj = {
             id: data.id,
             tabSelected: '',
@@ -338,6 +341,9 @@ export class DanhSachVonMuaVonUngComponent implements OnInit {
                 break;
             default:
                 break;
+        }
+        if (data.maLoai == 2 && data.ttNhan.trangThai == Utils.TT_BC_1) {
+            await this.addVonBanGuiDvct(data.namDnghi);
         }
         this.dataChange.emit(obj);
     }
@@ -425,6 +431,105 @@ export class DanhSachVonMuaVonUngComponent implements OnInit {
             }
         )
         this.spinner.hide();
+    }
+
+    //kiem tra ban ghi nop tien von ban hang len don vi cap tren da ton tai chua, neu chua thi thuc hien them moi
+    async addVonBanGuiDvct(nam: number) {
+        let check = false // ban ghi chua ton tai
+        //kiem tra ban ghi da ton tai chua
+        const request = {
+            loaiTimKiem: '0',
+            maLoai: 6,
+            maDvi: this.userInfo?.MA_DVI,
+            namBcao: nam,
+            paggingReq: {
+                limit: 10,
+                page: 1,
+            },
+        }
+        await this.capVonMuaBanTtthService.timKiemVonMuaBan(request).toPromise().then(
+            (data) => {
+                if (data.statusCode == 0) {
+                    if (data.data.content?.length > 0) {
+                        check = true;
+                    }
+                } else {
+                    this.notification.error(MESSAGE.ERROR, MESSAGE.ERROR_CALL_SERVICE);
+                }
+            },
+            (err) => {
+                this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+            }
+        );
+        //neu chua ton tai thi thuc hien them moi
+        if (!check) {
+            const response: Report = new Report();
+            response.namDnghi = nam;
+            response.ttGui = new sendInfo();
+            response.ttGui.lstCtietBcaos = [];
+            response.ttNhan = new receivedInfo();
+            response.ttNhan.lstCtietBcaos = [];
+            response.maDvi = this.userInfo?.MA_DVI;
+            response.ngayTao = new Date();
+            response.dot = 1;
+            response.maLoai = 6;
+            response.ttGui.trangThai = Utils.TT_BC_1;
+            response.ttNhan.trangThai = Utils.TT_BC_1;
+            response.ttGui.lstFiles = [];
+            response.ttNhan.lstFiles = [];
+            response.ttGui.lstCtietBcaos.push({
+                ...new TienThua(),
+                id: null,
+                maHang: Utils.MUA_THOC,
+                hangDtqg: 'Thóc',
+            })
+            if (!this.userService.isChiCuc()) {
+                response.ttGui.lstCtietBcaos.push({
+                    ...new TienThua(),
+                    id: null,
+                    maHang: Utils.MUA_GAO,
+                    hangDtqg: 'Gạo',
+                })
+                response.ttGui.lstCtietBcaos.push({
+                    ...new TienThua(),
+                    id: null,
+                    maHang: Utils.MUA_MUOI,
+                    hangDtqg: 'Muối',
+                })
+            }
+            if (this.userService.isTongCuc()) {
+                response.ttGui.lstCtietBcaos.push({
+                    ...new TienThua(),
+                    id: null,
+                    maHang: Utils.MUA_VTU,
+                    hangDtqg: 'Vật tư',
+                })
+            }
+            await this.capVonMuaBanTtthService.maCapVonUng().toPromise().then(
+                (res) => {
+                    if (res.statusCode == 0) {
+                        response.maCapUng = res.data;
+                    } else {
+                        this.notification.error(MESSAGE.ERROR, res?.msg);
+                    }
+                },
+                (err) => {
+                    this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+                },
+            );
+            this.capVonMuaBanTtthService.themMoiVonMuaBan(response).toPromise().then(
+                async (data) => {
+                    if (data.statusCode == 0) {
+                        // this.notification.success(MESSAGE.SUCCESS, MESSAGE.ADD_SUCCESS);
+                    } else {
+                        this.notification.error(MESSAGE.ERROR, data?.msg);
+                    }
+                },
+                (err) => {
+                    this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+                },
+            );
+        }
     }
 
 }
