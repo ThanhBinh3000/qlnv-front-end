dc0ba86643982fd9f41d20aa2ce0f424a2c4d80b
duongvd <duongvd@tecapro.com.vn>
2023-02-28 08:13:44 +0700

    + chi tiet quyet dinh xuat cap

---
+++ src/app/pages/xuat/xuat-cuu-tro-vien-tro/xuat-cap/quyet-dinh-xuat-cap/quyet-dinh-xuat-cap.component.ts
@@ -0,0 +1,116 @@
+import { Component, Input, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { StorageService } from "../../../../../services/storage.service";
+import { NzNotificationService } from "ng-zorro-antd/notification";
+import { NgxSpinnerService } from "ngx-spinner";
+import { NzModalService } from "ng-zorro-antd/modal";
+import { DatePipe } from "@angular/common";
+import { DonviService } from "../../../../../services/donvi.service";
+import {
+  QuyetDinhPheDuyetPhuongAnCuuTroService
+} from "../../../../../services/qlnv-hang/xuat-hang/xuat-cuu-tro-vien-tro/QuyetDinhPheDuyetPhuongAnCuuTro.service";
+import { UserLogin } from "../../../../../models/userlogin";
+import { MESSAGE } from "../../../../../constants/message";
+import dayjs from "dayjs";
+import { Utils } from "../../../../../Utility/utils";
+import { isEmpty } from "lodash";
+import { cloneDeep } from "lodash";
+import { Base2Component } from "../../../../../components/base2/base2.component";
+
+@Component({
+  selector: 'app-quyet-dinh-xuat-cap',
+  templateUrl: './quyet-dinh-xuat-cap.component.html',
+  styleUrls: ['./quyet-dinh-xuat-cap.component.scss']
+})
+export class QuyetDinhXuatCapComponent extends Base2Component implements OnInit {
+
+  constructor(httpClient: HttpClient,
+              storageService: StorageService,
+              notification: NzNotificationService,
+              spinner: NgxSpinnerService,
+              modal: NzModalService,
+              private datePipe: DatePipe,
+              private donviService: DonviService,
+              private quyetDinhPheDuyetPhuongAnCuuTroService: QuyetDinhPheDuyetPhuongAnCuuTroService) {
+    super(httpClient, storageService, notification, spinner, modal, quyetDinhPheDuyetPhuongAnCuuTroService);
+    this.formData = this.fb.group({
+      nam: null,
+      soQdXc: null,
+      ngayHieuLuc: null,
+      ngayXuatCtvt: null,
+      ngayXuatCtvtTu: null,
+      ngayHieuLucTu: null,
+      ngayHieuLucDen: null,
+      ngayXuatCtvtDen: null,
+    });
+    this.filterTable = {
+      soQdXc: "",
+      ngayHieuLucQdXc: "",
+      soQdChuyenXc: "",
+      ngayHieuLucQdChuyenXc: "",
+      soLuongGaoXc: "",
+      soLuongThocXc: "",
+      trichYeu: "",
+      tenTrangThai: ""
+    };
+  }
+  userInfo: UserLogin;
+  userdetail: any = {};
+  selectedId: number = 0;
+  isVatTu: boolean = false;
+  isView = false;
+
+  async ngOnInit() {
+    try {
+      this.initData();
+      await this.timKiem();
+      await this.spinner.hide();
+    } catch (e) {
+      console.log("error: ", e);
+      this.spinner.hide();
+      this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+    }
+  }
+
+  async initData() {
+    this.userInfo = this.userService.getUserLogin();
+    this.userdetail.maDvi = this.userInfo.MA_DVI;
+    this.userdetail.tenDvi = this.userInfo.TEN_DVI;
+  }
+
+  async timKiem() {
+    if (this.formData.value.ngayHieuLuc) {
+      this.formData.value.ngayHieuLucTu = dayjs(this.formData.value.ngayHieuLuc[0]).format("YYYY-MM-DD");
+      this.formData.value.ngayHieuLucDen = dayjs(this.formData.value.ngayHieuLuc[1]).format("YYYY-MM-DD");
+    }
+    if (this.formData.value.ngayXuatCtvt) {
+      this.formData.value.ngayXuatCtvtTu = dayjs(this.formData.value.ngayXuatCtvt[0]).format("YYYY-MM-DD");
+      this.formData.value.ngayXuatCtvtDen = dayjs(this.formData.value.ngayXuatCtvt[1]).format("YYYY-MM-DD");
+    }
+    await this.search();
+  }
+
+  redirectDetail(id, b: boolean) {
+    this.selectedId = id;
+    this.isDetail = true;
+    this.isView = b;
+  }
+
+  filterDateInTable(key: string, value: string) {
+    if (value && value != "") {
+      this.dataTable = [];
+      let temp = [];
+      if (this.dataTableAll && this.dataTableAll.length > 0) {
+        value = this.datePipe.transform(value, Utils.FORMAT_DATE_STR);
+        this.dataTableAll.forEach((item) => {
+          if (item[key] && item[key].toString().toLowerCase().indexOf(value.toString().toLowerCase()) != -1) {
+            temp.push(item);
+          }
+        });
+      }
+      this.dataTable = [...this.dataTable, ...temp];
+    } else {
+      this.dataTable = cloneDeep(this.dataTableAll);
+    }
+  };
+}
