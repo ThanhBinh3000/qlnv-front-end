99683a0b9167d3e66999aa3b4cc2713adb819b18
ta quang chien <tqchien1990@gmail.com>
2023-03-06 18:34:42 +0700

    update xuat cap qd gnv

--- src/app/pages/xuat/xuat-cuu-tro-vien-tro/xuat-cap/xuat-cap.module.ts
+++ src/app/pages/xuat/xuat-cuu-tro-vien-tro/xuat-cap/xuat-cap.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { XuatCapComponent } from "./xuat-cap.component";
-import { XuatCapRoutingModule } from "./xuat-cap-routing.module";
-import { ComponentsModule } from "../../../../components/components.module";
-import { DirectivesModule } from "../../../../directives/directives.module";
-import { QuyetDinhPhuongAnModule } from "./quyet-dinh-phuong-an/quyet-dinh-phuong-an.module";
-import { QuyetDinhXuatCapModule } from "./quyet-dinh-xuat-cap/quyet-dinh-xuat-cap.module";
-import { KiemTraChatLuongComponent } from './kiem-tra-chat-luong/kiem-tra-chat-luong.component';
-import { KiemTraChatLuongModule } from "./kiem-tra-chat-luong/kiem-tra-chat-luong.module";
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {XuatCapComponent} from "./xuat-cap.component";
+import {XuatCapRoutingModule} from "./xuat-cap-routing.module";
+import {ComponentsModule} from "../../../../components/components.module";
+import {DirectivesModule} from "../../../../directives/directives.module";
+import {QuyetDinhPhuongAnModule} from "./quyet-dinh-phuong-an/quyet-dinh-phuong-an.module";
+import {QuyetDinhXuatCapModule} from "./quyet-dinh-xuat-cap/quyet-dinh-xuat-cap.module";
+import {QuyetDinhGnvXuatHangComponent} from './quyet-dinh-gnv-xuat-hang/quyet-dinh-gnv-xuat-hang.component';
+import {KiemTraChatLuongModule} from "./kiem-tra-chat-luong/kiem-tra-chat-luong.module";
+import {QuyetDinhGnvXuatHangModule} from "./quyet-dinh-gnv-xuat-hang/quyet-dinh-gnv-xuat-hang.module";
 
 
 @NgModule({
   declarations: [
     XuatCapComponent,
-
-
+    QuyetDinhGnvXuatHangComponent,
+    XuatCapComponent,
   ],
   exports: [
     XuatCapComponent
@@ -26,7 +27,8 @@ import { KiemTraChatLuongModule } from "./kiem-tra-chat-luong/kiem-tra-chat-luon
     DirectivesModule,
     QuyetDinhPhuongAnModule,
     QuyetDinhXuatCapModule,
-    KiemTraChatLuongModule
+    KiemTraChatLuongModule,
+    QuyetDinhGnvXuatHangModule
   ]
 })
 export class XuatCapModule {
