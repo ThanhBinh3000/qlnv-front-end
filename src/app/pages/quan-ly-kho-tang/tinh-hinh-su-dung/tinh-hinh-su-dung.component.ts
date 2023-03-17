0c12eb9ce3a700b49fb277485e2110ea964ecaa5
othoa <othoa>
2023-03-17 14:48:59 +0700

    fix

---
+++ src/app/pages/quan-ly-kho-tang/tinh-hinh-su-dung/tinh-hinh-su-dung.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tinh-hinh-su-dung',
+  templateUrl: './tinh-hinh-su-dung.component.html',
+  styleUrls: ['./tinh-hinh-su-dung.component.scss']
+})
+export class TinhHinhSuDungComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
