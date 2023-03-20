8f809be1ea5cf7168d6de8cf044e51d1025a027d
cuongnv <cuongnv@ttkd6.com>
2023-03-20 10:51:26 +0700

    add

---
+++ src/app/pages/quan-ly-kho-tang/ke-hoach/dm-du-an-cong-trinh/danh-muc-sc-lon/thong-tin-dm-sc-lon/thong-tin-dm-sc-lon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thong-tin-dm-sc-lon',
+  templateUrl: './thong-tin-dm-sc-lon.component.html',
+  styleUrls: ['./thong-tin-dm-sc-lon.component.scss']
+})
+export class ThongTinDmScLonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
