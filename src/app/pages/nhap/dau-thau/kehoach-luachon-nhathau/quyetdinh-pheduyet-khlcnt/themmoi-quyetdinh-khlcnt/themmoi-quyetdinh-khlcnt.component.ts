7caa67b1177bdc61e0414c2a3364f20267db6b8d
haihm <haihm@tecapro.com.vn>
2023-03-28 18:19:25 +0700

    fix bug dauthau

--- src/app/pages/nhap/dau-thau/kehoach-luachon-nhathau/quyetdinh-pheduyet-khlcnt/themmoi-quyetdinh-khlcnt/themmoi-quyetdinh-khlcnt.component.ts
+++ src/app/pages/nhap/dau-thau/kehoach-luachon-nhathau/quyetdinh-pheduyet-khlcnt/themmoi-quyetdinh-khlcnt/themmoi-quyetdinh-khlcnt.component.ts
@@ -312,6 +312,7 @@ export class ThemmoiQuyetdinhKhlcntComponent implements OnInit {
     } else {
       res = await this.quyetDinhPheDuyetKeHoachLCNTService.create(body);
     }
+    debugger
     if (res.msg == MESSAGE.SUCCESS) {
       if (isGuiDuyet) {
         this.idInput = res.data.id;
@@ -320,7 +321,8 @@ export class ThemmoiQuyetdinhKhlcntComponent implements OnInit {
         if (this.formData.get('id').value) {
           this.notification.success(MESSAGE.SUCCESS, MESSAGE.UPDATE_SUCCESS);
         } else {
-          this.isCheckCreate = false
+          this.formData.get('id').setValue(res.data.id);
+          this.idInput = res.data.id;
           this.notification.success(MESSAGE.SUCCESS, MESSAGE.ADD_SUCCESS);
         }
       }
