d0c569043b36ad447f1a0de130229359cd96518c
duongvd <duongvd@tecapro.com.vn>
2023-03-29 08:50:01 +0700

    + fix bug

--- src/app/components/dialog/dialog-them-moi-goi-thau/dialog-them-moi-goi-thau.component.ts
+++ src/app/components/dialog/dialog-them-moi-goi-thau/dialog-them-moi-goi-thau.component.ts
@@ -190,10 +190,10 @@ export class DialogThemMoiGoiThauComponent implements OnInit {
     //   this.notification.error(MESSAGE.ERROR, 'Chủng loại hàng hóa chưa có giá mua tối đa');
     //   return;
     // }
-    if (this.validateRangPrice()) {
-      this.notification.error(MESSAGE.ERROR, 'Giá tạm tính phải nhỏ hơn giá mua tối đa (' + this.giaToiDa + ')');
-      return;
-    }
+    // if (this.validateRangPrice()) {
+    //   this.notification.error(MESSAGE.ERROR, 'Giá tạm tính phải nhỏ hơn giá mua tối đa (' + this.giaToiDa + ')');
+    //   return;
+    // }
     const body = this.formGoiThau.value;
     body.children = this.dataTable
     body.children.forEach(item => item.donGia = body.donGia);
