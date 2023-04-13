5d9b60febf080d720d0767bb7f4b75c72ccef3b9
othoa <othoa>
2023-04-13 17:05:10 +0700

    add thong ke truy cap

--- src/app/services/user-activity.service.ts
+++ src/app/services/user-activity.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {BaseService} from './base.service';
+import {OldResponseData} from "../interfaces/response";
 
 @Injectable({
   providedIn: 'root',
@@ -14,4 +15,8 @@ export class UserActivityService extends BaseService {
     super(httpClient, 'lich-su-truy-cap', '/qlnv-system');
   }
 
+  thongKeTruyCap(body) {
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/thong-ke-truy-cap`;
+    return this._httpClient.post<OldResponseData>(url, body).toPromise();
+  }
 }
