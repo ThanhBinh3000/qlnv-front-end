6fbe7b6054ff9bea4753b4c690f78dbf79d03020
haihm <haihm@tecapro.com.vn>
2023-04-20 21:07:11 +0700

    fix bug

--- src/app/services/qlnv-hang/nhap-hang/dau-thau/tochuc-trienkhai/quyetDinhPheDuyetKetQuaLCNT.service.ts
+++ src/app/services/qlnv-hang/nhap-hang/dau-thau/tochuc-trienkhai/quyetDinhPheDuyetKetQuaLCNT.service.ts
@@ -3,17 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../../../environments/environment';
 import { BaseService } from '../../../../base.service';
 import { Observable } from 'rxjs';
-import {OldResponseData} from "../../../../../interfaces/response";
+import { OldResponseData } from "../../../../../interfaces/response";
+import { BaseTestService } from 'src/app/services/base-test.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class QuyetDinhPheDuyetKetQuaLCNTService extends BaseService {
-  GATEWAY = '/qlnv-hang';
+export class QuyetDinhPheDuyetKetQuaLCNTService extends BaseTestService {
+  GATEWAY = '';
 
   constructor(public httpClient: HttpClient) {
-    super(httpClient, 'dx-kh/qd-pduyet-kqlcnt', '/qlnv-hang');
+    super(httpClient, 'dx-kh/qd-pduyet-kqlcnt', '');
   }
 
 
+  getDetailBySoQd(body) {
+    const url = `${environment.SERVICE_API_LOCAL}${this.GATEWAY}/${this.table}/chi-tiet/bySoQd`;
+    return this._httpClient.post<OldResponseData>(url, body).toPromise();
+  }
 }
