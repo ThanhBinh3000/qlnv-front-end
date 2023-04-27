46c70529240d1c40026402ffb724d83c5b5ea90d
othoa <othoa>
2023-04-27 17:51:23 +0700

    update de xuat ke hoach sua chua thuong xuyen

--- src/app/services/qlnv-kho/quy-hoach-ke-hoach/ke-hoach-sc-thuong-xuyen/tong-hop-sc-thuong-xuyen.service.ts
+++ src/app/services/qlnv-kho/quy-hoach-ke-hoach/ke-hoach-sc-thuong-xuyen/tong-hop-sc-thuong-xuyen.service.ts
@@ -7,12 +7,16 @@ import {environment} from "../../../../../environments/environment";
 @Injectable({
   providedIn: 'root'
 })
-export class DeXuatScThuongXuyenService extends BaseService {
+export class TongHopScThuongXuyenService extends BaseService {
 
   GATEWAY = '/qlnv-kho';
 
   constructor(public httpClient: HttpClient) {
-    super(httpClient, 'kt-kh-sc-thuong-xuyen/de-xuat', '/qlnv-kho');
+    super(httpClient, 'kt-kh-sc-thuong-xuyen/tong-hop', '/qlnv-kho');
   }
 
+  // tongHop(body): Promise<OldResponseData> {
+  //   const url = `${environment.SERVICE_API}${this.GATEWAY}/kt-kh-sc-lon/tra-cuu-dx-cuc`;
+  //   return this._httpClient.post<OldResponseData>(url, body).toPromise();
+  // }
 }
