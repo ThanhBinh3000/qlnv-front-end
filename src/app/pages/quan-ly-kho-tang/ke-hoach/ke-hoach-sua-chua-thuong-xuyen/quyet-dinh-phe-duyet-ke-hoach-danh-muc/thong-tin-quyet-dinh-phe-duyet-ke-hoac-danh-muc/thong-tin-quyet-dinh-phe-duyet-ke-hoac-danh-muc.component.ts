96f889ca394c79bd2eb2547240dcdfd8032eb352
othoa <othoa>
2023-05-04 09:27:18 +0700

    add quyet dinh ke hoach sua chua thuong xuyen

---
+++ src/app/pages/quan-ly-kho-tang/ke-hoach/ke-hoach-sua-chua-thuong-xuyen/quyet-dinh-phe-duyet-ke-hoach-danh-muc/thong-tin-quyet-dinh-phe-duyet-ke-hoac-danh-muc/thong-tin-quyet-dinh-phe-duyet-ke-hoac-danh-muc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thong-tin-quyet-dinh-phe-duyet-ke-hoac-danh-muc',
+  templateUrl: './thong-tin-quyet-dinh-phe-duyet-ke-hoac-danh-muc.component.html',
+  styleUrls: ['./thong-tin-quyet-dinh-phe-duyet-ke-hoac-danh-muc.component.scss']
+})
+export class ThongTinQuyetDinhPheDuyetKeHoacDanhMucComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
