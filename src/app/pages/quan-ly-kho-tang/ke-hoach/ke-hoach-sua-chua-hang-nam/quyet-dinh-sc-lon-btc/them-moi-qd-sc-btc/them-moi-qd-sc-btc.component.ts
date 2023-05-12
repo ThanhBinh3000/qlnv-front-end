3f65d24a481e52196e9508a679b3aac7b1414e8f
cuongnv <cuongnv@ttkd6.com>
2023-05-12 15:21:25 +0700

    Fix bug quy hoạch kế hoạch KT

--- src/app/pages/quan-ly-kho-tang/ke-hoach/ke-hoach-sua-chua-hang-nam/quyet-dinh-sc-lon-btc/them-moi-qd-sc-btc/them-moi-qd-sc-btc.component.ts
+++ src/app/pages/quan-ly-kho-tang/ke-hoach/ke-hoach-sua-chua-hang-nam/quyet-dinh-sc-lon-btc/them-moi-qd-sc-btc/them-moi-qd-sc-btc.component.ts
@@ -1,27 +1,31 @@
-import { cloneDeep } from 'lodash';
-import {Component, Input, OnInit, } from '@angular/core';
-import {Validators} from "@angular/forms";
-import {NgxSpinnerService} from "ngx-spinner";
-import {NzNotificationService} from "ng-zorro-antd/notification";
-import {NzModalService} from "ng-zorro-antd/modal";
-import {Base2Component} from "../../../../../../components/base2/base2.component";
-import {HttpClient} from "@angular/common/http";
-import {StorageService} from "../../../../../../services/storage.service";
-import {MESSAGE} from "../../../../../../constants/message";
+import { cloneDeep } from "lodash";
+import { Component, Input, OnInit } from "@angular/core";
+import { Validators } from "@angular/forms";
+import { NgxSpinnerService } from "ngx-spinner";
+import { NzNotificationService } from "ng-zorro-antd/notification";
+import { NzModalService } from "ng-zorro-antd/modal";
+import { Base2Component } from "../../../../../../components/base2/base2.component";
+import { HttpClient } from "@angular/common/http";
+import { StorageService } from "../../../../../../services/storage.service";
+import { MESSAGE } from "../../../../../../constants/message";
 import {
   KtKhSuaChuaBtcService
 } from "../../../../../../services/qlnv-kho/quy-hoach-ke-hoach/kh-sc-lon-btc/kt-kh-sua-chua-btc.service";
 import dayjs from "dayjs";
-import {DialogQdScBtcComponent} from "./dialog-qd-sc-btc/dialog-qd-sc-btc.component";
-import {STATUS} from "../../../../../../constants/status";
+import { DialogQdScBtcComponent } from "./dialog-qd-sc-btc/dialog-qd-sc-btc.component";
+import { STATUS } from "../../../../../../constants/status";
 import {
   DeXuatScLonService
 } from "../../../../../../services/qlnv-kho/quy-hoach-ke-hoach/ke-hoach-sc-lon/de-xuat-sc-lon.service";
-
+import {
+  TongHopDxScLonService
+} from "../../../../../../services/qlnv-kho/quy-hoach-ke-hoach/ke-hoach-sc-lon/tong-hop-dx-sc-lon.service";
+import { chain } from "lodash";
+import { v4 as uuidv4 } from "uuid";
 @Component({
-  selector: 'app-them-moi-qd-sc-btc',
-  templateUrl: './them-moi-qd-sc-btc.component.html',
-  styleUrls: ['./them-moi-qd-sc-btc.component.scss']
+  selector: "app-them-moi-qd-sc-btc",
+  templateUrl: "./them-moi-qd-sc-btc.component.html",
+  styleUrls: ["./them-moi-qd-sc-btc.component.scss"]
 })
 export class ThemMoiQdScBtcComponent extends Base2Component implements OnInit {
 
@@ -31,14 +35,12 @@ export class ThemMoiQdScBtcComponent extends Base2Component implements OnInit {
   maQd: string;
   dsCuc: any[] = [];
   dsChiCuc: any[] = [];
-  tablePaTc: any[] = []
-  dataEdit : any
-
+  tablePaTc: any[] = [];
+  dataEdit: any;
   listLoaiDuAn: any[] = [];
   listDxCuc: any[] = [];
   listTongHop: any[] = [];
-  typeQd: string
-  isEdit : number = -1
+  dataTableReq: any[] = [];
 
   constructor(
     private httpClient: HttpClient,
@@ -46,42 +48,42 @@ export class ThemMoiQdScBtcComponent extends Base2Component implements OnInit {
     notification: NzNotificationService,
     spinner: NgxSpinnerService,
     modal: NzModalService,
-    private qdScBtcService : KtKhSuaChuaBtcService,
-    private dexuatService : DeXuatScLonService
+    private qdScBtcService: KtKhSuaChuaBtcService,
+    private dexuatService: DeXuatScLonService,
+    private tongHopDxScLon : TongHopDxScLonService
   ) {
     super(httpClient, storageService, notification, spinner, modal, qdScBtcService);
-    super.ngOnInit()
+    super.ngOnInit();
     this.formData = this.fb.group({
       id: [null],
-      maDvi : [null],
-      tenDvi : [null],
-      soQd : [null, Validators.required],
-      namKeHoach : [dayjs().get('year'), Validators.required],
-      trichYeu : [null],
-      ngayKy : [null, Validators.required],
-      ngayTrinhBtc : [null, Validators.required],
-      maTh : [null],
-      soTt : [null],
-      trangThai : ['00'],
-      tenTrangThai : ['Dự thảo'],
-      type : ['00']
+      maDvi: [null],
+      tenDvi: [null],
+      soQuyetDinh: [null, Validators.required],
+      namKeHoach: [dayjs().get("year"), Validators.required],
+      trichYeu: [null],
+      ngayKy: [null, Validators.required],
+      ngayTrinhBtc: [null, Validators.required],
+      maTh: [null],
+      soTt: [null],
+      trangThai: ["00"],
+      tenTrangThai: ["Dự thảo"],
+      type: ["00"]
     });
   }
 
   async ngOnInit() {
     await this.spinner.show();
     try {
-      this.maQd = '/QĐ-BTC';
-      await Promise.all([
-      ]);
+      this.maQd = "/QĐ-BTC";
+      await Promise.all([]);
       if (this.idInput > 0) {
-        await this.getDataDetail(this.idInput)
+        await this.getDataDetail(this.idInput);
       } else {
-        this.loadDxCuc()
-        this.loadListTh()
+        this.loadDxCuc();
+        this.loadListTh();
       }
     } catch (e) {
-      console.log('error: ', e);
+      console.log("error: ", e);
       await this.spinner.hide();
       this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
     }
@@ -97,16 +99,16 @@ export class ThemMoiQdScBtcComponent extends Base2Component implements OnInit {
           "limit": 999,
           "page": 0
         }
-      }
+      };
       let res = await this.dexuatService.search(body);
       if (res.msg == MESSAGE.SUCCESS) {
         let data = res.data;
-        this.listDxCuc = []
+        this.listDxCuc = [];
         this.listDxCuc = data.content;
         if (this.listDxCuc) {
           this.listDxCuc = this.listDxCuc.filter(
             (item) => (item.trangThai == this.STATUS.DA_DUYET_CBV && item.trangThaiTh == STATUS.CHUA_TONG_HOP && !item.qdBtcId)
-          )
+          );
         }
       } else {
         this.listDxCuc = [];
@@ -120,25 +122,26 @@ export class ThemMoiQdScBtcComponent extends Base2Component implements OnInit {
       this.spinner.hide();
     }
   }
+
   async loadListTh() {
     this.spinner.show();
     try {
       let body = {
-        "capDvi": "1",
+        "namKeHoach" : this.formData.value.namKeHoach,
         "paggingReq": {
           "limit": 999,
           "page": 0
         }
-      }
-      let res = await this.dexuatService.search(body);
+      };
+      let res = await this.tongHopDxScLon.search(body);
       if (res.msg == MESSAGE.SUCCESS) {
         let data = res.data;
-        this.listTongHop = []
+        this.listTongHop = [];
         this.listTongHop = data.content;
         if (this.listTongHop) {
           this.listTongHop = this.listTongHop.filter(
-            (item) => (item.trangThai == this.STATUS.DA_DUYET_LDTC && !item.qdBtcId)
-          )
+            (item) => (item.trangThai == this.STATUS.DA_DUYET_LDTC && !item.soQdBtc)
+          );
         }
       } else {
         this.listDxCuc = [];
@@ -153,29 +156,59 @@ export class ThemMoiQdScBtcComponent extends Base2Component implements OnInit {
     }
   }
 
-  async changSoTh(event, type?: string) {
-    let result;
-    if (type == 'DX') {
-      result = this.listDxCuc.filter(item => item.soCongVan = event)
-    } else {
-      result = this.listTongHop.filter(item => item.id = event)
-    }
+  async changSoTh(event) {
+    let result = this.listTongHop.filter(item => item.id = event);
     if (result && result.length > 0) {
-      let detailTh = result[0]
-      let res = await this.dexuatService.getDetail(detailTh.id);
+      let detailTh = result[0];
+      let res = await this.tongHopDxScLon.getDetail(detailTh.id);
       if (res.msg == MESSAGE.SUCCESS) {
         if (res.data) {
-          this.dataTable = []
+          this.dataTable = [];
           const data = res.data;
-          this.dataTable = data.chiTiets;
-          this.tablePaTc = cloneDeep(this.dataTable)
+          this.dataTableReq = data.chiTiets;
+          this.dataTable = this.convertListData(this.dataTableReq);
+          this.tablePaTc = cloneDeep(this.dataTable);
         }
       } else {
-        this.notification.error(MESSAGE.ERROR, res.msg)
+        this.notification.error(MESSAGE.ERROR, res.msg);
       }
     }
   }
 
+  convertListData(table: any[]): any[] {
+    let arr = [];
+    if (table && table.length > 0) {
+      arr = chain(table)
+        .groupBy("tenCuc")
+        .map((value, key) => {
+          let rs = chain(value)
+            .groupBy("tenChiCuc")
+            .map((v, k) => {
+              let rs1 = chain(v)
+                .groupBy("tenKhoi")
+                .map((v1, k1) => {
+                    return {
+                      idVirtual: uuidv4(),
+                      tenKhoi: k1,
+                      dataChild: v1
+                    };
+                  }
+                ).value();
+              return {
+                idVirtual: uuidv4(),
+                tenChiCuc: k,
+                dataChild: rs1
+              };
+            }).value();
+          return {
+            idVirtual: uuidv4(),
+            tenCuc: key,
+            dataChild: rs
+          };
+        }).value();
+    }
+    return arr;
+  }
 
 
 
@@ -183,17 +216,17 @@ export class ThemMoiQdScBtcComponent extends Base2Component implements OnInit {
     if (id > 0) {
       let res = await this.qdScBtcService.getDetail(id);
       const data = res.data;
+      this.maQd = data.soQuyetDinh ? "/" + data.soQuyetDinh.split("/")[1] : null,
       this.helperService.bidingDataInFormGroup(this.formData, data);
       this.formData.patchValue({
-        soQd : data.soQd ? data.soQd.split("/")[0] : ''
-      })
-      this.fileDinhKem = data.fileDinhKems
-      this.tablePaTc = data.phuongAnTc;
-      this.dataTable = data.ctietList;
+        soQuyetDinh: data.soQuyetDinh ? data.soQuyetDinh.split("/")[0] : ""
+      });
+      this.fileDinhKem = data.fileDinhKems;
+      this.dataTableReq = data.chiTiets;
+      this.dataTable = this.convertListData(this.dataTableReq);
+      this.tablePaTc = this.convertListData(data.chiTietDxs);
     }
   }
-
-
 
 
   async save(isOther: boolean) {
@@ -203,16 +236,14 @@ export class ThemMoiQdScBtcComponent extends Base2Component implements OnInit {
       return;
     }
     let body = this.formData.value;
-    body.maDvi = this.userInfo.MA_DVI
-    body.soQd = body.soQd + this.maQd
-    body.fileDinhKems = this.fileDinhKem
-    body.ctietList = this.dataTable
+    body.maDvi = this.userInfo.MA_DVI;
+    body.soQuyetDinh = body.soQuyetDinh + this.maQd;
+    body.fileDinhKems = this.fileDinhKem;
+    body.chiTiets = this.dataTableReq;
     let data = await this.createUpdate(body);
     if (data) {
       if (isOther) {
-        this.approve(data.id, this.STATUS.DA_KY, "Bạn có muốn ký hợp đồng ?")
-      } else {
-        this.goBack()
+        this.approve(data.id, this.STATUS.BAN_HANH, "Bạn có muốn ban hành?");
       }
     }
   }
@@ -220,46 +251,44 @@ export class ThemMoiQdScBtcComponent extends Base2Component implements OnInit {
   xoaItem(index: number) {
     this.modal.confirm({
       nzClosable: false,
-      nzTitle: 'Xác nhận',
-      nzContent: 'Bạn có chắc chắn muốn xóa?',
-      nzOkText: 'Đồng ý',
-      nzCancelText: 'Không',
+      nzTitle: "Xác nhận",
+      nzContent: "Bạn có chắc chắn muốn xóa?",
+      nzOkText: "Đồng ý",
+      nzCancelText: "Không",
       nzOkDanger: true,
       nzWidth: 400,
       nzOnOk: async () => {
         try {
           this.dataTable.splice(index, 1);
         } catch (e) {
-          console.log('error', e);
+          console.log("error", e);
         }
-      },
+      }
     });
   }
 
   chonMaTongHop() {
-    if (!this.isViewDetail && this.typeQd == 'TH') {
-      this.formData.controls['maTh'].setValidators(Validators.required);
-      this.formData.controls['soTt'].clearValidators()
+    if (!this.isViewDetail) {
       let modalQD = this.modal.create({
-        nzTitle: 'DANH SÁCH TỔNG HỢP PHƯƠNG ÁN SỬA CHỮA LỚN CỦA CỤC',
+        nzTitle: "DANH SÁCH QUYẾT ĐỊNH SỬA CHỮA LỚN CỦA TỔNG CỤC",
         nzContent: DialogQdScBtcComponent,
         nzMaskClosable: false,
         nzClosable: false,
-        nzWidth: '700px',
+        nzWidth: "700px",
         nzFooter: null,
         nzComponentParams: {
-          type: "01",
-          listTh : this.listTongHop
-        },
+          type: "00",
+          listTh: this.listTongHop
+        }
       });
       modalQD.afterClose.subscribe(async (data) => {
         if (data) {
           this.formData.patchValue({
-            maTh : data.id
-          })
-          this.changSoTh(data.id, 'TH')
+            soTt: data.soQuyetDinh
+          });
+          this.changSoTh(data.id);
         }
-      })
+      });
     }
   }
 
