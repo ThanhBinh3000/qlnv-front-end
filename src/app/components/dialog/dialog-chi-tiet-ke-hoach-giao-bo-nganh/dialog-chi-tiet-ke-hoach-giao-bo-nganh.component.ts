897ddc91b0870f506b5794b6d291b27ed6fee3f1
othoa <othoa>
2023-06-02 14:03:18 +0700

    fix bug chi tieu ke hoach, luong thuc

--- src/app/components/dialog/dialog-chi-tiet-ke-hoach-giao-bo-nganh/dialog-chi-tiet-ke-hoach-giao-bo-nganh.component.ts
+++ src/app/components/dialog/dialog-chi-tiet-ke-hoach-giao-bo-nganh/dialog-chi-tiet-ke-hoach-giao-bo-nganh.component.ts
@@ -100,7 +100,7 @@ export class DialogChiTietKeHoachGiaoBoNganhComponent implements OnInit {
               this.dsHangHoa = [...this.dsHangHoa, ...item.child]
             });
           } else {
-            this.dsHangHoa = hangHoa.data.filter(item => item.cap == 1);
+            this.dsHangHoa = hangHoa.data.filter(item => item.cap == 2);
           }
         }
       })
