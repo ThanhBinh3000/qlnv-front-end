f4f0197f58a05089f45b3156df21855da128215f
kieunv <kieunv@ttkd6.com>
2023-06-14 15:50:32 +0700

    phe duyet bien ban nghiem thu bao quan lan dau

---
+++ src/app/pages/dieu-chuyen-noi-bo/nhap-dieu-chuyen/tablevel3/bang-ke-can-hang/thong-tin-bang-ke-can-hang/thong-tin-bang-ke-can-hang.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thong-tin-bang-ke-can-hang',
+  templateUrl: './thong-tin-bang-ke-can-hang.component.html',
+  styleUrls: ['./thong-tin-bang-ke-can-hang.component.scss']
+})
+export class ThongTinBangKeCanHangComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/dieu-chuyen-noi-bo/nhap-dieu-chuyen/tablevel3/bien-ban-ket-thuc-nhap-kho/thong-tin-bien-ban-ket-thuc-nhap-kho/thong-tin-bien-ban-ket-thuc-nhap-kho.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thong-tin-bien-ban-ket-thuc-nhap-kho',
+  templateUrl: './thong-tin-bien-ban-ket-thuc-nhap-kho.component.html',
+  styleUrls: ['./thong-tin-bien-ban-ket-thuc-nhap-kho.component.scss']
+})
+export class ThongTinBienBanKetThucNhapKhoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
