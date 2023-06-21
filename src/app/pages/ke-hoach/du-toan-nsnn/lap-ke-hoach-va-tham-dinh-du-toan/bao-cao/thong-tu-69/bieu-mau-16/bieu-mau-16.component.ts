22023791dc9ae86557a362ee4a9f226c2a98c267
datpd <datpd@enao.local>
2023-06-21 10:00:39 +0700

    fix: add file

--- src/app/pages/ke-hoach/du-toan-nsnn/lap-ke-hoach-va-tham-dinh-du-toan/bao-cao/thong-tu-69/bieu-mau-16/bieu-mau-16.component.ts
+++ src/app/pages/ke-hoach/du-toan-nsnn/lap-ke-hoach-va-tham-dinh-du-toan/bao-cao/thong-tu-69/bieu-mau-16/bieu-mau-16.component.ts
@@ -10,7 +10,9 @@ import { LapThamDinhService } from 'src/app/services/quan-ly-von-phi/lapThamDinh
 import { displayNumber, exchangeMoney, getHead, getTail, sortByIndex, sumNumber } from 'src/app/Utility/func';
 import { AMOUNT, BOX_NUMBER_WIDTH, DON_VI_TIEN, LA_MA, MONEY_LIMIT, QUATITY } from "src/app/Utility/utils";
 import * as uuid from "uuid";
-
+import * as fileSaver from 'file-saver';
+import { QuanLyVonPhiService } from 'src/app/services/quanLyVonPhi.service';
+import { NzUploadFile } from 'ng-zorro-antd/upload';
 export class ItemData {
     id!: string;
     stt!: string;
@@ -63,6 +65,28 @@ export class BieuMau16Component implements OnInit {
     isDataAvailable = false;
     //nho dem
     editCache: { [key: string]: { edit: boolean; data: ItemData } } = {};
+    fileList: NzUploadFile[] = [];
+    listFile: File[] = [];                      // list file chua ten va id de hien tai o input
+    lstFiles: any[] = [];
+    listIdDeleteFiles: string[] = [];
+
+    beforeUpload = (file: NzUploadFile): boolean => {
+        this.fileList = this.fileList.concat(file);
+        console.log(this.fileList);
+        return false;
+    };
+
+    // them file vao danh sach
+    handleUpload(): void {
+        console.log(this.fileList);
+
+        this.fileList.forEach((file: any) => {
+            const id = file?.lastModified.toString();
+            this.lstFiles.push({ id: id, fileName: file?.name });
+            this.listFile.push(file);
+        });
+        this.fileList = [];
+    };
 
     constructor(
         private _modalRef: NzModalRef,
@@ -71,6 +95,7 @@ export class BieuMau16Component implements OnInit {
         private lapThamDinhService: LapThamDinhService,
         private notification: NzNotificationService,
         private modal: NzModalService,
+        private quanLyVonPhiService: QuanLyVonPhiService,
     ) {
     }
 
@@ -147,6 +172,28 @@ export class BieuMau16Component implements OnInit {
         }
     }
 
+    //upload file
+    async uploadFile(file: File) {
+        // day file len server
+        const upfile: FormData = new FormData();
+        upfile.append('file', file);
+        upfile.append('folder', this.dataInfo.maDvi + '/' + this.dataInfo.maBcao);
+        const temp = await this.quanLyVonPhiService.uploadFile(upfile).toPromise().then(
+            (data) => {
+                const objfile = {
+                    fileName: data.filename,
+                    fileSize: data.size,
+                    fileUrl: data.url,
+                }
+                return objfile;
+            },
+            err => {
+                this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+            },
+        );
+        return temp;
+    }
+
     // luu
     async save(trangThai: string, lyDoTuChoi: string) {
         let checkSaveEdit;
@@ -189,6 +236,12 @@ export class BieuMau16Component implements OnInit {
         })
 
         const request = JSON.parse(JSON.stringify(this.formDetail));
+        if (!request.fileDinhKems) {
+            request.fileDinhKems = [];
+        }
+        for (const iterator of this.listFile) {
+            request.fileDinhKems.push(await this.uploadFile(iterator));
+        }
         request.lstCtietLapThamDinhs = lstCtietBcaoTemp;
         request.trangThai = trangThai;
 
@@ -404,5 +457,33 @@ export class BieuMau16Component implements OnInit {
     handleCancel() {
         this._modalRef.close();
     }
+
+    deleteFile(id: string): void {
+        this.lstFiles = this.lstFiles.filter((a: any) => a.id !== id);
+        this.listFile = this.listFile.filter((a: any) => a?.lastModified.toString() !== id);
+        this.listIdDeleteFiles.push(id);
+    };
+
+    //download file về máy tính
+    async downloadFile(id: string) {
+        //let file!: File;
+        const file: File = this.listFile.find(element => element?.lastModified.toString() == id);
+        if (!file) {
+            const fileAttach = this.lstFiles.find(element => element?.id == id);
+            if (fileAttach) {
+                await this.quanLyVonPhiService.downloadFile(fileAttach.fileUrl).toPromise().then(
+                    (data) => {
+                        fileSaver.saveAs(data, fileAttach.fileName);
+                    },
+                    err => {
+                        this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+                    },
+                );
+            }
+        } else {
+            const blob = new Blob([file], { type: "application/octet-stream" });
+            fileSaver.saveAs(blob, file.name);
+        }
+    };
 
 }
