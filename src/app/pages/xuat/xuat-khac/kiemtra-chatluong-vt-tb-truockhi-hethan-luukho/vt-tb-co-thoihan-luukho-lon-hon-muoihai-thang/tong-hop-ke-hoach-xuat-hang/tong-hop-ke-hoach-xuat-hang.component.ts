82e0d6a5c339d58670c1a907576ecfe4a753afe3
PM <anhpp@tecapro.com.vn>
2023-06-28 17:41:47 +0700

    update modules xuat khac

---
+++ src/app/pages/xuat/xuat-khac/kiemtra-chatluong-vt-tb-truockhi-hethan-luukho/vt-tb-co-thoihan-luukho-lon-hon-muoihai-thang/tong-hop-ke-hoach-xuat-hang/tong-hop-ke-hoach-xuat-hang.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tong-hop-ke-hoach-xuat-hang',
+  templateUrl: './tong-hop-ke-hoach-xuat-hang.component.html',
+  styleUrls: ['./tong-hop-ke-hoach-xuat-hang.component.scss']
+})
+export class TongHopKeHoachXuatHangComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
