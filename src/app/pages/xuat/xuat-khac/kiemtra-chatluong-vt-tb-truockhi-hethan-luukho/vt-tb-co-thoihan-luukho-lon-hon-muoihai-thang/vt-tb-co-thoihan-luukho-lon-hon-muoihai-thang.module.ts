eb8b8ac06b3839825ba559c29283325ee54a9145
PM <anhpp@tecapro.com.vn>
2023-06-29 11:11:03 +0700

    update modules xuat khac

--- src/app/pages/xuat/xuat-khac/kiemtra-chatluong-vt-tb-truockhi-hethan-luukho/vt-tb-co-thoihan-luukho-lon-hon-muoihai-thang/vt-tb-co-thoihan-luukho-lon-hon-muoihai-thang.module.ts
+++ src/app/pages/xuat/xuat-khac/kiemtra-chatluong-vt-tb-truockhi-hethan-luukho/vt-tb-co-thoihan-luukho-lon-hon-muoihai-thang/vt-tb-co-thoihan-luukho-lon-hon-muoihai-thang.module.ts
@@ -5,6 +5,16 @@ import { TongHopDanhSachComponent } from './tong-hop-danh-sach/tong-hop-danh-sac
 import { KeHoachXuatHangCuaCucComponent } from './ke-hoach-xuat-hang-cua-cuc/ke-hoach-xuat-hang-cua-cuc.component';
 import { TongHopKeHoachXuatHangComponent } from './tong-hop-ke-hoach-xuat-hang/tong-hop-ke-hoach-xuat-hang.component';
 import { KeHoachXuatHangCuaTongCucComponent } from './ke-hoach-xuat-hang-cua-tong-cuc/ke-hoach-xuat-hang-cua-tong-cuc.component';
+import {NzCardModule} from "ng-zorro-antd/card";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzPaginationModule} from "ng-zorro-antd/pagination";
+import {NzSelectModule} from "ng-zorro-antd/select";
+import {DirectivesModule} from "../../../../../directives/directives.module";
+import {NzStatisticModule} from "ng-zorro-antd/statistic";
+import {NzPipesModule} from "ng-zorro-antd/pipes";
+import {MainModule} from "../../../../../layout/main/main.module";
+import {ComponentsModule} from "../../../../../components/components.module";
+
 
 
 
@@ -24,7 +34,12 @@ import { KeHoachXuatHangCuaTongCucComponent } from './ke-hoach-xuat-hang-cua-ton
     KeHoachXuatHangCuaTongCucComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    DirectivesModule,
+    NzStatisticModule,
+    NzPipesModule,
+    MainModule,
+    ComponentsModule,
   ]
 })
 export class VtTbCoThoihanLuukhoLonHonMuoihaiThangModule { }
