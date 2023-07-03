b6d993b15e79ae508cc239b1aa83e5b30e9bb216
PM <anhpp@tecapro.com.vn>
2023-07-03 17:46:41 +0700

    update title button

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  AUTHEN_API: 'http://192.168.1.80:8888',
-  SERVICE_API: 'http://192.168.1.80:8888',
+  AUTHEN_API: 'http://192.168.1.62:8888',
+  SERVICE_API: 'http://192.168.1.62:8888',
 };
