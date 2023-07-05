4c047daf297a6abf31b72751ea338c9db8a6f2e3
haihm <haihm@ttkd6.com>
2023-07-05 17:16:12 +0700

    nhap khac

---
+++ src/app/services/qlnv-hang/nhap-hang/nhap-khac/quanLyPhieuKiemNghiemChatLuongHangKhac.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {environment} from "../../../../../environments/environment";
+import {BaseService} from "../../../base.service";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class QuanLyPhieuKiemNghiemChatLuongHangKhacService extends BaseService {
+  GATEWAY = '';
+
+  constructor(public httpClient: HttpClient) {
+    super(httpClient, 'knghiem-cluong-khac', '');
+  }
+
+}
