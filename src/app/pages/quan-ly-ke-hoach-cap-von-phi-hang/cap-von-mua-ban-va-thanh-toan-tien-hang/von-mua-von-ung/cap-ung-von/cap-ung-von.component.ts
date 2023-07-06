8e27e1b41b4428bf28b1676b50a676e6662ac838
Tran Huy Hoang <hoangth@enao.local>
2023-07-06 17:35:59 +0700

    cap ung von 1.7

---
+++ src/app/pages/quan-ly-ke-hoach-cap-von-phi-hang/cap-von-mua-ban-va-thanh-toan-tien-hang/von-mua-von-ung/cap-ung-von/cap-ung-von.component.ts
@@ -0,0 +1,407 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { NzUploadFile } from 'ng-zorro-antd/upload';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { FileManip, Operator, Roles, Status, Table, Utils } from 'src/app/Utility/utils';
+import { DialogTuChoiComponent } from 'src/app/components/dialog/dialog-tu-choi/dialog-tu-choi.component';
+import { MESSAGE } from 'src/app/constants/message';
+import { MESSAGEVALIDATE } from 'src/app/constants/messageValidate';
+import { CapVonMuaBanTtthService } from 'src/app/services/quan-ly-von-phi/capVonMuaBanTtth.service';
+import { QuanLyVonPhiService } from 'src/app/services/quanLyVonPhi.service';
+import { UserService } from 'src/app/services/user.service';
+import { Globals } from 'src/app/shared/globals';
+import { BtnStatus, CapUng, Const, Report } from '../../cap-von-mua-ban-va-thanh-toan-tien-hang.constant';
+
+@Component({
+    selector: 'app-cap-ung-von',
+    templateUrl: './cap-ung-von.component.html',
+    styleUrls: ['../von-mua-von-ung.component.scss'],
+})
+export class CapUngVonComponent implements OnInit {
+    @Input() dataInfo;
+    @Output() dataChange = new EventEmitter();
+    Status = Status;
+    Op = Operator;
+    Utils = Utils;
+    Const = Const;
+    //thong tin dang nhap
+    userInfo: any;
+    //thong tin chung bao cao
+    baoCao: Report = new Report();
+    lstCtiets: CapUng[] = [];
+    editCache: { [key: string]: { edit: boolean; data: CapUng } } = {};
+    title: string;
+    maDviTien: string = '1';
+    //danh muc
+    donVis: any[] = [];
+    scrollX: string;
+    //trang thai cac nut
+    status: BtnStatus = new BtnStatus();
+    isDataAvailable: boolean = false;
+    editMoneyUnit: boolean = false;
+    isEdit: boolean = true;
+    //file
+    fileList: NzUploadFile[] = [];
+    listFile: File[] = [];
+    listIdDeleteFiles: string[] = [];
+
+    beforeUpload = (file: NzUploadFile): boolean => {
+        this.fileList = this.fileList.concat(file);
+        return false;
+    };
+
+    // them file vao danh sach
+    handleUpload(): void {
+        this.fileList.forEach((file: any) => {
+            const id = file?.lastModified.toString();
+            this.baoCao.lstFiles.push({
+                id: id,
+                fileName: file?.name
+            });
+            this.listFile.push(file);
+        });
+        this.fileList = [];
+    };
+
+    constructor(
+        private quanLyVonPhiService: QuanLyVonPhiService,
+        private capVonMuaBanTtthService: CapVonMuaBanTtthService,
+        private spinner: NgxSpinnerService,
+        private userService: UserService,
+        private notification: NzNotificationService,
+        private modal: NzModalService,
+        public globals: Globals,
+        private fileManip: FileManip,
+    ) { }
+
+    async ngOnInit() {
+        this.action('init');
+    }
+
+    async action(code: string) {
+        this.spinner.show();
+        this.isDataAvailable = false;
+        switch (code) {
+            case 'init':
+                await this.initialization().then(() => {
+                    this.isDataAvailable = true;
+                });
+                break;
+            case 'detail':
+                await this.getDetailReport().then(() => {
+                    this.isDataAvailable = true;
+                });
+                break;
+            case 'save':
+                await this.save().then(() => {
+                    this.isDataAvailable = true;
+                })
+                break;
+            case 'submit':
+                await this.submitReport().then(() => {
+                    this.isDataAvailable = true;
+                })
+                break;
+            case 'nonpass':
+                await this.tuChoi('3').then(() => {
+                    this.isDataAvailable = true;
+                })
+                break;
+            case 'pass':
+                await this.onSubmit('4', null).then(() => {
+                    this.isDataAvailable = true;
+                })
+                break;
+            case 'nonapprove':
+                await this.tuChoi('5').then(() => {
+                    this.isDataAvailable = true;
+                })
+                break;
+            case 'approve':
+                await this.onSubmit('7', null).then(() => {
+                    this.isDataAvailable = true;
+                })
+                break;
+            default:
+                break;
+        }
+        this.spinner.hide();
+    }
+
+    async initialization() {
+        //lay id cua de nghi
+        this.userInfo = this.userService.getUserLogin();
+        if (this.dataInfo?.id) {
+            this.baoCao.id = this.dataInfo?.id;
+            await this.getDetailReport();
+        } else {
+            this.baoCao = this.dataInfo?.baoCao;
+            this.lstCtiets = this.baoCao.lstCtiets;
+        }
+        if (this.baoCao.maLoai == Const.CU_VON_DVCD) {
+            this.title = 'Cấp ứng vốn cho đơn vị cấp dưới';
+        } else {
+            this.title = 'Ghi nhận cấp ứng vốn từ đơn vị cấp trên';
+            this.isEdit = this.userService.isTongCuc();
+        }
+        this.updateEditCache();
+        this.getStatusButton();
+    }
+
+    async getChildUnit() {
+        const request = {
+            maDviCha: this.baoCao.maDvi,
+            trangThai: '01',
+        }
+        await this.quanLyVonPhiService.dmDviCon(request).toPromise().then(
+            data => {
+                if (data.statusCode == 0) {
+                    if (this.userService.isTongCuc()) {
+                        this.donVis = data.data.filter(e => e.tenVietTat && e.tenVietTat.startsWith('CDT'));
+                    } else {
+                        this.donVis = data.data.filter(e => e.tenVietTat && e.tenVietTat.startsWith('CCDT'))
+                    }
+                } else {
+                    this.notification.error(MESSAGE.ERROR, data?.msg);
+                }
+            },
+            (err) => {
+                this.notification.error(MESSAGE.ERROR, MESSAGE.ERROR_CALL_SERVICE);
+            }
+        )
+    }
+
+    //check role cho các nut trinh duyet
+    getStatusButton() {
+        const isChild = this.baoCao.maDvi == this.userInfo?.MA_DVI;
+        this.status.general = Status.check('saveWHist', this.baoCao.trangThai) && isChild;
+        this.status.submit = Status.check('submit', this.baoCao.trangThai) && isChild && !(!this.baoCao.id);
+        this.status.pass = Status.check('pass', this.baoCao.trangThai) && isChild;
+        this.status.approve = Status.check('approve', this.baoCao.trangThai) && isChild;
+        if (this.baoCao.maLoai == Const.CU_VON_DVCD) {
+            this.status.general = this.status.general && this.userService.isAccessPermisson(Roles.CVMB.EDIT_CV);
+            this.status.submit = this.status.submit && this.userService.isAccessPermisson(Roles.CVMB.SUBMIT_CV);
+            this.status.pass = this.status.pass && this.userService.isAccessPermisson(Roles.CVMB.PASS_CV);
+            this.status.approve = this.status.approve && this.userService.isAccessPermisson(Roles.CVMB.APPROVE_CV);
+            this.scrollX = this.status.general ? Table.tableWidth(350, 6, 6, 60) : Table.tableWidth(350, 6, 6, 0);
+        } else {
+            this.status.general = this.status.general && this.userService.isAccessPermisson(Roles.CVMB.EDIT_GNV);
+            this.status.submit = this.status.submit && this.userService.isAccessPermisson(Roles.CVMB.SUBMIT_GNV);
+            this.status.pass = this.status.pass && this.userService.isAccessPermisson(Roles.CVMB.PASS_GNV);
+            this.status.approve = this.status.approve && this.userService.isAccessPermisson(Roles.CVMB.APPROVE_GNV);
+            this.scrollX = Table.tableWidth(350, 6, 6, 0);
+        }
+    }
+
+    back() {
+        const obj = {
+            tabSelected: this.dataInfo?.preTab,
+        }
+        this.dataChange.emit(obj);
+    }
+
+    // call chi tiet bao cao
+    async getDetailReport() {
+        await this.capVonMuaBanTtthService.ctietVonMuaBan(this.baoCao.id).toPromise().then(
+            async (data) => {
+                if (data.statusCode == 0) {
+                    this.baoCao = data.data;
+                    this.lstCtiets = this.baoCao.lstCtiets;
+                    this.listFile = [];
+                    this.updateEditCache();
+                    this.getStatusButton();
+                } else {
+                    this.notification.error(MESSAGE.ERROR, data?.msg);
+                }
+            },
+            (err) => {
+                this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+            },
+        );
+    }
+
+    async submitReport() {
+        this.modal.confirm({
+            nzClosable: false,
+            nzTitle: 'Xác nhận',
+            nzContent: 'Bạn có chắc chắn muốn trình duyệt?<br/>(Trình duyệt trước khi lưu báo cáo có thể gây mất dữ liệu)',
+            nzOkText: 'Đồng ý',
+            nzCancelText: 'Không',
+            nzOkDanger: true,
+            nzWidth: 500,
+            nzOnOk: () => {
+                this.onSubmit(Status.TT_02, '')
+            },
+        });
+    }
+
+    // chuc nang check role
+    async onSubmit(mcn: string, lyDoTuChoi: string) {
+        const requestGroupButtons = {
+            id: this.baoCao.id,
+            maChucNang: mcn,
+            lyDoTuChoi: lyDoTuChoi,
+        };
+        await this.capVonMuaBanTtthService.trinhDuyetVonMuaBan(requestGroupButtons).toPromise().then(async (data) => {
+            if (data.statusCode == 0) {
+                this.baoCao.trangThai = mcn;
+                this.getStatusButton();
+                if (Status.check('reject', mcn)) {
+                    this.notification.success(MESSAGE.SUCCESS, MESSAGE.REJECT_SUCCESS);
+                } else {
+                    this.notification.success(MESSAGE.SUCCESS, MESSAGE.APPROVE_SUCCESS);
+                }
+            } else {
+                this.notification.error(MESSAGE.ERROR, data?.msg);
+            }
+        }, err => {
+            this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+        });
+    }
+
+    //show popup tu choi
+    async tuChoi(mcn: string) {
+        const modalTuChoi = this.modal.create({
+            nzTitle: 'Từ chối',
+            nzContent: DialogTuChoiComponent,
+            nzMaskClosable: false,
+            nzClosable: false,
+            nzWidth: '900px',
+            nzFooter: null,
+            nzComponentParams: {},
+        });
+        modalTuChoi.afterClose.subscribe(async (text) => {
+            if (text) {
+                this.onSubmit(mcn, text);
+            }
+        });
+    }
+
+    // luu
+    async save() {
+        //kiem tra ca trg thon tin nhan da duoc nhap du chua
+        if (this.baoCao.maLoai == Const.GHI_NHAN_CU_VON) {
+            if (!this.baoCao.ngayNhanLenhChuyenCo || !this.baoCao.tkNhan) {
+                this.notification.warning(MESSAGE.WARNING, MESSAGEVALIDATE.NOTEMPTYS);
+                return;
+            }
+            //kiem tra ca ky tu trong tai khoan nhan cos phai deu la so ko
+            if (!Operator.numOnly(this.baoCao.tkNhan)) {
+                this.notification.warning(MESSAGE.WARNING, 'Trường tài khoản nhận chỉ chứa ký tự số');
+                return;
+            }
+        }
+
+        if (this.lstCtiets.some(e => this.editCache[e.id].edit)) {
+            this.notification.warning(MESSAGE.WARNING, MESSAGEVALIDATE.NOTSAVE);
+            return;
+        }
+
+        if (this.lstCtiets.some(e => e.lkCong > Utils.MONEY_LIMIT || e.uncCong > Utils.MONEY_LIMIT)) {
+            this.notification.warning(MESSAGE.WARNING, MESSAGEVALIDATE.MONEYRANGE);
+            return;
+        }
+
+        if (this.listFile.some(file => file.size > Utils.FILE_SIZE)) {
+            this.notification.warning(MESSAGE.WARNING, MESSAGEVALIDATE.OVER_SIZE);
+            return;
+        }
+
+        const lstCtietTemp: CapUng[] = [];
+        this.lstCtiets.forEach(item => {
+            lstCtietTemp.push({
+                ...item,
+                id: item.id?.length == 38 ? null : item.id,
+            })
+        })
+        const request = JSON.parse(JSON.stringify(this.baoCao));
+        request.lstCtiets = lstCtietTemp;
+        request.fileDinhKems = [];
+        for (let iterator of this.listFile) {
+            request.fileDinhKems.push(await this.fileManip.uploadFile(iterator, this.baoCao.maDvi + '/' + this.baoCao.maCapUng));
+        }
+
+        if (!this.baoCao.id) {
+            this.capVonMuaBanTtthService.themMoiVonMuaBan(request).toPromise().then(
+                async (data) => {
+                    if (data.statusCode == 0) {
+                        this.notification.success(MESSAGE.SUCCESS, MESSAGE.ADD_SUCCESS);
+                        this.baoCao.id = data.data.id;
+                        this.action('detail');
+                    } else {
+                        this.notification.error(MESSAGE.ERROR, data?.msg);
+                    }
+                },
+                (err) => {
+                    this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+                },
+            );
+        } else {
+            this.capVonMuaBanTtthService.capNhatVonMuaBan(request).toPromise().then(
+                async (data) => {
+                    if (data.statusCode == 0) {
+                        this.notification.success(MESSAGE.SUCCESS, MESSAGE.UPDATE_SUCCESS);
+                        this.action('detail');
+                    } else {
+                        this.notification.error(MESSAGE.ERROR, data?.msg);
+                    }
+                },
+                (err) => {
+                    this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+                },
+            );
+        }
+    }
+
+    updateEditCache(): void {
+        this.lstCtiets.forEach(item => {
+            this.editCache[item.id] = {
+                edit: false,
+                data: { ...item }
+            };
+        });
+    }
+
+    startEdit(id: string): void {
+        this.editCache[id].edit = true;
+    }
+
+    // huy thay doi
+    cancelEdit(id: string): void {
+        const index = this.lstCtiets.findIndex(item => item.id === id);
+        // lay vi tri hang minh sua
+        this.editCache[id] = {
+            data: { ...this.lstCtiets[index] },
+            edit: false
+        };
+    }
+
+    // luu thay doi
+    saveEdit(id: string): void {
+        const index = this.lstCtiets.findIndex(item => item.id === id); // lay vi tri hang minh sua
+        Object.assign(this.lstCtiets[index], this.editCache[id].data); // set lai data cua lstCtietBcao[index] = this.editCache[id].data
+        this.editCache[id].edit = false; // CHUYEN VE DANG TEXT
+    }
+
+    changeModel(id: string) {
+        const index = this.lstCtiets.findIndex(e => e.id == id);
+        this.editCache[id].data.uncCong = Operator.sum([this.editCache[id].data.uncVonCap, this.editCache[id].data.uncVonUng]);
+        this.editCache[id].data.lkVonUng = Operator.sum([this.lstCtiets[index].lkVonUng, this.editCache[id].data.uncVonUng, -this.lstCtiets[index].uncVonUng]);
+        this.editCache[id].data.lkVonCap = Operator.sum([this.lstCtiets[index].lkVonCap, this.editCache[id].data.uncVonCap, -this.lstCtiets[index].uncVonCap]);
+        this.editCache[id].data.lkCong = Operator.sum([this.editCache[id].data.lkVonCap, this.editCache[id].data.lkVonUng]);
+    }
+
+    // xoa file trong bang file
+    deleteFile(id: string): void {
+        this.baoCao.lstFiles = this.baoCao.lstFiles.filter((a: any) => a.id !== id);
+        this.listFile = this.listFile.filter((a: any) => a?.lastModified.toString() !== id);
+        this.baoCao.listIdDeleteFiles.push(id);
+    }
+
+    async downloadFile(id: string) {
+        let file: any = this.listFile.find(element => element?.lastModified.toString() == id);
+        let doc: any = this.baoCao.lstFiles.find(element => element?.id == id);
+        await this.fileManip.downloadFile(file, doc);
+    }
+}
