ee1e0be92155b68e4ed545588ebd3cc6f946dc71
VuxNT <vunt1809@gmail.com>
2023-07-08 16:18:40 +0700

    Sua chua - trinh tham dinh - quuyet dinh

--- src/app/pages/sua-chua/quyet-dinh/them-moi-qd/them-moi-qd.component.ts
+++ src/app/pages/sua-chua/quyet-dinh/them-moi-qd/them-moi-qd.component.ts
@@ -6,9 +6,12 @@ import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Base3Component } from 'src/app/components/base3/base3.component';
 import { DialogTableSelectionComponent } from 'src/app/components/dialog/dialog-table-selection/dialog-table-selection.component';
-import { DanhMucService } from 'src/app/services/danhmuc.service';
-import { MmHienTrangMmService } from 'src/app/services/mm-hien-trang-mm.service';
 import { StorageService } from 'src/app/services/storage.service';
+import {TrinhThamDinhScService} from "../../../../services/sua-chua/trinhThamDinhSc.service";
+import * as moment from "moment/moment";
+import { cloneDeep, chain } from 'lodash';
+import {STATUS} from "../../../../constants/status";
+import {QuyetDinhScService} from "../../../../services/sua-chua/quyetDinhSc.service";
 
 @Component({
   selector: 'app-them-moi-qd',
@@ -27,23 +30,24 @@ export class ThemMoiQdComponent extends Base3Component implements OnInit {
     modal: NzModalService,
     route: ActivatedRoute,
     router: Router,
-    private hienTrangSv: MmHienTrangMmService,
-    private danhMucSv: DanhMucService,
+    private quyetDinhScService: QuyetDinhScService,
+    private trinhThamDinhScService: TrinhThamDinhScService,
   ) {
-    super(httpClient, storageService, notification, spinner, modal, route, router, hienTrangSv);
+    super(httpClient, storageService, notification, spinner, modal, route, router, quyetDinhScService);
     this.defaultURL = 'sua-chua/quyet-dinh'
     this.getId();
     this.formData = this.fb.group({
-      trangThai: '00',
-      tenTrangThai: 'Dự thảo',
-      soToTrinh: '',
-      ngayTaoHs: '',
-      ngayDuyetLdc: '',
-      thoiHanXuat: '',
-      thoiHanNhap: '',
-      soQdSc: '',
-      trichYeu: '',
-
+      id : [],
+      trangThai: ['00'] ,
+      tenTrangThai: ['Dự thảo'],
+      soQd : [''],
+      soTtr: [],
+      idTtr : [],
+      ngayKy: [''],
+      ngayDuyetLdtc: [''],
+      thoiHanXuat: [''],
+      thoiHanNhap: [''],
+      trichYeu: [''],
     })
   }
 
@@ -52,24 +56,61 @@ export class ThemMoiQdComponent extends Base3Component implements OnInit {
   }
 
   openDialogDanhSach() {
-    const modalQD = this.modal.create({
-      nzTitle: 'Danh sách sửa chữa',
-      nzContent: DialogTableSelectionComponent,
-      nzMaskClosable: false,
-      nzClosable: false,
-      nzWidth: '900px',
-      nzFooter: null,
-      nzComponentParams: {
-        // dataTable: this.listDanhSachTongHop,
-        dataHeader: ['Mã sửa chữa', 'Tên'],
-        dataColumn: ['id', 'noiDungThop']
-      },
-    });
-    modalQD.afterClose.subscribe(async (data) => {
-      // if (data) {
-      //   await this.selectMaTongHop(data.id);
-      // }
-    });
+    if(this.disabled()){
+      return;
+    }
+    this.spinner.show();
+    this.trinhThamDinhScService.getDanhSachQuyetDinh({}).then((res)=>{
+      this.spinner.hide();
+      if(res.data){
+        res.data?.forEach(item => {
+          item.ngayDuyetLdtcFr = moment(item.ngayDuyetLdtc).format('DD/MM/yyyy');
+          item.thoiHanNhapFr = moment(item.thoiHanNhap).format('DD/MM/yyyy');;
+          item.thoiHanXuatFr = moment(item.thoiHanXuat).format('DD/MM/yyyy');
+        })
+        const modalQD = this.modal.create({
+          nzTitle: 'Danh sách sửa chữa',
+          nzContent: DialogTableSelectionComponent,
+          nzMaskClosable: false,
+          nzClosable: false,
+          nzWidth: '900px',
+          nzFooter: null,
+          nzComponentParams: {
+            dataTable: res.data,
+            dataHeader: ['Mã tờ trình','Trích yếu','Ngày duyệt','Thời hạn xuất','Thời hạn nhập'],
+            dataColumn: ['soTtr', 'trichYeu','ngayDuyetLdtcFr','thoiHanXuatFr','thoiHanNhapFr']
+          },
+        });
+        modalQD.afterClose.subscribe(async (data) => {
+          if (data) {
+            this.spinner.show();
+            console.log(data)
+            this.trinhThamDinhScService.getDetail(data.id).then((res)=>{
+              this.spinner.hide();
+              if(res.data){
+                const dataTh = res.data
+                this.formData.patchValue({
+                  soTtr : data.soTtr,
+                  idTtr : data.id,
+                  ngayDuyetLdtc : data.ngayDuyetLdtc,
+                  thoiHanNhap : data.thoiHanNhap,
+                  thoiHanXuat : data.thoiHanXuat,
+                })
+                this.dataTable = chain(dataTh.children).groupBy('scDanhSachHdr.tenChiCuc').map((value, key) => ({
+                    tenDonVi: key,
+                    children: value,
+                  })
+                ).value()
+              }
+            });
+          }
+        });
+      }
+    })
+  }
+
+  disabled(){
+    return this.formData.value.trangThai != STATUS.DU_THAO;
   }
 
 }
