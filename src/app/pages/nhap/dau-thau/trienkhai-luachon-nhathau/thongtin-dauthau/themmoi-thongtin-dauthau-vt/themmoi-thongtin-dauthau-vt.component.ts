1f792cf173d784ac903891e2422922c6a4a42372
DuongVD <duongvd@ttkd6.com>
2023-07-15 18:02:53 +0700

    *qd pd hsmt

---
+++ src/app/pages/nhap/dau-thau/trienkhai-luachon-nhathau/thongtin-dauthau/themmoi-thongtin-dauthau-vt/themmoi-thongtin-dauthau-vt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-themmoi-thongtin-dauthau-vt',
+  templateUrl: './themmoi-thongtin-dauthau-vt.component.html',
+  styleUrls: ['./themmoi-thongtin-dauthau-vt.component.scss']
+})
+export class ThemmoiThongtinDauthauVtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
