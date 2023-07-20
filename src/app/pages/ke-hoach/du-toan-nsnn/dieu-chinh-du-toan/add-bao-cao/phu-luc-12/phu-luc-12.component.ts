dfe7b9f45b1b76b6e5f397ab09b56bcd9359726a
datpd <datpd@enao.local>
2023-07-20 18:34:55 +0700

    fix: 1.5 ngay 20/7

--- src/app/pages/ke-hoach/du-toan-nsnn/dieu-chinh-du-toan/add-bao-cao/phu-luc-12/phu-luc-12.component.ts
+++ src/app/pages/ke-hoach/du-toan-nsnn/dieu-chinh-du-toan/add-bao-cao/phu-luc-12/phu-luc-12.component.ts
@@ -145,9 +145,9 @@ export class PhuLuc12Component implements OnInit {
 				})
 			})
 			this.setLevel();
-			this.lstCtietBcao.forEach(item => {
-				item.tenNoiDung += Utils.getName(item.level, item.maNoiDung);
-			})
+			// this.lstCtietBcao.forEach(item => {
+			// 	item.tenNoiDung += Utils.getName(item.level, item.maNoiDung);
+			// })
 		} else if (!this.lstCtietBcao[0]?.stt) {
 			this.lstCtietBcao.forEach(item => {
 				item.stt = item.maNoiDung;
