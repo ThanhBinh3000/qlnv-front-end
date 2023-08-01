b7102626008d77b1a0983ef2470b40f053b97b26
TTBinh <anhpp@tecapro.com.vn>
2023-08-01 20:09:09 +0700

    xuất kho thanh lý

---
+++ src/app/pages/xuat/xuat-thanh-ly/xuat-kho-thanh-ly/bien-ban-tinh-kho-thanh-ly/bien-ban-tinh-kho-thanh-ly.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bien-ban-tinh-kho-thanh-ly',
+  templateUrl: './bien-ban-tinh-kho-thanh-ly.component.html',
+  styleUrls: ['./bien-ban-tinh-kho-thanh-ly.component.scss']
+})
+export class BienBanTinhKhoThanhLyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
