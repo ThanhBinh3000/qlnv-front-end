6cdab6239d277b425ae258401b32903a86f4f5c3
VuNT <vunt1809@gmail.com>
2023-08-01 10:50:43 +0700

    Luu kho & QL cl

--- src/app/services/luu-kho/theoDoiBqDtl.service.ts
+++ src/app/services/luu-kho/theoDoiBqDtl.service.ts
@@ -6,7 +6,7 @@ import {BaseServiceLocal} from "../baseLocal.service";
 @Injectable({
   providedIn: 'root'
 })
-export class TheoDoiBqDtlService extends BaseServiceLocal {
+export class TheoDoiBqDtlService extends BaseService {
 
   constructor(public httpClient: HttpClient) {
     super(httpClient, 'theo-doi-bao-quan-chi-tiet', '/qlnv-luukho');
