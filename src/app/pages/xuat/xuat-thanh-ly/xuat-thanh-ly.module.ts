aa7b2d076579cba6c89027f0149d64b3049a0fcb
TTBinh <anhpp@tecapro.com.vn>
2023-08-02 11:54:16 +0700

    xuất kho thanh lý

--- src/app/pages/xuat/xuat-thanh-ly/xuat-thanh-ly.module.ts
+++ src/app/pages/xuat/xuat-thanh-ly/xuat-thanh-ly.module.ts
@@ -51,18 +51,19 @@ import {
 import {
   ThongTinHopDongThanhLyComponent
 } from './to-chuc-thuc-hien-thanh-ly/hop-dong-thanh-ly/thong-tin-hop-dong-thanh-ly/thong-tin-hop-dong-thanh-ly.component';
-import { QuyetDinhGiaoNhiemVuThanhLyComponent } from './to-chuc-thuc-hien-thanh-ly/quyet-dinh-giao-nhiem-vu-thanh-ly/quyet-dinh-giao-nhiem-vu-thanh-ly.component';
-import { ChiTietQuyetDinhGiaoNhiemVuThanhLyComponent } from './to-chuc-thuc-hien-thanh-ly/quyet-dinh-giao-nhiem-vu-thanh-ly/chi-tiet-quyet-dinh-giao-nhiem-vu-thanh-ly/chi-tiet-quyet-dinh-giao-nhiem-vu-thanh-ly.component';
-import { XuatKhoThanhLyComponent } from './xuat-kho-thanh-ly/xuat-kho-thanh-ly.component';
-import { PhieuXuatKhoThanhLyComponent } from './xuat-kho-thanh-ly/phieu-xuat-kho-thanh-ly/phieu-xuat-kho-thanh-ly.component';
-import { ChiTietPhieuXuatKhoThanhLyComponent } from './xuat-kho-thanh-ly/phieu-xuat-kho-thanh-ly/chi-tiet-phieu-xuat-kho-thanh-ly/chi-tiet-phieu-xuat-kho-thanh-ly.component';
-import { BangKeCanHangThanhLyComponent } from './xuat-kho-thanh-ly/bang-ke-can-hang-thanh-ly/bang-ke-can-hang-thanh-ly.component';
-import { ChiTietBangCanKeThanhLyComponent } from './xuat-kho-thanh-ly/bang-ke-can-hang-thanh-ly/chi-tiet-bang-can-ke-thanh-ly/chi-tiet-bang-can-ke-thanh-ly.component';
-import { BienBanTinhKhoThanhLyComponent } from './xuat-kho-thanh-ly/bien-ban-tinh-kho-thanh-ly/bien-ban-tinh-kho-thanh-ly.component';
-import { ChiTietBienBanTinhKhoThanhLyComponent } from './xuat-kho-thanh-ly/bien-ban-tinh-kho-thanh-ly/chi-tiet-bien-ban-tinh-kho-thanh-ly/chi-tiet-bien-ban-tinh-kho-thanh-ly.component';
-import { BienBanHaoDoiThanhLyComponent } from './xuat-kho-thanh-ly/bien-ban-hao-doi-thanh-ly/bien-ban-hao-doi-thanh-ly.component';
-import { ChiTietBienBanHaoDoiThanhLyComponent } from './xuat-kho-thanh-ly/bien-ban-hao-doi-thanh-ly/chi-tiet-bien-ban-hao-doi-thanh-ly/chi-tiet-bien-ban-hao-doi-thanh-ly.component';
-
+import {
+  QuyetDinhGiaoNhiemVuThanhLyComponent
+} from './to-chuc-thuc-hien-thanh-ly/quyet-dinh-giao-nhiem-vu-thanh-ly/quyet-dinh-giao-nhiem-vu-thanh-ly.component';
+import {
+  ChiTietQuyetDinhGiaoNhiemVuThanhLyComponent
+} from './to-chuc-thuc-hien-thanh-ly/quyet-dinh-giao-nhiem-vu-thanh-ly/chi-tiet-quyet-dinh-giao-nhiem-vu-thanh-ly/chi-tiet-quyet-dinh-giao-nhiem-vu-thanh-ly.component';
+import {XuatKhoThanhLyComponent} from './xuat-kho-thanh-ly/xuat-kho-thanh-ly.component';
+import {
+  PhieuXuatKhoThanhLyComponent
+} from './xuat-kho-thanh-ly/phieu-xuat-kho-thanh-ly/phieu-xuat-kho-thanh-ly.component';
+import {
+  ChiTietPhieuXuatKhoThanhLyComponent
+} from './xuat-kho-thanh-ly/phieu-xuat-kho-thanh-ly/chi-tiet-phieu-xuat-kho-thanh-ly/chi-tiet-phieu-xuat-kho-thanh-ly.component';
 
 @NgModule({
   declarations: [
@@ -95,12 +96,6 @@ import { ChiTietBienBanHaoDoiThanhLyComponent } from './xuat-kho-thanh-ly/bien-b
     XuatKhoThanhLyComponent,
     PhieuXuatKhoThanhLyComponent,
     ChiTietPhieuXuatKhoThanhLyComponent,
-    BangKeCanHangThanhLyComponent,
-    ChiTietBangCanKeThanhLyComponent,
-    BienBanTinhKhoThanhLyComponent,
-    ChiTietBienBanTinhKhoThanhLyComponent,
-    BienBanHaoDoiThanhLyComponent,
-    ChiTietBienBanHaoDoiThanhLyComponent,
   ],
   imports: [
     CommonModule,
