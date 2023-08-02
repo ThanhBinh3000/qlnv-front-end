ee86b0cb1e42f1338288f69dc658e462c64a456c
ta quang chien <tqchien1990@gmail.com>
2023-08-02 11:51:19 +0700

    xuat thanh ly + bien ban lay mau

--- src/app/pages/xuat/xuat.module.ts
+++ src/app/pages/xuat/xuat.module.ts
@@ -7,9 +7,14 @@ import { ComponentsModule } from 'src/app/components/components.module';
 import { MainModule } from 'src/app/layout/main/main.module';
 import { XuatTieuHuyComponent } from './xuat-tieu-huy/xuat-tieu-huy.component';
 import {XuatTieuHuyModule} from "./xuat-tieu-huy/xuat-tieu-huy.module";
+import { BienBanLayMauComponent } from './kiem-tra-chat-luong/bien-ban-lay-mau/bien-ban-lay-mau.component';
+import { ChiTietBienBanLayMauComponent } from './kiem-tra-chat-luong/bien-ban-lay-mau/chi-tiet-bien-ban-lay-mau/chi-tiet-bien-ban-lay-mau.component';
 
 @NgModule({
-  declarations: [XuatComponent, XuatTieuHuyComponent],
+  declarations: [XuatComponent, XuatTieuHuyComponent, BienBanLayMauComponent, ChiTietBienBanLayMauComponent],
   imports: [CommonModule, XuatRoutingModule, ComponentsModule, MainModule, XuatTieuHuyModule],
+  exports: [
+    BienBanLayMauComponent
+  ]
 })
 export class XuatModule {}
