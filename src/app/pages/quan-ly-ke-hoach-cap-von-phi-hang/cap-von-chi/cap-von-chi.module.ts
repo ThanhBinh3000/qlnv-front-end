967fd4621939622671d20b9015f42acb979501d8
Tran Huy Hoang <hoangth@enao.local>
2023-08-02 18:32:25 +0700

    tao menu cho 2.1

--- src/app/pages/quan-ly-ke-hoach-cap-von-phi-hang/cap-von-chi/cap-von-chi.module.ts
+++ src/app/pages/quan-ly-ke-hoach-cap-von-phi-hang/cap-von-chi/cap-von-chi.module.ts
@@ -1,14 +1,14 @@
-import { ThongTriDuyetYDuToanModule } from './thong-tri-duyet-y-du-toan/thong-tri-duyet-y-du-toan.module';
-import { DeNghiCapVonBoNganhModule } from './de-nghi-cap-von-bo-nganh/de-nghi-cap-von-bo-nganh.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { DirectivesModule } from 'src/app/directives/directives.module';
 import { CapVonChiRoutingModule } from './cap-von-chi-routing.module';
 import { CapVonChiComponent } from './cap-von-chi.component';
-import { TongHopModule } from './tong-hop/tong-hop.module';
+import { DeNghiCapVonBoNganhModule } from './de-nghi-cap-von-bo-nganh/de-nghi-cap-von-bo-nganh.module';
+import { DeNghiCapVonCacDonViModule } from './de-nghi-cap-von-cac-don-vi/de-nghi-cap-von-cac-don-vi.module';
+import { ThongTriDuyetYDuToanModule } from './thong-tri-duyet-y-du-toan/thong-tri-duyet-y-du-toan.module';
 import { TongHopTheoDoiCapVonModule } from './tong-hop-theo-doi-cap-von/tong-hop-theo-doi-cap-von.module';
-import { DeNghiCapVonModule } from './de-nghi-cap-von/de-nghi-cap-von.module';
+import { TongHopModule } from './tong-hop/tong-hop.module';
 // import { TongHopDeNghiCapVonModule } from './tong-hop-de-nghi-cap-von/tong-hop-de-nghi-cap-von.module';
 @NgModule({
   declarations: [
@@ -23,7 +23,8 @@ import { DeNghiCapVonModule } from './de-nghi-cap-von/de-nghi-cap-von.module';
     TongHopModule,
     ThongTriDuyetYDuToanModule,
     TongHopTheoDoiCapVonModule,
-    DeNghiCapVonModule,
+    // DeNghiCapVonModule,
+    DeNghiCapVonCacDonViModule,
     // TongHopDeNghiCapVonModule,
   ],
 })
