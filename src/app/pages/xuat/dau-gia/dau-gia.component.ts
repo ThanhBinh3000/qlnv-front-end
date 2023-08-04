70360c7433268901378d6a13a8d4ab3e0f3aec99
TTBinh <anhpp@tecapro.com.vn>
2023-08-04 17:16:55 +0700

    đề xuất bán đấu giá

--- src/app/pages/xuat/dau-gia/dau-gia.component.ts
+++ src/app/pages/xuat/dau-gia/dau-gia.component.ts
@@ -1,32 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
-import { UserService } from 'src/app/services/user.service';
-import { Globals } from 'src/app/shared/globals';
+import {Component, OnInit} from '@angular/core';
+import {Subject} from 'rxjs';
+import {UserService} from 'src/app/services/user.service';
+import {Globals} from 'src/app/shared/globals';
 import {STATUS} from "../../../constants/status";
 import {cloneDeep} from 'lodash';
+
 @Component({
   selector: 'app-dau-gia',
   templateUrl: './dau-gia.component.html',
   styleUrls: ['./dau-gia.component.scss'],
 })
 export class DauGiaComponent implements OnInit {
+
   isVisibleChangeTab$ = new Subject();
   visibleTab: boolean = true;
+
   constructor(
     public userService: UserService,
     public globals: Globals
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.isVisibleChangeTab$.subscribe((value: boolean) => {
       this.visibleTab = value;
     });
   }
-  tabSelected: number ;
+
+  tabSelected: number;
+
   selectTab(tab: number) {
     this.tabSelected = tab;
-    // this.loaiVthhSelected = '';
   }
+
   checkStatusPermission(data: any, action: any) {
     let mapQuyen = {
       XEM: [
@@ -36,14 +42,18 @@ export class DauGiaComponent implements OnInit {
         STATUS.CHO_DUYET_LDC, STATUS.TU_CHOI_LDC, STATUS.DA_DUYET_LDC,
         STATUS.CHO_DUYET_LDCC, STATUS.DA_DUYET_LDCC, STATUS.TU_CHOI_LDCC,
         STATUS.CHO_DUYET_KTVBQ, STATUS.TU_CHOI_KTVBQ, STATUS.CHO_DUYET_KT, STATUS.TU_CHOI_KT,
-        STATUS.BAN_HANH
+        STATUS.BAN_HANH,
+        STATUS.DA_HOAN_THANH,
+        STATUS.DA_DUYET_CBV,
       ],
-      SUA: [STATUS.DU_THAO, STATUS.TU_CHOI_TP, STATUS.TU_CHOI_LDTC, STATUS.TU_CHOI_LDV, STATUS.TU_CHOI_LDC, STATUS.TU_CHOI_LDCC],
+      SUA: [STATUS.DU_THAO, STATUS.TU_CHOI_TP, STATUS.TU_CHOI_LDTC, STATUS.TU_CHOI_LDV, STATUS.TU_CHOI_LDC, STATUS.TU_CHOI_LDCC, STATUS.TU_CHOI_CBV],
       XOA: [STATUS.DU_THAO],
+      DUYET_BTC: [STATUS.CHODUYET_BTC],
       DUYET_LDTC: [STATUS.CHO_DUYET_LDTC],
       DUYET_TP: [STATUS.CHO_DUYET_TP],
       DUYET_LDV: [STATUS.CHO_DUYET_LDV],
       DUYET_LDC: [STATUS.CHO_DUYET_LDC],
+      DUYET_CBV: [STATUS.DA_DUYET_LDC],
       DUYET_LDCC: [STATUS.CHO_DUYET_LDCC],
       DUYET_KTVBQ: [STATUS.CHO_DUYET_KTVBQ],
       DUYET_KT: [STATUS.CHO_DUYET_KT],
