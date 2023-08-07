0e3772cda833e7a424a46ca5f03764e1aa28798f
datpd <datpd@enao.local>
2023-08-07 16:14:13 +0700

    fix: pl7

--- src/app/pages/ke-hoach/du-toan-nsnn/dieu-chinh-du-toan/add-bao-cao/phu-luc-7/phu-luc-7.component.ts
+++ src/app/pages/ke-hoach/du-toan-nsnn/dieu-chinh-du-toan/add-bao-cao/phu-luc-7/phu-luc-7.component.ts
@@ -329,7 +329,7 @@ export class PhuLuc7Component implements OnInit {
                 item.dkienThienChiPhiTongCong = Operator.sum([item.dkienThienChiPhiTaiCuaKho, item.dkienThienChiPhiNgoaiCuaKho])
 
                 item.tdiemBcaoChiPhiTaiCuaKho = Operator.mul(item.tdiemBcaoLuong, item.dmucChiPhiTaiCuaKho)
-                item.tdiemBcaoChiPhiNgoaiCuaKho = Operator.mul(item.binhQuanChiPhiNgoaiCuaKho, item.dmucChiPhiTaiCuaKho)
+                item.tdiemBcaoChiPhiNgoaiCuaKho = Operator.mul(item.binhQuanChiPhiNgoaiCuaKho, item.tdiemBcaoLuong)
                 item.tdiemBcaoChiPhiTongCong = Operator.sum([item.tdiemBcaoChiPhiTaiCuaKho, item.tdiemBcaoChiPhiNgoaiCuaKho])
 
                 item.ncauDtoan = Operator.sum([item.tdiemBcaoChiPhiTongCong, item.dkienThienChiPhiTongCong])
@@ -349,7 +349,7 @@ export class PhuLuc7Component implements OnInit {
                 item.dkienThienChiPhiTongCong = Operator.sum([item.dkienThienChiPhiTaiCuaKho, item.dkienThienChiPhiNgoaiCuaKho])
 
                 item.tdiemBcaoChiPhiTaiCuaKho = Operator.mul(item.tdiemBcaoLuong, item.dmucChiPhiTaiCuaKho)
-                item.tdiemBcaoChiPhiNgoaiCuaKho = Operator.mul(item.binhQuanChiPhiNgoaiCuaKho, item.dmucChiPhiTaiCuaKho)
+                item.tdiemBcaoChiPhiNgoaiCuaKho = Operator.mul(item.binhQuanChiPhiNgoaiCuaKho, item.tdiemBcaoLuong)
                 item.tdiemBcaoChiPhiTongCong = Operator.sum([item.tdiemBcaoChiPhiTaiCuaKho, item.tdiemBcaoChiPhiNgoaiCuaKho])
 
                 item.ncauDtoan = Operator.sum([item.tdiemBcaoChiPhiTongCong, item.dkienThienChiPhiTongCong])
@@ -658,7 +658,6 @@ export class PhuLuc7Component implements OnInit {
         this.dToanVuGiam = 0;
         this.lstCtietBcao.forEach(item => {
             item.chenhLech = Operator.sum([item.dtoanVuTvqtDnghi, - item.dtoanDnghiDchinh])
-            debugger
             if (item.level == 0) {
                 if (item.dtoanDnghiDchinh && item.dtoanDnghiDchinh !== null) {
                     if (item.dtoanDnghiDchinh < 0) {
