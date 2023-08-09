9bb64e90c835a4f803d9062e7826a5eaee629600
OTHoa <anhpp@tecapro.com.vn>
2023-08-09 09:45:08 +0700

    push code xuat hang khoi danh muc

---
+++ src/app/pages/xuat/xuat-khac/xuat-hang-khoi-danh-muc/quyet-dinh-xuat-hang-khoi-danh-muc/thong-tin-quyet-dinh-xuat-hang-khoi-dm/thong-tin-quyet-dinh-xuat-hang-khoi-dm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thong-tin-quyet-dinh-xuat-hang-khoi-dm',
+  templateUrl: './thong-tin-quyet-dinh-xuat-hang-khoi-dm.component.html',
+  styleUrls: ['./thong-tin-quyet-dinh-xuat-hang-khoi-dm.component.scss']
+})
+export class ThongTinQuyetDinhXuatHangKhoiDmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
