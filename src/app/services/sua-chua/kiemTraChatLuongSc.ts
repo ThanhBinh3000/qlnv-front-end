30764f99221de47576a1df321392b2fd9f3eb819
VuNT <vunt1809@gmail.com>
2023-08-09 17:35:13 +0700

    fixbug SC : Ktra cl

--- src/app/services/sua-chua/kiemTraChatLuongSc.ts
+++ src/app/services/sua-chua/kiemTraChatLuongSc.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BaseService } from '../base.service';
-import {OldResponseData} from "../../interfaces/response";
-import {environment} from "../../../environments/environment";
+import { OldResponseData } from "../../interfaces/response";
+import { environment } from "../../../environments/environment";
+import { BaseServiceLocal } from 'src/environments/baseLocal.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class KiemTraChatLuongScService extends BaseService {
+export class KiemTraChatLuongScService extends BaseServiceLocal {
 
   constructor(public httpClient: HttpClient) {
     super(httpClient, 'sua-chua/kt-cl', '/qlnv-hang');
   }
 
-  getDanhSachTaoQdNh(body): Promise<OldResponseData>{
+  getDanhSachTaoQdNh(body): Promise<OldResponseData> {
     const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/ds-tao-qd-nh`;
     return this._httpClient.post<OldResponseData>(url, body).toPromise();
   }
