558389a42f148c8e08d2f8b3303000fab88ec866
PM <anhpp@tecapro.com.vn>
2023-09-15 16:28:07 +0700

    update code xuat dc

--- src/app/pages/dieu-chuyen-noi-bo/xuat-dieu-chuyen/component-chung/danh-sach-bien-ban-lay-mau/chi-tiet-danh-sach-bien-ban-lay-mau/chi-tiet-danh-sach-bien-ban-lay-mau.component.ts
+++ src/app/pages/dieu-chuyen-noi-bo/xuat-dieu-chuyen/component-chung/danh-sach-bien-ban-lay-mau/chi-tiet-danh-sach-bien-ban-lay-mau/chi-tiet-danh-sach-bien-ban-lay-mau.component.ts
@@ -330,7 +330,10 @@ export class ChiTietDanhSachBienBanLayMau extends Base2Component implements OnIn
         data.danhSachQuyetDinh.forEach(element => {
           if (Array.isArray(element.danhSachKeHoach)) {
             element.danhSachKeHoach.forEach(item => {
-              if (dataChiCuc.findIndex(f => ((!f.maLoKho && !item.maLoKho && item.maNganKho && f.maNganKho == item.maNganKho) || (f.maLoKho && f.maLoKho == item.maLoKho))) < 0) {
+              // if (dataChiCuc.findIndex(f => ((!f.maLoKho && !item.maLoKho && item.maNganKho && f.maNganKho == item.maNganKho) || (f.maLoKho && f.maLoKho == item.maLoKho))) < 0) {
+              //   dataChiCuc.push(item)
+              // }
+              if (item.thayDoiThuKho === this.thayDoiThuKho) {
                 dataChiCuc.push(item)
               }
             });
@@ -338,7 +341,8 @@ export class ChiTietDanhSachBienBanLayMau extends Base2Component implements OnIn
         });
       }
       if (dataChiCuc) {
-        this.listDiaDiemNhap = cloneDeep(dataChiCuc);
+        // this.listDiaDiemNhap = cloneDeep(dataChiCuc);
+        this.listDiaDiemNhap = dataChiCuc.map(f => ({ ...f, noiNhan: `${f.tenDiemKhoNhan || ""} - ${f.tenNhaKhoNhan || ""} - ${f.tenNganKhoNhan || ""} - ${f.tenLoKhoNhan || ""}` }));
       }
     } catch (error) {
       console.log("error", error);
@@ -377,12 +381,12 @@ export class ChiTietDanhSachBienBanLayMau extends Base2Component implements OnIn
       nzContent: DialogTableSelectionComponent,
       nzMaskClosable: false,
       nzClosable: false,
-      nzWidth: '900px',
+      nzWidth: '1200px',
       nzFooter: null,
       nzComponentParams: {
         dataTable: this.listDiaDiemNhap,
-        dataHeader: ['Điểm kho', 'Nhà kho', 'Ngăn kho', 'Lô kho'],
-        dataColumn: ['tenDiemKho', 'tenNhaKho', 'tenNganKho', 'tenLoKho']
+        dataHeader: ['Điểm kho xuất', 'Nhà kho xuất', 'Ngăn kho xuất', 'Lô kho xuất', 'Nơi nhận'],
+        dataColumn: ['tenDiemKho', 'tenNhaKho', 'tenNganKho', 'tenLoKho', 'noiNhan']
       },
     });
     modalQD.afterClose.subscribe(async (data) => {
