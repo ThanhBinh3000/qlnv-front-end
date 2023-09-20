6a90c4c8ab56c84989570fee30096e9be11e0e7e
OTHoa <anhpp@tecapro.com.vn>
2023-09-20 15:07:26 +0700

    update điều chỉnh chỉ tiêu kế hoạch

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,17 +2,17 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// export const environment = {
-//   production: false,
-//   AUTHEN_API: 'http://192.168.1.80:8888',
-//   SERVICE_API: 'http://192.168.1.80:8888',
-// };
-
 export const environment = {
   production: false,
-  AUTHEN_API: 'http://192.168.1.62:8888',
-  SERVICE_API: 'http://192.168.1.62:8888',
+  AUTHEN_API: 'http://192.168.1.80:8888',
+  SERVICE_API: 'http://192.168.1.80:8888',
 };
+
+// export const environment = {
+//   production: false,
+//   AUTHEN_API: 'http://192.168.1.62:8888',
+//   SERVICE_API: 'http://192.168.1.62:8888',
+// };
 
 /*
  * For easier debugging in development mode, you can import the following file
