5505e209953f22269513f5f558efe9418a166083
VuxNT <vunt1809@gmail.com>
2023-09-21 11:18:56 +0700

    fix xuat thanh ly

--- src/app/services/qlnv-hang/xuat-hang/xuat-thanh-ly/HoSoThanhLy.service.ts
+++ src/app/services/qlnv-hang/xuat-hang/xuat-thanh-ly/HoSoThanhLy.service.ts
@@ -1,6 +1,6 @@
-import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {BaseService} from "../../../base.service";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BaseService } from "../../../base.service";
 
 @Injectable({
   providedIn: 'root'
