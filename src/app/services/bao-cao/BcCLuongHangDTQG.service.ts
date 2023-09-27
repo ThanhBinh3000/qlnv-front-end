723565c76fde85df6b986388d88604ce50087d79
cuongnv <cuongnv@tecapro.com.vn>
2023-09-27 10:08:48 +0700

    fix bug quản lý kho

--- src/app/services/bao-cao/BcCLuongHangDTQG.service.ts
+++ src/app/services/bao-cao/BcCLuongHangDTQG.service.ts
@@ -7,45 +7,44 @@ import { environment } from "../../../environments/environment";
   providedIn: 'root',
 })
 export class BcCLuongHangDTQGService extends BaseService {
-  // GATEWAY = '/qlnv-report';
-  GATEWAY = '';
+  GATEWAY = '/qlnv-report';
 
   constructor(public httpClient: HttpClient) {
     super(httpClient, 'bc-cl-hang-dtqg', '');
   }
 
   baoCaoSLuongCLuongCcdc(body) {
-    const url = `${environment.SERVICE_API_LOCAL}${this.GATEWAY}/${this.table}/bc-sl-cl-ccdc`;
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/bc-sl-cl-ccdc`;
     return this._httpClient.post(url, body, { responseType: 'blob' }).toPromise();
   }
 
   baoCaoSLuongCLuongMmTbcd(body) {
-    const url = `${environment.SERVICE_API_LOCAL}${this.GATEWAY}/${this.table}/bc-sl-cl-mm-tbcd`;
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/bc-sl-cl-mm-tbcd`;
     return this._httpClient.post(url, body, { responseType: 'blob' }).toPromise();
   }
 
   baoCaoCongTacBqHangDtqg(body) {
-    const url = `${environment.SERVICE_API_LOCAL}${this.GATEWAY}/${this.table}/bccl-ct-bq-hangdtqg`;
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/bccl-ct-bq-hangdtqg`;
     return this._httpClient.post(url, body, { responseType: 'blob' }).toPromise();
   }
 
   bcclNhapHangDtqg(body) {
-    const url = `${environment.SERVICE_API_LOCAL}${this.GATEWAY}/${this.table}/bc-cl-hang-dtqg-nhap-kho`;
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/bc-cl-hang-dtqg-nhap-kho`;
     return this._httpClient.post(url, body, { responseType: 'blob' }).toPromise();
   }
 
   bcclXuatHangDtqg(body) {
-    const url = `${environment.SERVICE_API_LOCAL}${this.GATEWAY}/${this.table}/bc-cl-hang-dtqg-xuat-kho`;
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/bc-cl-hang-dtqg-xuat-kho`;
     return this._httpClient.post(url, body, { responseType: 'blob' }).toPromise();
   }
 
   bcclHangHaoHut(body) {
-    const url = `${environment.SERVICE_API_LOCAL}${this.GATEWAY}/${this.table}/bc-cl-hao-hut-hang-dtqg`;
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/bc-cl-hao-hut-hang-dtqg`;
     return this._httpClient.post(url, body, { responseType: 'blob' }).toPromise();
   }
 
   bcCongTacKtkt(body) {
-    const url = `${environment.SERVICE_API_LOCAL}${this.GATEWAY}/${this.table}/bc-congtac-qly-ktkt`;
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/bc-congtac-qly-ktkt`;
     return this._httpClient.post(url, body, { responseType: 'blob' }).toPromise();
   }
 
