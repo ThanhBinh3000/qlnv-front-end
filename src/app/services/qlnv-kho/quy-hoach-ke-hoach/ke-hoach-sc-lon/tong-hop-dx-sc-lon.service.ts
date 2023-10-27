79b3e8d784f81bb2b0b3baff8c4daa96b7438d16
VuxNT <vunt1809@gmail.com>
2023-10-27 17:18:11 +0700

    fix xuat thanh ly

--- src/app/services/qlnv-kho/quy-hoach-ke-hoach/ke-hoach-sc-lon/tong-hop-dx-sc-lon.service.ts
+++ src/app/services/qlnv-kho/quy-hoach-ke-hoach/ke-hoach-sc-lon/tong-hop-dx-sc-lon.service.ts
@@ -7,7 +7,7 @@ import { BaseService } from "../../../base.service";
 @Injectable({
   providedIn: 'root'
 })
-export class TongHopDxScLonService extends BaseService{
+export class TongHopDxScLonService extends BaseService {
 
   GATEWAY = '/qlnv-kho';
   constructor(public httpClient: HttpClient) {
@@ -16,6 +16,11 @@ export class TongHopDxScLonService extends BaseService{
 
   tongHop(body): Promise<OldResponseData> {
     const url = `${environment.SERVICE_API}${this.GATEWAY}/kt-kh-sc-lon/tong-hop/danh-sach-dx`;
+    return this._httpClient.post<OldResponseData>(url, body).toPromise();
+  }
+
+  getListThTaoQdGiaoNv(body){
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/kt-kh-sc-lon/tong-hop/ds-tao-qd-gnv`;
     return this._httpClient.post<OldResponseData>(url, body).toPromise();
   }
 }
