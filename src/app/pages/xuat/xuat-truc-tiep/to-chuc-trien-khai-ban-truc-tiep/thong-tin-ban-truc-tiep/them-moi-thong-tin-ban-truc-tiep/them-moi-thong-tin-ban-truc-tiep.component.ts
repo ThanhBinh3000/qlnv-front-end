774cc932e34a4bf02d1d75936f94ac871285efea
TTBinh <anhpp@tecapro.com.vn>
2023-11-07 10:51:22 +0700

    hiển thị trường mô tả hàng hóa

--- src/app/pages/xuat/xuat-truc-tiep/to-chuc-trien-khai-ban-truc-tiep/thong-tin-ban-truc-tiep/them-moi-thong-tin-ban-truc-tiep/them-moi-thong-tin-ban-truc-tiep.component.ts
+++ src/app/pages/xuat/xuat-truc-tiep/to-chuc-trien-khai-ban-truc-tiep/thong-tin-ban-truc-tiep/them-moi-thong-tin-ban-truc-tiep/them-moi-thong-tin-ban-truc-tiep.component.ts
@@ -191,6 +191,7 @@ export class ThemMoiThongTinBanTrucTiepComponent extends Base2Component implemen
         fileUyQuyen: this.fileUyQuyen,
         fileBanLe: this.fileBanLe,
       };
+      body.id = 0;
       await this.createUpdate(body);
     } catch (e) {
       console.error('Error: ', e);
