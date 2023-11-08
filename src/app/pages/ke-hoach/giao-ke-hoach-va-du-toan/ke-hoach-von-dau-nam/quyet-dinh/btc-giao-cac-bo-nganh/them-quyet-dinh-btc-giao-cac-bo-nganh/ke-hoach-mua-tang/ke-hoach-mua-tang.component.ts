2d4aaa3116cae2ed7bfcab7fee89e72998a02dec
OTHoa <anhpp@tecapro.com.vn>
2023-11-08 14:09:59 +0700

    fix lỗi vkh sau UAT

--- src/app/pages/ke-hoach/giao-ke-hoach-va-du-toan/ke-hoach-von-dau-nam/quyet-dinh/btc-giao-cac-bo-nganh/them-quyet-dinh-btc-giao-cac-bo-nganh/ke-hoach-mua-tang/ke-hoach-mua-tang.component.ts
+++ src/app/pages/ke-hoach/giao-ke-hoach-va-du-toan/ke-hoach-von-dau-nam/quyet-dinh/btc-giao-cac-bo-nganh/them-quyet-dinh-btc-giao-cac-bo-nganh/ke-hoach-mua-tang/ke-hoach-mua-tang.component.ts
@@ -7,16 +7,16 @@ import {
   DoCheck,
   IterableDiffers,
   OnChanges,
-  SimpleChanges
+  SimpleChanges,
 } from '@angular/core';
-import {NzModalService} from 'ng-zorro-antd/modal';
-import {NzNotificationService} from 'ng-zorro-antd/notification';
-import {MESSAGE} from 'src/app/constants/message';
-import {KeHoachMuaXuat} from 'src/app/models/DeXuatKeHoachuaChonNhaThau';
-import {DanhMucService} from 'src/app/services/danhmuc.service';
-import {Globals} from './../../../../../../../../shared/globals';
-import {STATUS} from 'src/app/constants/status';
-import {AMOUNT_ONE_DECIMAL} from "../../../../../../../../Utility/utils";
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { MESSAGE } from 'src/app/constants/message';
+import { KeHoachMuaXuat } from 'src/app/models/DeXuatKeHoachuaChonNhaThau';
+import { DanhMucService } from 'src/app/services/danhmuc.service';
+import { Globals } from './../../../../../../../../shared/globals';
+import { STATUS } from 'src/app/constants/status';
+import { AMOUNT_ONE_DECIMAL } from '../../../../../../../../Utility/utils';
 
 @Component({
   selector: 'app-ke-hoach-mua-tang',
@@ -46,7 +46,7 @@ export class KeHoachMuaTangComponent implements OnInit, OnChanges {
     private modal: NzModalService,
     private danhMucService: DanhMucService,
     private notification: NzNotificationService,
-    public globals: Globals
+    public globals: Globals,
   ) {
   }
 
@@ -65,21 +65,21 @@ export class KeHoachMuaTangComponent implements OnInit, OnChanges {
   }
 
   async getListDmLoaiChi() {
-    let res = await this.danhMucService.danhMucChungGetAll("DM_ND_DT");
+    let res = await this.danhMucService.danhMucChungGetAll('DM_ND_DT');
     if (res.msg == MESSAGE.SUCCESS) {
       this.dsNoiDung = res.data;
     }
   }
 
   async getListNguonVon() {
-    let res = await this.danhMucService.danhMucChungGetAll("NGUON_VON");
+    let res = await this.danhMucService.danhMucChungGetAll('NGUON_VON');
     if (res.msg == MESSAGE.SUCCESS) {
       this.dsNguonVon = res.data;
     }
   }
 
   emitDataTable() {
-    this.dataTableChange.emit(this.dataTable)
+    this.dataTableChange.emit(this.dataTable);
   }
 
 
@@ -111,19 +111,19 @@ export class KeHoachMuaTangComponent implements OnInit, OnChanges {
   themMoiItem() {
     if (this.rowItem.loaiChi && this.rowItem.sluongDtoan != null) {
       this.rowItem.idDanhMuc = +this.rowItem.idDanhMuc;
-      this.dataTable = [...this.dataTable, this.rowItem]
+      this.dataTable = [...this.dataTable, this.rowItem];
 
       this.updateEditCache();
       this.emitDataTable();
       // Validate tổng dự toán
-      if (this.rowItem.loaiChi == "DT01" && this.rowItem.sluongDtoan > this.tongGiaTri && this.calcTong()) {
+      if (this.rowItem.loaiChi == 'DT01' && this.rowItem.sluongDtoan > this.tongGiaTri && this.calcTong()) {
         this.dataTable.splice(this.lastIndex - 1, 1);
-        this.notification.error(MESSAGE.ERROR, "Dự toán chi DTQG không được lớn hơn kế hoạch chỉ tiêu của Thủ tướng chính phủ");
+        this.notification.error(MESSAGE.ERROR, 'Dự toán chi DTQG không được lớn hơn kế hoạch chỉ tiêu của Thủ tướng chính phủ (' + Intl.NumberFormat('vi-VN').format(this.tongGiaTri) + ') triệu đồng');
         return;
       }
       this.rowItem = new KeHoachMuaXuat();
     } else {
-      this.notification.error(MESSAGE.ERROR, "Vui lòng điền đầy đủ thông tin")
+      this.notification.error(MESSAGE.ERROR, 'Vui lòng điền đầy đủ thông tin');
     }
 
   }
@@ -134,22 +134,22 @@ export class KeHoachMuaTangComponent implements OnInit, OnChanges {
   huyEdit(id: number): void {
     const index = this.dataTable.findIndex((item) => item.idVirtual == id);
     this.dataEdit[id] = {
-      data: {...this.dataTable[index]},
+      data: { ...this.dataTable[index] },
       edit: false,
     };
   }
 
   luuEdit(index: number): void {
     this.hasError.emit(false);
-    let beforeData = {...this.dataTable[index]};
+    let beforeData = { ...this.dataTable[index] };
     Object.assign(this.dataTable[index], this.dataEdit[index].data);
     this.emitDataTable();
     // Validate tổng dự toán
-    if (this.dataTable[index].loaiChi == "DT01" && this.dataTable[index].sluongDtoan > this.tongGiaTri && this.calcTong()) {
+    if (this.dataTable[index].loaiChi == 'DT01' && this.dataTable[index].sluongDtoan > this.tongGiaTri && this.calcTong()) {
       Object.assign(this.dataTable[index], beforeData);
       // this.dataTable.splice(this.lastIndex - 1, 1);
-      this.hasError.emit(true)
-      this.notification.error(MESSAGE.ERROR, "Tổng Dự toán không được lớn hơn Tổng kế hoạch chi DTQG theo QĐ của TTCP");
+      this.hasError.emit(true);
+      this.notification.warning(MESSAGE.WARNING, 'Tổng Dự toán không được lớn hơn Tổng kế hoạch chi DTQG theo QĐ của TTCP');
     } else {
       this.dataEdit[index].edit = false;
     }
@@ -159,15 +159,15 @@ export class KeHoachMuaTangComponent implements OnInit, OnChanges {
     if (this.dataTable) {
       let i = 0;
       this.dataTable.forEach((item) => {
-        const dataNd = this.dsNoiDung.filter(d => d.id == item.idDanhMuc)
+        const dataNd = this.dsNoiDung.filter(d => d.id == item.idDanhMuc);
         if (dataNd.length > 0) {
           item.noiDung = dataNd[0].noiDung;
         }
         this.dataEdit[i] = {
           edit: false,
-          data: {...item},
+          data: { ...item },
         };
-        i++
+        i++;
         // Validate tổng dự toán
         this.lastIndex = i;
       });
@@ -175,7 +175,7 @@ export class KeHoachMuaTangComponent implements OnInit, OnChanges {
   }
 
   async onChangeNoiDung(loaiChi, typeData?) {
-    const dataNd = this.dsNoiDung.find(d => d.ma == loaiChi)
+    const dataNd = this.dsNoiDung.find(d => d.ma == loaiChi);
     if (typeData) {
       typeData.tenLoaiChi = dataNd ? dataNd.giaTri : null;
     } else {
