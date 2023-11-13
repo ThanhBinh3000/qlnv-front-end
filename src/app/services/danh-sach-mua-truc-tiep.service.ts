a5245e3339de400ad60da97bc87c15916e44fbac
haihm <haihm@ttkd6.com>
2023-11-13 14:51:33 +0700

    fix bug nhap tt + khac +  bao cao

--- src/app/services/danh-sach-mua-truc-tiep.service.ts
+++ src/app/services/danh-sach-mua-truc-tiep.service.ts
@@ -4,26 +4,25 @@ import { BaseService } from './base.service';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { OldResponseData } from '../interfaces/response';
-import {BaseLocalService} from "./base-local.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class DanhSachMuaTrucTiepService extends BaseLocalService {
-  GATEWAY = '';
+export class DanhSachMuaTrucTiepService extends BaseService {
+  GATEWAY = '/qlnv-hang';
 
   constructor(public httpClient: HttpClient
   ) {
-    super(httpClient, 'mua-truc-tiep/dx-kh-mtt', '');
+    super(httpClient, 'mua-truc-tiep/dx-kh-mtt', '/qlnv-hang');
   }
 
   updateStatus(body: any): Promise<any> {
-    const url = `${environment.SERVICE_API_LOCAL}${this.GATEWAY}/mua-truc-tiep/dx-kh-mtt/phe-duyet`;
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/mua-truc-tiep/dx-kh-mtt/phe-duyet`;
     return this.httpClient.post(url, body).toPromise();
   }
 
   getSoLuongAdded(body): Promise<OldResponseData> {
-    const url = `${environment.SERVICE_API_LOCAL}${this.GATEWAY}/${this.table}/count-sl-kh`;
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/count-sl-kh`;
     return this._httpClient.post<OldResponseData>(url, body).toPromise();
   }
 
