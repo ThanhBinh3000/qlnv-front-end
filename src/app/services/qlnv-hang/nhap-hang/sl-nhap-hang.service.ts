a5e90a4d0d4ed73df14d7579db633ca52d26431c
haihm <haihm@ttkd6.com>
2023-11-13 14:45:53 +0700

    fix bug nhap tt + khac +  bao cao

--- src/app/services/qlnv-hang/nhap-hang/sl-nhap-hang.service.ts
+++ src/app/services/qlnv-hang/nhap-hang/sl-nhap-hang.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {BaseService} from "../../base.service";
+import {OldResponseData} from "../../../interfaces/response";
+import {environment} from "../../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,16 @@ export class SoLuongNhapHangService extends BaseService {
   GATEWAY = '/qlnv-hang';
   constructor(public httpClient: HttpClient) {
     super(httpClient, 'sl-nhap-hang', '/qlnv-hang');
+  }
+
+  countSoLuongKeHoachNam(body): Promise<OldResponseData> {
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/count-sl-ctkh-qd`;
+    return this._httpClient.post<OldResponseData>(url, body).toPromise();
+  }
+
+  countSoLuongKeHoachNamTheoKh(body): Promise<OldResponseData> {
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/count-sl-ctkh-kh`;
+    return this._httpClient.post<OldResponseData>(url, body).toPromise();
   }
 
 }
