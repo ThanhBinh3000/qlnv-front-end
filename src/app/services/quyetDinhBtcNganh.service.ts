beb3676b9d1d7ad603c6901746e5f7045e4a12e6
cuongnv <cuongnv@tecapro.com.vn>
2023-11-14 17:40:02 +0700

    phuong an gia fix bug

--- src/app/services/quyetDinhBtcNganh.service.ts
+++ src/app/services/quyetDinhBtcNganh.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { BaseService } from './base.service';
-import {OldResponseData} from "../interfaces/response";
 
 @Injectable({
   providedIn: 'root',
@@ -14,11 +13,6 @@ export class QuyetDinhBtcNganhService extends BaseService {
 
   constructor(public httpClient: HttpClient) {
     super(httpClient, 'giao-chi-tieu-von-dau-nam/quyet-dinh/btc-nganh', '/qlnv-khoach');
-  }
-
-  preview(body) {
-    const url = `${environment.SERVICE_API_LOCAL}/${this.table}/xem-truoc`;
-    return this._httpClient.post<OldResponseData>(url, body).toPromise();
   }
 
 }
