ac7ce461b229560e5dae7bdff3e50518f8ea4f3d
cuongnv <cuongnv@tecapro.com.vn>
2023-11-14 17:34:34 +0700

    phuong an gia fix bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  AUTHEN_API: '/api',
-  SERVICE_API: '/api',
+  AUTHEN_API: 'http://192.168.1.62:8888',
+  SERVICE_API: 'http://192.168.1.62:8888',
 };
