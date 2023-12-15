0ee9ddcd6cd906b2784eb23215330c19af411389
OTHoa <anhpp@tecapro.com.vn>
2023-12-15 17:03:09 +0700

    add báo cáo quản trị vụ qlhdt

--- src/app/pages/khai-thac-bao-cao/bao-cao-nhap-xuat-hang-dtqg/bao-cao-nhap-xuat-hang-dtqg.module.ts
+++ src/app/pages/khai-thac-bao-cao/bao-cao-nhap-xuat-hang-dtqg/bao-cao-nhap-xuat-hang-dtqg.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { BaoCaoNhapXuatHangDtqgRoutingModule } from './bao-cao-nhap-xuat-hang-dtqg-routing.module';
+import { BaoCaoNhapXuatHangDtqgComponent } from './bao-cao-nhap-xuat-hang-dtqg.component';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { ComponentsModule } from '../../../components/components.module';
+import { MainModule } from '../../../layout/main/main.module';
+import { NzTreeViewModule } from 'ng-zorro-antd/tree-view';
+import { DirectivesModule } from '../../../directives/directives.module';
+import { BcTienDoNhapGaoTheoGoiThauComponent } from './bc-tien-do-nhap-gao-theo-goi-thau/bc-tien-do-nhap-gao-theo-goi-thau.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BaoCaoNhapXuatHangDtqgComponent,
+    BcTienDoNhapGaoTheoGoiThauComponent
+  ],
   imports: [
     CommonModule,
-    BaoCaoNhapXuatHangDtqgRoutingModule
-  ]
+    BaoCaoNhapXuatHangDtqgRoutingModule,
+    ComponentsModule,
+    MainModule,
+    NzTreeViewModule,
+    DirectivesModule,
+    NzIconModule
+  ],
 })
 export class BaoCaoNhapXuatHangDtqgModule { }
