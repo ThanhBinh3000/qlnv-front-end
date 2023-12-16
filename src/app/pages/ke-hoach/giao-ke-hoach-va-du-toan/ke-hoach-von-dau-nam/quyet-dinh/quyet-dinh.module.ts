22f3c1c0eef261902d2490d1419d8a68b7aaff25
OTHoa <anhpp@tecapro.com.vn>
2023-12-16 15:46:11 +0700

    add qd tcdt giao don vi

--- src/app/pages/ke-hoach/giao-ke-hoach-va-du-toan/ke-hoach-von-dau-nam/quyet-dinh/quyet-dinh.module.ts
+++ src/app/pages/ke-hoach/giao-ke-hoach-va-du-toan/ke-hoach-von-dau-nam/quyet-dinh/quyet-dinh.module.ts
@@ -6,9 +6,12 @@ import { DirectivesModule } from 'src/app/directives/directives.module';
 import { TtcpModule } from './ttcp/ttcp.module';
 import { BtcGiaoCacBoNganhModule } from './btc-giao-cac-bo-nganh/btc-giao-cac-bo-nganh.module';
 import { BtcGiaoTcdtModule } from './btc-giao-tcdt/btc-giao-tcdt.module';
+import { TcdtGiaoCacDonviComponent } from './tcdt-giao-cac-donvi/tcdt-giao-cac-donvi.component';
+import { ThongTinTcdtGiaoCacDonviComponent } from './tcdt-giao-cac-donvi/thong-tin-tcdt-giao-cac-donvi/thong-tin-tcdt-giao-cac-donvi.component';
+import { ThemSuaVatTuChiTietComponent } from './tcdt-giao-cac-donvi/thong-tin-tcdt-giao-cac-donvi/them-sua-vat-tu-chi-tiet/them-sua-vat-tu-chi-tiet.component';
 
 @NgModule({
-  declarations: [QuyetDinhComponent],
+  declarations: [QuyetDinhComponent, TcdtGiaoCacDonviComponent, ThongTinTcdtGiaoCacDonviComponent, ThemSuaVatTuChiTietComponent],
   imports: [
     CommonModule,
     ComponentsModule,
