87b17cc7c9ff35efaefa6f00818803ad308748ff
thonq <thongtv@tecapro.com.vn>
2023-12-19 18:25:38 +0700

    Báo cáo nhập xuất tồn thóc gạo muối

---
+++ src/app/pages/khai-thac-bao-cao/bao-cao-nhap-xuat-hang-dtqg/bao-cao-nhap-xuat-ton/bao-cao-nhap-xuat-ton.component.ts
@@ -0,0 +1,135 @@
+import { Component, OnInit } from '@angular/core';
+import { Base2Component } from '../../../../components/base2/base2.component';
+import { HttpClient } from '@angular/common/http';
+import { StorageService } from '../../../../services/storage.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { BcCLuongHangDTQGService } from '../../../../services/bao-cao/BcCLuongHangDTQG.service';
+import { UserService } from '../../../../services/user.service';
+import { DonviService } from '../../../../services/donvi.service';
+import { DanhMucService } from '../../../../services/danhmuc.service';
+import { Globals } from '../../../../shared/globals';
+import * as dayjs from 'dayjs';
+import {saveAs} from "file-saver";
+import { Validators } from '@angular/forms';
+import { MESSAGE } from '../../../../constants/message';
+import { BcNhapXuatMuaBanHangDTQGService } from '../../../../services/bao-cao/BcNhapXuatMuaBanHangDTQG.service';
+
+@Component({
+  selector: 'app-bao-cao-nhap-xuat-ton',
+  templateUrl: './bao-cao-nhap-xuat-ton.component.html',
+  styleUrls: ['./bao-cao-nhap-xuat-ton.component.scss']
+})
+export class BaoCaoNhapXuatTonComponent extends Base2Component implements OnInit {
+  pdfSrc: any;
+  excelSrc: any;
+  pdfBlob: any;
+  excelBlob: any;
+  nameFile: any;
+
+  constructor(httpClient: HttpClient,
+              storageService: StorageService,
+              notification: NzNotificationService,
+              spinner: NgxSpinnerService,
+              modal: NzModalService,
+              private bcNhapXuatMuaBanHangDTQGService: BcNhapXuatMuaBanHangDTQGService,
+              public userService: UserService,
+              private donViService: DonviService,
+              private danhMucSv: DanhMucService,
+              public globals: Globals) {
+    super(httpClient, storageService, notification, spinner, modal, bcNhapXuatMuaBanHangDTQGService);
+    this.formData = this.fb.group(
+      {
+        nam: [dayjs().get("year"), [Validators.required]],
+        ngayBatDau: '',
+        ngayKetThuc: '',
+      }
+    );
+  }
+  disabledStartNgay = (startValue: Date): boolean => {
+    if (startValue && this.formData.value.ngayKetThuc) {
+      return startValue.getTime() > this.formData.value.ngayKetThuc.getTime();
+    }
+    return false;
+  };
+
+  disabledEndNgay = (endValue: Date): boolean => {
+    if (!endValue || !this.formData.value.ngayBatDau) {
+      return false;
+    }
+    return endValue.getTime() <= this.formData.value.ngayBatDau.getTime();
+  };
+
+
+  async ngOnInit() {
+    await this.spinner.show();
+    try {
+    } catch (e) {
+      console.log("error: ", e);
+      await this.spinner.hide();
+      this.notification.error(MESSAGE.ERROR, MESSAGE.SYSTEM_ERROR);
+    }
+    await this.spinner.hide();
+  }
+  downloadPdf() {
+    saveAs(this.pdfBlob, this.nameFile + ".pdf");
+  }
+
+  async downloadExcel() {
+    try {
+      this.spinner.show();
+      let body = this.formData.value;
+      body.typeFile = "xlsx";
+      body.trangThai = "01";
+      body.maDonVi = !body.maChiCuc ? (!body.maCuc ? null : body.maCuc) : body.maChiCuc
+      await this.bcNhapXuatMuaBanHangDTQGService.baoCaoNhapXuatTon(body).then(async s => {
+        this.excelBlob = s;
+        this.excelSrc = await new Response(s).arrayBuffer();
+        saveAs(this.excelBlob, this.nameFile + ".xlsx");
+      });
+      this.showDlgPreview = true;
+    } catch (e) {
+      console.log(e);
+    } finally {
+      this.spinner.hide();
+    }
+
+  }
+
+  closeDlg() {
+    this.showDlgPreview = false;
+  }
+
+  async preView() {
+
+    this.helperService.markFormGroupTouched(this.formData);
+    if (this.formData.invalid) {
+      this.spinner.hide();
+      return;
+    }
+    try {
+      this.spinner.show();
+      let body = this.formData.value;
+      body.maDonVi = !body.maChiCuc ? (!body.maCuc ? null : body.maCuc) : body.maChiCuc
+      body.typeFile = "pdf";
+      await this.bcNhapXuatMuaBanHangDTQGService.baoCaoNhapXuatTon(body).then(async s => {
+        this.pdfBlob = s;
+        this.pdfSrc = await new Response(s).arrayBuffer();
+      });
+      this.showDlgPreview = true;
+    } catch (e) {
+      console.log(e);
+    } finally {
+      this.spinner.hide();
+    }
+  }
+
+  async clearFilter() {
+    this.formData.reset();
+    this.formData.patchValue({
+      nam: dayjs().get('year')
+    })
+  }
+
+}
