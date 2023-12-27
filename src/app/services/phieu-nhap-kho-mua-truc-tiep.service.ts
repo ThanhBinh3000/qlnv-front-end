2cea083c7a868509f214c8805d69e8c98f11f679
haihm <haihm@ttkd6.com>
2023-12-27 13:48:37 +0700

    fix bug nhập trực tiếp

--- src/app/services/phieu-nhap-kho-mua-truc-tiep.service.ts
+++ src/app/services/phieu-nhap-kho-mua-truc-tiep.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BaseService } from './base.service';
 import { HttpClient } from '@angular/common/http';
-import {BaseLocalService} from "./base-local.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class PhieuNhapKhoMuaTrucTiepService extends BaseLocalService {
+export class PhieuNhapKhoMuaTrucTiepService extends BaseService {
 
   constructor(public httpClient: HttpClient) {
-    super(httpClient, 'phieu-nhap-kho/phieu-nhap-kho-mtt', '');
+    super(httpClient, 'phieu-nhap-kho/phieu-nhap-kho-mtt', '/qlnv-hang');
   }
 }
