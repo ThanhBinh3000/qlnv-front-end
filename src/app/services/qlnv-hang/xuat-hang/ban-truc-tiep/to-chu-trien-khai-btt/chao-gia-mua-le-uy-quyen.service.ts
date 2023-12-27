2cf23c1b527ca82006e812e00fcfed28bd99b37a
TTBinh <anhpp@tecapro.com.vn>
2023-12-27 08:52:20 +0700

    tổ chức cá nhân chào giá

--- src/app/services/qlnv-hang/xuat-hang/ban-truc-tiep/to-chu-trien-khai-btt/chao-gia-mua-le-uy-quyen.service.ts
+++ src/app/services/qlnv-hang/xuat-hang/ban-truc-tiep/to-chu-trien-khai-btt/chao-gia-mua-le-uy-quyen.service.ts
@@ -4,6 +4,7 @@ import {BaseService} from '../../../../base.service';
 import {PATH} from 'src/app/constants/path';
 import {Observable} from "rxjs";
 import {environment} from "../../../../../../environments/environment";
+import {OldResponseData} from "../../../../../interfaces/response";
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,10 @@ export class ChaoGiaMuaLeUyQuyenService extends BaseService {
   exportHopDong(body: any): Observable<Blob> {
     const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/ket-xuat/hop-dong`;
     return this._httpClient.post(url, body, {responseType: 'blob'});
+  }
+
+  getToChucCaNhan(): Promise<OldResponseData> {
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/to-chuc-ca-nhan`;
+    return this._httpClient.get<OldResponseData>(url).toPromise();
   }
 }
