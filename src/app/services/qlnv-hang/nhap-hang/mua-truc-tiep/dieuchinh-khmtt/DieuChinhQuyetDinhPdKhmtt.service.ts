8bd886369e82e191ee6fb5e9bf6b1958fcd8fa90
haihm <haihm@ttkd6.com>
2024-01-02 08:32:03 +0700

    fix bug nhập trực tiếp

--- src/app/services/qlnv-hang/nhap-hang/mua-truc-tiep/dieuchinh-khmtt/DieuChinhQuyetDinhPdKhmtt.service.ts
+++ src/app/services/qlnv-hang/nhap-hang/mua-truc-tiep/dieuchinh-khmtt/DieuChinhQuyetDinhPdKhmtt.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BaseService } from 'src/app/services/base.service';
+import {environment} from "../../../../../../environments/environment";
+import {OldResponseData} from "../../../../../interfaces/response";
 
 @Injectable({
     providedIn: 'root',
@@ -9,7 +11,12 @@ export class DieuChinhQuyetDinhPdKhmttService extends BaseService {
     GATEWAY = '/qlnv-hang';
 
     constructor(public httpClient: HttpClient) {
-        super(httpClient, 'mua-truc-tiep/dc-qd-pd', '/qlnv-hang');
+        super(httpClient, 'mua-truc-tiep/dc-qd-pd', '');
     }
+
+  danhSachQdDc(body) {
+    const url = `${environment.SERVICE_API}${this.GATEWAY}/${this.table}/tra-cuu/dieu-chinh`;
+    return this._httpClient.post<OldResponseData>(url, body).toPromise();
+  }
 
 }
