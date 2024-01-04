abee9f7ba8f60646b702f94f576863d8c5786c67
VuxNT <vunt1809@gmail.com>
2024-01-04 11:47:29 +0700

    Fix tieu huy

--- src/app/pages/xuat/xuat-tieu-huy/ho-so-tieu-huy/chi-tiet-ho-so-tieu-huy/chi-tiet-ho-so-tieu-huy.component.ts
+++ src/app/pages/xuat/xuat-tieu-huy/ho-so-tieu-huy/chi-tiet-ho-so-tieu-huy/chi-tiet-ho-so-tieu-huy.component.ts
@@ -329,7 +329,9 @@ export class ChiTietHoSoTieuHuyComponent extends Base3Component implements OnIni
   }
 
   viewTongCuc() {
-    return this.userService.isTongCuc();
+    let trangThai = this.formData.value.trangThai;
+    return this.userService.isTongCuc() || trangThai == STATUS.CHO_DUYET_LDTC
+      || trangThai == STATUS.DA_DUYET_LDTC || trangThai == STATUS.DADUYET_BTC || trangThai == STATUS.TUCHOI_BTC
   }
 
   onChangeTimeQd($event) {
