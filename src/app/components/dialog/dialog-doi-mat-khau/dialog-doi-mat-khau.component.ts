3446d5f2e011537bafc3ce3771dad68d0c3f8ebe
kieunv <kieunv@ttkd6.com>
2024-01-09 14:25:11 +0700

    update

--- src/app/components/dialog/dialog-doi-mat-khau/dialog-doi-mat-khau.component.ts
+++ src/app/components/dialog/dialog-doi-mat-khau/dialog-doi-mat-khau.component.ts
@@ -6,6 +6,9 @@ import { Globals } from 'src/app/shared/globals';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { HelperService } from '../../../services/helper.service';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { UserService } from 'src/app/services/user.service';
+import { UserLogin } from 'src/app/models/userlogin';
+import { QlNguoiSuDungService } from 'src/app/services/quantri-nguoidung/qlNguoiSuDung.service';
 
 @Component({
   selector: 'app-dialog-doi-mat-khau',
@@ -17,7 +20,9 @@ export class DialogDoiMatKhauComponent implements OnInit {
   isOld: boolean;
   pattern: any
   username: string
+  email: string
   regexComplex: string
+  userInfo: UserLogin;
 
   constructor(
     private fb: FormBuilder,
@@ -26,12 +31,15 @@ export class DialogDoiMatKhauComponent implements OnInit {
     private spinner: NgxSpinnerService,
     private helperService: HelperService,
     private notification: NzNotificationService,
+    public userService: UserService,
+    private qlNSDService: QlNguoiSuDungService,
   ) {
     this.formData = this.fb.group({
       username: [null, [Validators.required]],
       oldPassword: [null, [Validators.required]],
       newPassword: [null, [Validators.required]],
       renewPassword: [null, [Validators.required]],
+      otp: [null, [Validators.required]],
     });
 
   }
@@ -45,17 +53,35 @@ export class DialogDoiMatKhauComponent implements OnInit {
       this.regexComplex = ` Mật khẩu yêu cầu ${this.pattern.sizePassword ? 'tối thiểu ' + this.pattern.sizePassword + ' ký tự ' : ''}${this.pattern.includeNumberAndChar ? 'cả số và chữ, chữ hoa và chữ thường ' : ''}${this.pattern.minSpecial ? this.pattern.minSpecial + ' ký tự đặc biệt' : ''}`
       this.formData.controls["newPassword"].setValidators([Validators.pattern(this.pattern.regexComplex)]);
     }
-
+    this.userInfo = this.userService.getUserLogin();
+    console.log("this.userInfo", this.userInfo)
   }
 
   handleCancel() {
     this._modalRef.destroy();
   }
 
+  async sendOTP() {
+    if (!this.email) {
+      this.notification.error(MESSAGE.ERROR, "Chưa có thông tin email.");
+      return
+    }
+    let res = await this.qlNSDService.senOTP(this.username, this.email);
+    if (res.msg == MESSAGE.SUCCESS) {
+      this.notification.success(MESSAGE.SUCCESS, MESSAGE.SUCCESS);
+    } else {
+      this.notification.error(MESSAGE.ERROR, res.msg);
+    }
+  }
+
   save() {
     if (!this.isOld) {
       this.formData.controls["oldPassword"].clearValidators();
       delete this.formData.value.oldPassword
+    }
+    if (this.userInfo.sub === 'admin') {
+      this.formData.controls["otp"].clearValidators();
+      delete this.formData.value.otp
     }
     this.helperService.markFormGroupTouched(this.formData);
     if (this.formData.invalid) {
