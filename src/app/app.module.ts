f1ea72b7810144c1c674cd2c61bf2773cdaa71d5
PM <anhpp@tecapro.com.vn>
2024-01-16 08:46:48 +0700

    install lib date-fns and update text date picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,13 @@ import { LoginComponent } from './pages/login/login.component';
 
 import { CommonInterceptor } from './interceptor/common.interceptor';
 import { NzConfig, NZ_CONFIG } from 'ng-zorro-antd/core/config';
-import { NZ_I18N, vi_VN } from 'ng-zorro-antd/i18n';
+import { NZ_I18N, NzI18nService, vi_VN } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import vi from '@angular/common/locales/vi';
 import { ObservableService } from './services/observable.service';
 import { DirectivesModule } from './directives/directives.module';
 import { Globals } from './shared/globals';
-
+import { vi as viDateFs } from 'date-fns/locale';
 registerLocaleData(vi);
 
 const ngZorroConfig: NzConfig = {
@@ -47,4 +47,10 @@ const ngZorroConfig: NzConfig = {
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    private i18n: NzI18nService
+  ) {
+    this.i18n.setDateLocale(viDateFs)
+  }
+}
