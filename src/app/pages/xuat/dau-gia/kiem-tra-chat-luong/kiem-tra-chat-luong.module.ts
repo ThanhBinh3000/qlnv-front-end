4e9a7cfdbbcaa31298cf298decb192bca4b4ed67
TTBinh <anhpp@tecapro.com.vn>
2024-03-11 10:18:28 +0700

    bán đấu giá

--- src/app/pages/xuat/dau-gia/kiem-tra-chat-luong/kiem-tra-chat-luong.module.ts
+++ src/app/pages/xuat/dau-gia/kiem-tra-chat-luong/kiem-tra-chat-luong.module.ts
@@ -1,41 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { ComponentsModule } from 'src/app/components/components.module';
-import { DirectivesModule } from 'src/app/directives/directives.module';
-import { ChucNangKiemTraComponent } from './main/chuc-nang-kiem-tra.component';
-import { HoSoKyThuatComponent } from './ho-so-ky-thuat/ho-so-ky-thuat.component';
-import { KiemTraChatLuongComponent } from './kiem-tra-chat-luong.component';
-import { TableBienBanLayMauComponent } from './bien-ban-lay-mau/table-bien-ban-lay-mau.component';
-import { CreateBienBanLayMauComponent } from './bien-ban-lay-mau/create-bien-ban-lay-mau/create-bien-ban-lay-mau.component';
-import { QuanLyPhieuKiemNghiemChatLuongComponent } from './phieu-kiem-nghiem-cl/quan-ly-phieu-kiem-nghiem-chat-luong.component';
-import { ThemMoiPhieuKiemNghiemChatLuongComponent } from './phieu-kiem-nghiem-cl/them-moi-phieu-kiem-nghiem-chat-luong/them-moi-phieu-kiem-nghiem-chat-luong.component';
-import { GiaoXuatHangModule } from '../giao-xuat-hang/giao-xuat-hang.module';
-import { CreateDaiDienComponent } from './bien-ban-lay-mau/create-bien-ban-lay-mau/create-dai-dien/create-dai-dien.component';
+import {NgModule} from '@angular/core';
 
-@NgModule({
-  // declarations: [
-  //   KiemTraChatLuongComponent,
-  //   ChucNangKiemTraComponent,
-  //   TableBienBanLayMauComponent,
-  //   CreateBienBanLayMauComponent,
-  //   HoSoKyThuatComponent,
-  //   ThongTinHoSoKyThuatComponent,
-  //   QuanLyPhieuKiemNghiemChatLuongComponent,
-  //   ThemMoiPhieuKiemNghiemChatLuongComponent,
-  //   CreateDaiDienComponent,
-  // ],
-  // imports: [
-  //   GiaoXuatHangModule,
-  //   CommonModule,
-  //   ComponentsModule,
-  //   DirectivesModule,
-  // ],
-  // exports: [
-  //   KiemTraChatLuongComponent,
-  //   ChucNangKiemTraComponent,
-  //   TableBienBanLayMauComponent,
-  //   CreateBienBanLayMauComponent,
-  //   HoSoKyThuatComponent,
-  // ]
-})
-export class KiemTraChatLuongModule { }
+@NgModule({})
+export class KiemTraChatLuongModule {
+}
--- src/app/pages/xuat/dau-gia/quyet-dinh-phe-duyet-ket-qua/quyet-dinh-phe-duyet-ket-qua.module.ts
+++ src/app/pages/xuat/dau-gia/quyet-dinh-phe-duyet-ket-qua/quyet-dinh-phe-duyet-ket-qua.module.ts
@@ -1,33 +1,5 @@
-import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {ComponentsModule} from 'src/app/components/components.module';
-import {DirectivesModule} from 'src/app/directives/directives.module';
-import {QuyetDinhPheDuyetKetQuaComponent} from "./quyet-dinh-phe-duyet-ket-qua.component";
-import {
-  DanhSachQuyetDinhPheDuyetKetQuaComponent
-} from './danh-sach-quyet-dinh-phe-duyet-ket-qua/danh-sach-quyet-dinh-phe-duyet-ket-qua.component';
-import {
-  ThemMoiQuyetDinhPheDuyetKetQuaComponent
-} from './danh-sach-quyet-dinh-phe-duyet-ket-qua/them-moi-quyet-dinh-phe-duyet-ket-qua/them-moi-quyet-dinh-phe-duyet-ket-qua.component';
-import {KeHoachBanDauGiaModule} from "../ke-hoach-ban-dau-gia/ke-hoach-ban-dau-gia.module";
 
-@NgModule({
-  declarations: [
-    QuyetDinhPheDuyetKetQuaComponent,
-    DanhSachQuyetDinhPheDuyetKetQuaComponent,
-    ThemMoiQuyetDinhPheDuyetKetQuaComponent,
-  ],
-  exports: [
-    QuyetDinhPheDuyetKetQuaComponent,
-    ThemMoiQuyetDinhPheDuyetKetQuaComponent,
-    DanhSachQuyetDinhPheDuyetKetQuaComponent,
-  ],
-  imports: [
-    CommonModule,
-    ComponentsModule,
-    DirectivesModule,
-    KeHoachBanDauGiaModule,
-  ]
-})
+@NgModule({})
 export class QuyetDinhPheDuyetKetQuaModule {
 }
