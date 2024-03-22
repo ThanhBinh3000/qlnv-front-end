65ad166c6bee48d450d32c53a1b492f553a9cf23
OTHoa <anhpp@tecapro.com.vn>
2024-03-22 10:41:29 +0700

    add báo cáo vụ QLHDT - Tổng hợp kết quả đấu thầu mua gạo năm ....

---
+++ src/app/pages/khoi-tao-du-lieu/ht-may-moc-thiet-bi/ht-may-moc-thiet-bi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ht-may-moc-thiet-bi',
+  templateUrl: './ht-may-moc-thiet-bi.component.html',
+  styleUrls: ['./ht-may-moc-thiet-bi.component.scss']
+})
+export class HtMayMocThietBiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
