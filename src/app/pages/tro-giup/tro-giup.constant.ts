a28dc7526b729511bb33c04dc463508519f37ed6
PM <anhpp@tecapro.com.vn>
2024-04-03 11:02:48 +0700

    update tro giup

--- src/app/pages/tro-giup/tro-giup.constant.ts
+++ src/app/pages/tro-giup/tro-giup.constant.ts
@@ -2,7 +2,7 @@ import { ChiTietMenu } from "src/app/models/ChiTietMenu";
 export const MAIN_ROUTE_TROGIUP = 'tro-giup'
 export const ROUTE_LIST: Array<ChiTietMenu> = [
     {
-        icon: 'htvbdh_xac-nhan1',
+        icon: 'htvbdh_cau-hoi',
         title: 'Hướng dẫn sử dụng',
         url: `/${MAIN_ROUTE_TROGIUP}/huong-dan-su-dung`,
         dropdown: 'huong-dan-su-dung',
